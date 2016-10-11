fc730f1c20d49f5f2598689334a0b2642a547f7e
Stanley Shyiko <stanley.shyiko@gmail.com>
2016-10-11 08:58:23 -0700

    Made GlobFileFilterTest pass on macOS

--- ktlint/src/test/kotlin/com/github/shyiko/ktlint/internal/path/GlobFileFilterTest.kt
+++ ktlint/src/test/kotlin/com/github/shyiko/ktlint/internal/path/GlobFileFilterTest.kt
@@ -1,6 +1,5 @@
 package com.github.shyiko.ktlint.internal.path
 
-import com.github.shyiko.ktlint.internal.path.GlobFileFilter
 import org.testng.Assert.assertTrue
 import org.testng.Assert.assertFalse
 import org.testng.annotations.Test
@@ -8,20 +7,22 @@ import java.io.File
 
 class GlobFileFilterTest {
 
+    private val dir = File("/tmp").canonicalPath
+
     @Test
     fun testNegation() {
-        val filter = GlobFileFilter("/tmp", "/tmp/**/*.kt", "!/tmp/**/*test*/**/*.kt", "!/tmp/**/prefix*/**/*.kt",
-            "!/tmp/**/*suffix/**/*.kt")
-        assertTrue(filter.accept(File("/tmp/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/test_/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/_test_/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/_test/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/prefix_/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/prefix/a.kt")))
-        assertTrue(filter.accept(File("/tmp/a/_prefix/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/_suffix/a.kt")))
-        assertFalse(filter.accept(File("/tmp/a/suffix/a.kt")))
-        assertTrue(filter.accept(File("/tmp/a/suffix_/a.kt")))
+        val filter = GlobFileFilter("$dir", "$dir/**/*.kt", "!$dir/**/*test*/**/*.kt", "!$dir/**/prefix*/**/*.kt",
+            "!$dir/**/*suffix/**/*.kt")
+        assertTrue(filter.accept(File("$dir/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/test_/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/_test_/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/_test/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/prefix_/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/prefix/a.kt")))
+        assertTrue(filter.accept(File("$dir/a/_prefix/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/_suffix/a.kt")))
+        assertFalse(filter.accept(File("$dir/a/suffix/a.kt")))
+        assertTrue(filter.accept(File("$dir/a/suffix_/a.kt")))
         assertTrue(GlobFileFilter("/C:/ktlint", "/C:/ktlint/src/**/*.kt")
             .accept(File("/C:/ktlint/src/test/kotlin/com/github/shyiko/ktlint/LinterTest.kt")))
     }
