14b83c5418850c4b22b7d0ec8bfbac3f6f9bc2a9
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-05-27 11:56:56 -0700

    Introduced "no-unit-return" rule

---
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoUnitReturnRule.kt
@@ -0,0 +1,28 @@
+package com.github.shyiko.ktlint.ruleset.standard
+
+import com.github.shyiko.ktlint.core.Rule
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
+
+class NoUnitReturnRule : Rule("no-unit-return") {
+
+    override fun visit(
+        node: ASTNode,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        if (node.elementType == KtStubElementTypes.TYPE_REFERENCE
+            && node.treeParent.elementType == KtStubElementTypes.FUNCTION
+            && node.text.contentEquals("Unit")) {
+            emit(node.startOffset, "Unnecessary \"Unit\" return type", true)
+            if (autoCorrect) {
+                var prevNode = node
+                while (prevNode.treePrev.elementType != KtStubElementTypes.VALUE_PARAMETER_LIST) {
+                    prevNode = prevNode.treePrev
+                }
+                node.treeParent.removeRange(prevNode, node.treeNext)
+            }
+        }
+    }
+
+}
