0c27268845744107c91aed73eb4e6a11958130cb
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-05-30 08:54:18 -0700

    Added support for vertically aligned comments

--- ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoMultipleSpacesRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoMultipleSpacesRule.kt
@@ -2,19 +2,83 @@ package com.github.shyiko.ktlint.ruleset.standard
 
 import com.github.shyiko.ktlint.core.Rule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.com.intellij.openapi.util.TextRange
+import org.jetbrains.kotlin.com.intellij.psi.PsiComment
+import org.jetbrains.kotlin.com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.jetbrains.kotlin.com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.diagnostics.DiagnosticUtils
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
+import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
+
+typealias Offset = Int
 
 class NoMultipleSpacesRule : Rule("no-multi-spaces") {
 
+    data class CommentRelativeLocation(
+        val prevCommentOffset: Offset,
+        val line: Int,
+        val column: Int,
+        val nextCommentOffset: Offset
+    )
+
+    private lateinit var fileNode: ASTNode
+
+    // todo: do not recalculate tree below node.startOffset
+    private val commentMap: Map<Offset, CommentRelativeLocation>
+        get() {
+            val comments = mutableListOf<PsiComment>()
+            fileNode.visit { node ->
+                val psi = node.psi
+                if (psi is PsiComment) { comments.add(psi) }
+            }
+            return comments.foldIndexed(mutableMapOf<Offset, CommentRelativeLocation>()) { i, acc, comment ->
+                val pos = DiagnosticUtils.getLineAndColumnInPsiFile(fileNode.psi as PsiFile,
+                    TextRange(comment.startOffset, comment.startOffset))
+                acc.put(comment.startOffset, CommentRelativeLocation(
+                    prevCommentOffset = comments.getOrNull(i - 1)?.startOffset ?: -1,
+                    line = pos.line,
+                    column = pos.column,
+                    nextCommentOffset = comments.getOrNull(i + 1)?.startOffset ?: -1
+                ))
+                acc
+            }
+        }
+
     override fun visit(node: ASTNode, autoCorrect: Boolean,
             emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
+        if (node.elementType == KtStubElementTypes.FILE) {
+            fileNode = node
+        } else
         if (node is PsiWhiteSpace && !node.textContains('\n') && node.getTextLength() > 1) {
+            val nextLeaf = PsiTreeUtil.nextLeaf(node, true)
+            if (nextLeaf is PsiComment) {
+                val positionMap = commentMap
+                val commentRL = commentMap[nextLeaf.startOffset]!! // NPE here (or anywhere below) would mean that TARFU
+                if (commentRL.prevCommentOffset != -1) {
+                    val prevCommentRL = positionMap[commentRL.prevCommentOffset]!!
+                    if (commentRL.line - 1 == prevCommentRL.line && commentRL.column == prevCommentRL.column) {
+                        return
+                    }
+                }
+                if (commentRL.nextCommentOffset != -1) {
+                    val nextCommentRL = positionMap[commentRL.nextCommentOffset]!!
+                    if (commentRL.line + 1 == nextCommentRL.line && commentRL.column == nextCommentRL.column) {
+                        return
+                    }
+                }
+            }
             emit(node.startOffset + 1, "Unnecessary space(s)", true)
             if (autoCorrect) {
                 (node as LeafPsiElement).replaceWithText(" ")
             }
         }
+    }
+
+    private fun ASTNode.visit(cb: (node: ASTNode) -> Unit) {
+        cb(this)
+        this.getChildren(null).forEach { it.visit(cb) }
     }
 
 }
--- ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoMultipleSpacesRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoMultipleSpacesRuleTest.kt
@@ -14,6 +14,25 @@ class NoMultipleSpacesRuleTest {
             .isEqualTo(listOf(
                 LintError(1, 22, "no-multi-spaces", "Unnecessary space(s)")
             ))
+        // allow vertical alignment of comments
+        assertThat(NoMultipleSpacesRule().lint(
+            """
+            // square/kotlinpoet/src/main/java/com/squareup/kotlinpoet/FunSpec.kt case
+            fun characterLiteralWithoutSingleQuotes(c: Char): String {
+              return when {
+                c == '\b' -> "\\b"   // \u0008: backspace (BS)
+                c == '\t' -> "\\t"   // \u0009: horizontal tab (HT)
+                c == '\n' -> "\\n"   // \u000a: linefeed (LF)
+                c == '\r' -> "\\r"   // \u000d: carriage return (CR)
+                c == '\"' -> "\""    // \u0022: double quote (")
+                c == '\'' -> "\\'"   // \u0027: single quote (')
+                c == '\\' -> "\\\\"  // \u005c: backslash (\)
+                isISOControl(c) -> String.format("\\u%04x", c.toInt())
+                else -> Character.toString(c)
+              }
+            }
+            """.trimIndent()
+        )).isEmpty()
     }
 
     @Test
