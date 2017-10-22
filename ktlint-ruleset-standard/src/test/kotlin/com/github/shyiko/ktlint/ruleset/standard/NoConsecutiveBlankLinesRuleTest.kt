062601d2dbfb05ea25271638fa8006778e7da6c9
Ilya Ghirici <ilya.ghirici@gmail.com>
2017-10-22 22:31:05 +0300

    Add more unit test for NoConsecutiveBlankLinesRule

--- ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoConsecutiveBlankLinesRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoConsecutiveBlankLinesRuleTest.kt
@@ -7,35 +7,90 @@ import org.assertj.core.api.Assertions.assertThat
 import org.testng.annotations.Test
 
 class NoConsecutiveBlankLinesRuleTest {
-
     @Test
-    fun testLint() {
-        assertThat(NoConsecutiveBlankLinesRule().lint("fun main() {\n\n\n}")).isEqualTo(listOf(
-            LintError(3, 1, "no-consecutive-blank-lines", "Needless blank line(s)")
-        ))
-        assertThat(NoConsecutiveBlankLinesRule().lint("fun main() {println(\"\"\"\n\n\n\"\"\")}")).isEmpty()
+    fun testLintInDeclarations() {
+        assertThat(NoConsecutiveBlankLinesRule().lint(
+            """fun a() {
+
+            }
+
+
+            fun b() {
+
+            }"""
+        )).isEqualTo(listOf(
+            LintError(5, 1, "no-consecutive-blank-lines", "Needless blank line(s)")))
     }
 
     @Test
-    fun testFormat() {
+    fun testLintInCode() {
+        assertThat(NoConsecutiveBlankLinesRule().lint(
+            """fun main() {
+                fun a()
+                fun b()
+
+
+                fun c()
+            }"""
+        )).isEqualTo(listOf(
+            LintError(5, 1, "no-consecutive-blank-lines", "Needless blank line(s)")))
+    }
+
+    @Test
+    fun testLintInString() {
+        assertThat(NoConsecutiveBlankLinesRule().lint(
+            "fun main() {println(\"\"\"\n\n\n\"\"\")}")).isEmpty()
+    }
+
+    @Test
+    fun testFormatInDeclarations() {
         assertThat(NoConsecutiveBlankLinesRule().format(
             """
-            fun main() {
-
+            fun a() {
 
             }
 
 
+            fun b() {
 
+            }
             """
         )).isEqualTo(
             """
-            fun main() {
+            fun a() {
 
             }
 
+            fun b() {
+
+            }
             """
         )
     }
 
+    @Test
+    fun testFormatInCode() {
+        assertThat(NoConsecutiveBlankLinesRule().format(
+            """
+            fun main() {
+                fun a()
+                fun b()
+
+
+                fun c()
+
+            }
+            """
+        )).isEqualTo(
+            """
+            fun main() {
+                fun a()
+                fun b()
+
+                fun c()
+
+            }
+            """
+        )
+    }
 }
