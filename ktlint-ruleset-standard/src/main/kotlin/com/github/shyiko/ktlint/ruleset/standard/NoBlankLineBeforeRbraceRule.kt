597092ffbe6ba5ead8f9bdf8419d92f0d38623bc
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-10-25 03:49:12 -0700

    Fixed RBRACE detection (no-blank-line-before-rbrace rule)

--- ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoBlankLineBeforeRbraceRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoBlankLineBeforeRbraceRule.kt
@@ -4,14 +4,17 @@ import com.github.shyiko.ktlint.core.Rule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.jetbrains.kotlin.com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.lexer.KtTokens
 
 class NoBlankLineBeforeRbraceRule : Rule("no-blank-line-before-rbrace") {
     override fun visit(node: ASTNode, autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
-        if (node is PsiWhiteSpace) {
+        if (node is PsiWhiteSpace &&
+            node.textContains('\n') &&
+            PsiTreeUtil.nextLeaf(node, true)?.node?.elementType == KtTokens.RBRACE) {
             val split = node.getText().split("\n")
-
-            if (split.size > 2 && isNextElementRbrace(node)) {
+            if (split.size > 2) {
                 emit(node.startOffset + split[0].length + split[1].length + 1,
                     "Needless blank line(s)", true)
                 if (autoCorrect) {
@@ -20,7 +23,4 @@ class NoBlankLineBeforeRbraceRule : Rule("no-blank-line-before-rbrace") {
             }
         }
     }
-
-    private fun isNextElementRbrace(node: ASTNode) =
-        node.treeNext?.elementType?.index ?: 0 == 147.toShort()
 }
---
+++ ktlint-ruleset-standard/src/test/resources/spec/no-blank-line-before-rbrace/lint.kt.spec
@@ -0,0 +1,21 @@
+fun main() {
+    fun a() {
+
+    }
+    fun b()
+
+}
+
+class A {
+
+}
+
+fun main() {println("""
+
+
+}""")}
+
+// expect
+// 3:1:Needless blank line(s)
+// 6:1:Needless blank line(s)
+// 10:1:Needless blank line(s)
