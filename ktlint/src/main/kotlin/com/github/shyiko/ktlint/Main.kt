eb75e265902a4ecdb5be633fcd5d24c58cc66ee0
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-10-26 11:31:15 -0700

    Fixed --reporter's output handling (previously parent directory was expected to exist) #97

--- ktlint/src/main/kotlin/com/github/shyiko/ktlint/Main.kt
+++ ktlint/src/main/kotlin/com/github/shyiko/ktlint/Main.kt
@@ -275,7 +275,7 @@ ${ByteArrayOutputStream().let { this.printUsage(it); it }.toString().trimEnd().s
                 System.err.println("[DEBUG] Initializing \"${r.id}\" reporter with ${r.config}" +
                     (r.output?.let { ", output=$it" } ?: ""))
             }
-            val output = if (r.output != null) PrintStream(r.output) else
+            val output = if (r.output != null) { File(r.output).parentFile?.mkdirsOrFail(); PrintStream(r.output) } else
                 if (stdin) System.err else System.out
             reporterProvider.get(output, r.config).let { reporter ->
                 if (r.output != null)
