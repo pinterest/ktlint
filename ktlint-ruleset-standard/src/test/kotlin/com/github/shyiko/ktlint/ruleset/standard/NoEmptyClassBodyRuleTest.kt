871a5d708394537286b4b7ac139cff1d5eacf0eb
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-11-22 22:34:16 -0800

    Fixed no-empty-class-body auto-correction when file ends with {} (without a newline) (#109)

--- ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoEmptyClassBodyRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoEmptyClassBodyRuleTest.kt
@@ -1,5 +1,7 @@
 package com.github.shyiko.ktlint.ruleset.standard
 
+import com.github.shyiko.ktlint.test.format
+import org.assertj.core.api.Assertions.assertThat
 import org.testng.annotations.Test
 
 class NoEmptyClassBodyRuleTest {
@@ -12,5 +14,11 @@ class NoEmptyClassBodyRuleTest {
     @Test
     fun testFormat() {
         testFormatUsingResource(NoEmptyClassBodyRule())
+    }
+
+    @Test
+    fun testFormatEmptyClassBodyAtTheEndOfFile() {
+        assertThat(NoEmptyClassBodyRule().format("class A {}\n")).isEqualTo("class A\n")
+        assertThat(NoEmptyClassBodyRule().format("class A {}")).isEqualTo("class A")
     }
 }
