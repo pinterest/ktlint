ea3c6fb406069d8906d084545da4536abbc7e470
Stanley Shyiko <stanley.shyiko@gmail.com>
2017-11-27 22:26:55 -0800

    Made no-unused-imports rule reset internal state "on file"

--- ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoUnusedImportsRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/NoUnusedImportsRule.kt
@@ -38,12 +38,14 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
         // destructuring assignment
         "component1", "component2", "component3", "component4", "component5"
     )
-    private val ref = mutableSetOf("*")
+    private val ref = mutableSetOf<String>()
     private var packageName = ""
 
     override fun visit(node: ASTNode, autoCorrect: Boolean,
             emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit) {
         if (node.elementType == KtStubElementTypes.FILE) {
+            ref.clear() // rule can potentially be executed more than once (when formatting)
+            ref.add("*")
             node.visit { vnode ->
                 val psi = vnode.psi
                 val type = vnode.elementType
