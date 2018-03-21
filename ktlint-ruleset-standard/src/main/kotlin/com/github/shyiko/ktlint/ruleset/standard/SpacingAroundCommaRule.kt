4d07ba533afb640fccaa0f413519aee2f683ad4c
Stanley Shyiko <stanley.shyiko@gmail.com>
2018-03-20 17:05:13 -0700

    Made comma-spacing rule more strict (#173)

--- ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/SpacingAroundCommaRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/SpacingAroundCommaRule.kt
@@ -6,6 +6,7 @@ import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import org.jetbrains.kotlin.com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
 class SpacingAroundCommaRule : Rule("comma-spacing") {
 
@@ -14,11 +15,19 @@ class SpacingAroundCommaRule : Rule("comma-spacing") {
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
     ) {
-        if (node is LeafPsiElement && node.textMatches(",") && !node.isPartOfString() &&
-            PsiTreeUtil.nextLeaf(node) !is PsiWhiteSpace) {
-            emit(node.startOffset + 1, "Missing spacing after \"${node.text}\"", true)
-            if (autoCorrect) {
-                node.rawInsertAfterMe(PsiWhiteSpaceImpl(" "))
+        if (node is LeafPsiElement && node.textMatches(",") && !node.isPartOfString()) {
+            val prevLeaf = PsiTreeUtil.prevLeaf(node, true)
+            if (prevLeaf is PsiWhiteSpace) {
+                emit(prevLeaf.startOffset, "Unexpected spacing before \"${node.text}\"", true)
+                if (autoCorrect) {
+                    prevLeaf.node.treeParent.removeChild(prevLeaf.node)
+                }
+            }
+            if (PsiTreeUtil.nextLeaf(node) !is PsiWhiteSpace) {
+                emit(node.startOffset + 1, "Missing spacing after \"${node.text}\"", true)
+                if (autoCorrect) {
+                    node.rawInsertAfterMe(PsiWhiteSpaceImpl(" "))
+                }
             }
         }
     }
--- ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/SpacingAroundCommaRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/SpacingAroundCommaRuleTest.kt
@@ -23,11 +23,38 @@ class SpacingAroundCommaRuleTest {
         )).isEqualTo(listOf(
             LintError(2, 10, "comma-spacing", "Missing spacing after \",\"")
         ))
+        assertThat(SpacingAroundCommaRule().lint(
+            """
+            some.method(1 , 2)
+            """.trimIndent(),
+            script = true
+        )).isEqualTo(listOf(
+            LintError(1, 14, "comma-spacing", "Unexpected spacing before \",\"")
+        ))
     }
 
     @Test
     fun testFormat() {
         assertThat(SpacingAroundCommaRule().format("fun main() { x(1,3); x(1, 3) }"))
             .isEqualTo("fun main() { x(1, 3); x(1, 3) }")
+        assertThat(SpacingAroundCommaRule().format(
+            """
+            fun fn(
+                arg1: Int ,
+                arg2: Int
+                ,
+
+                arg3: Int
+            ) = Unit
+            """.trimIndent()
+        )).isEqualTo(
+            """
+            fun fn(
+                arg1: Int,
+                arg2: Int,
+
+                arg3: Int
+            ) = Unit
+            """.trimIndent())
     }
 }
