9ff88819e5714afd777b7b37288cde4939045ee8
Pär Nils Amsen <par.nils.amsen@gmail.com>
2018-04-12 08:43:08 +0200

    Update ktlint-git-pre-commit-hook.sh
    Proposing a more verbose pre-commit githook

--- ktlint/src/main/resources/ktlint-git-pre-commit-hook.sh
+++ ktlint/src/main/resources/ktlint-git-pre-commit-hook.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 # https://github.com/shyiko/ktlint pre-commit hook
-git diff --name-only --cached --relative | grep '\.kt[s"]\?$' | xargs ktlint --relative .
-if [ $? -ne 0 ]; then exit 1; fi
+export hasKt=$(git diff --name-only --cached --relative | grep '\.kt[s"]\?$')
+
+if [ ! -z "$hasKt" ]; then
+	tput setaf 2; tput bold; echo "\n🔍  Ktlint check in progress\n"; tput sgr0
+	
+	tput setaf 1; tput bold; git diff --name-only --cached --relative | grep '\.kt[s"]\?$' | xargs ./.ktlint --relative .
+	
+	if [ $? -ne 0 ]; then
+    tput setaf 1; tput bold; echo "\n✘ Kotlin lint errors found, fix and recommit (run \"./.ktlint -F\" to format simple lint errors) \n"; tput sgr0
+	  exit 1
+	fi
+	
+	tput setaf 2; tput bold; echo "✔ Ktlint passed\n"; tput sgr0
+fi
