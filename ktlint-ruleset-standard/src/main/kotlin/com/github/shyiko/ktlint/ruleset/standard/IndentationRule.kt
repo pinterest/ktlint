834996054191c16c66ad2a62c78383e76d68c5d6
Stanley Shyiko <stanley.shyiko@gmail.com>
2018-04-22 10:29:23 -0700

    Removed continuation_indent_size from indent rule (#171)

--- ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/IndentationRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/github/shyiko/ktlint/ruleset/standard/IndentationRule.kt
@@ -6,25 +6,13 @@ import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.lang.FileASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiComment
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
-import org.jetbrains.kotlin.lexer.KtTokens
-import org.jetbrains.kotlin.psi.KtBinaryExpression
-import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtParameterList
-import org.jetbrains.kotlin.psi.KtParenthesizedExpression
-import org.jetbrains.kotlin.psi.KtSafeQualifiedExpression
-import org.jetbrains.kotlin.psi.KtSecondaryConstructor
-import org.jetbrains.kotlin.psi.KtSuperTypeList
-import org.jetbrains.kotlin.psi.KtSuperTypeListEntry
 import org.jetbrains.kotlin.psi.KtTypeConstraintList
-import org.jetbrains.kotlin.psi.KtTypeProjection
-import org.jetbrains.kotlin.psi.KtValueArgumentList
-import org.jetbrains.kotlin.psi.psiUtil.getPrevSiblingIgnoringWhitespaceAndComments
 import org.jetbrains.kotlin.psi.stubs.elements.KtStubElementTypes
 
 class IndentationRule : Rule("indent") {
 
     private var indentSize = -1
-    private var continuationIndentSize = -1
 
     override fun visit(
         node: ASTNode,
@@ -34,10 +22,9 @@ class IndentationRule : Rule("indent") {
         if (node.elementType == KtStubElementTypes.FILE) {
             val ec = EditorConfig.from(node as FileASTNode)
             indentSize = ec.indentSize
-            continuationIndentSize = ec.continuationIndentSize
             return
         }
-        if (indentSize <= 0 || continuationIndentSize <= 0) {
+        if (indentSize <= 0) {
             return
         }
         if (node is PsiWhiteSpace) {
@@ -45,14 +32,12 @@ class IndentationRule : Rule("indent") {
             if (lines.size > 1 && !node.isPartOf(PsiComment::class) && !node.isPartOf(KtTypeConstraintList::class)) {
                 var offset = node.startOffset + lines.first().length + 1
                 val previousIndentSize = node.previousIndentSize()
-                val expectedIndentSize = if (continuationIndentSize == indentSize || shouldUseContinuationIndent(node))
-                    continuationIndentSize else indentSize
                 lines.tail().forEach { indent ->
-                    if (indent.isNotEmpty() && (indent.length - previousIndentSize) % expectedIndentSize != 0) {
+                    if (indent.isNotEmpty() && (indent.length - previousIndentSize) % indentSize != 0) {
                         if (!node.isPartOf(KtParameterList::class)) { // parameter list wrapping enforced by ParameterListWrappingRule
                             emit(
                                 offset,
-                                "Unexpected indentation (${indent.length}) (it should be ${previousIndentSize + expectedIndentSize})",
+                                "Unexpected indentation (${indent.length}) (it should be ${previousIndentSize + indentSize})",
                                 false
                             )
                         }
@@ -61,25 +46,6 @@ class IndentationRule : Rule("indent") {
                 }
             }
         }
-    }
-
-    private fun shouldUseContinuationIndent(node: PsiWhiteSpace): Boolean {
-        val parentNode = node.parent
-        val prevNode = node.getPrevSiblingIgnoringWhitespaceAndComments()?.node?.elementType
-        val nextNode = node.nextSibling?.node?.elementType
-        return (
-            prevNode in KtTokens.ALL_ASSIGNMENTS ||
-            parentNode is KtSecondaryConstructor ||
-            nextNode == KtStubElementTypes.TYPE_REFERENCE ||
-            node.nextSibling is KtSuperTypeList ||
-            node.nextSibling is KtSuperTypeListEntry ||
-            node.nextSibling is KtTypeProjection ||
-            parentNode is KtValueArgumentList ||
-            parentNode is KtBinaryExpression ||
-            parentNode is KtDotQualifiedExpression ||
-            parentNode is KtSafeQualifiedExpression ||
-            parentNode is KtParenthesizedExpression
-        )
     }
 
     // todo: calculating indent based on the previous line value is wrong (see IndentationRule.testLint)
--- ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/IndentationRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/github/shyiko/ktlint/ruleset/standard/IndentationRuleTest.kt
@@ -40,21 +40,6 @@ class IndentationRuleTest {
     }
 
     @Test
-    fun testLintNested() {
-        assertThat(IndentationRule().lint(
-            """
-            fun funA() =
-                  doStuff().use {
-                      while (it.moveToNext()) {
-                          doMore()
-                      }
-                  }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
     fun testLintCustomIndentSize() {
         assertThat(IndentationRule().lint(
             """
@@ -104,250 +89,6 @@ class IndentationRuleTest {
         )).isEmpty()
     }
 
-    @Test
-    fun testLintContinuationIndent() {
-        assertThat(IndentationRule().lint(
-            """
-                class TestContinuation {
-                    fun main() {
-                        val list = listOf(
-                              listOf(
-                                    "string",
-                                    "another string"
-                              ),
-                              listOf("one", "two")
-                        )
-                    }
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentConcatenation() {
-        assertThat(IndentationRule().lint(
-            """
-                class TestSubClass {
-                    fun asdf(string: String) = string
-                    val c = asdf("long_string" +
-                          "")
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentDotQualifiedExpression() {
-        assertThat(IndentationRule().lint(
-            """
-                fun funA() {
-                    ClassA()
-                          .methodA()
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintIndentInsideObjectBody() {
-        assertThat(IndentationRule().lint(
-            """
-                @Test fun field() {
-                    field.validateWith()
-                          .handleWith(object : InterfaceA {
-                              override fun handleFailed(input: String, errors: List<String>) {
-                                  failedMessages.addAll(errors)
-                              }
-
-                              override fun handleSucceeded() {
-                                  succeededMessages.add("success")
-                              }
-                          })
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    // fixme: should each argument be on a separate line?
-    @Test
-    fun testLintContinuationIndentFunctionCallArgumentList() {
-        assertThat(IndentationRule().lint(
-            """
-                fun funA() {
-                    val valueA =
-                          listOf(ClassA(),
-                                ClassB())
-                }
-                fun data() = listOf(
-                      with(ClassA()) {
-                          arrayOf({ paramA: TypeA ->
-                              paramA.build()
-                          }, funB())
-                      },
-                      arrayOf({ paramA: TypeA -> paramA.build() },
-                            funB()
-                      )
-                )
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentInsideParenthesis() {
-        assertThat(IndentationRule().lint(
-            """
-                fun funA() {
-                    val valA = ClassA(
-                          field = (
-                                ClassB(
-                                      fieldBOne = "one",
-                                      fieldBTwo = "two",
-                                      fieldBThree = 0
-                                ))
-                    )
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintIndentGettersAndSetters() {
-        assertThat(IndentationRule().lint(
-            """
-                val storyBody: String
-                    get() = String.format(body, "")
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentSuperTypeList() {
-        assertThat(IndentationRule().lint(
-            """
-            class ClassA(fieldA: TypeA, fieldB: TypeB = DefaultB) :
-                  SuperClassA(fieldA, fieldB)
-            class ClassA(a: TypeA) :
-                  BasePresenter<View>() {
-
-                private lateinit var view: View
-            }
-            """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintIndentInsideFunctionBody() {
-        assertThat(IndentationRule().lint(
-            """
-                fun funA() {
-                    val valueA = ClassA()
-                    valueA.doStuff()
-                    assertThat(valueA.getFieldB()).isEqualTo(100L)
-                }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentInsideSuperTypeList() {
-        assertThat(IndentationRule().lint(
-            """
-            class ClassA : ClassB(), InterfaceA,
-                  InterfaceB {
-            }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentTypeProjection() {
-        assertThat(IndentationRule().lint(
-            """
-            val variable: SuperTpe<TypeA,
-                  TypeB> = Implementation()
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentAssignment() {
-        assertThat(IndentationRule().lint(
-            """
-            fun funA() {
-                val (a, b, c) =
-                      anotherFun()
-            }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test()
-    fun testLintContinuationIndentTypeCasting() {
-        assertThat(IndentationRule().lint(
-            """
-            fun funA() = funB() as
-                  TypeA
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentConstructorDelegation() {
-        assertThat(IndentationRule().lint(
-            """
-            class A : B() {
-                constructor(a: String) :
-                      this(a)
-            }
-            class MyClass{
-                constructor(a: TypeA) :
-                      super(a) {
-                    init(a)
-                }
-            }
-            """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentSafeChaining() {
-        assertThat(IndentationRule().lint(
-            """
-            val valueA = call()
-                  //comment
-                  ?.chainCallC { it.anotherCall() }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
-    @Test
-    fun testLintContinuationIndentTypeDeclaration() {
-        assertThat(IndentationRule().lint(
-            """
-            private fun funA(a: Int, b: String):
-                  MyTypeA {
-                return MyTypeA(a, b)
-            }
-              """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
-        )).isEmpty()
-    }
-
     // https://kotlinlang.org/docs/reference/coding-conventions.html#method-call-formatting
     @Test
     fun testLintMultilineFunctionCall() {
@@ -370,9 +111,9 @@ class IndentationRuleTest {
         assertThat(IndentationRule().lint(
             """
             fun funA(argA: String) =
-                  // comment
+                // comment
             // comment
-                  call(argA)
+                call(argA)
             fun main() {
                 addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
              // comment
@@ -381,7 +122,7 @@ class IndentationRuleTest {
                 })
             }
             """.trimIndent(),
-            mapOf("indent_size" to "4", "continuation_indent_size" to "6")
+            mapOf("indent_size" to "4")
         )).isEqualTo(listOf(
             LintError(7, 1, "indent", "Unexpected indentation (1) (it should be 8)")
         ))
