90f4bccb12a1d01b8ecc9ff1ca491fba8f30abde
Pilipenko Aleksandr <z3d1kk@gmail.com>
2019-05-17 17:09:23 +0300

    Added unary-op-spacing rule

---
+++ ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/SpacingAroundUnaryOperatorsRuleTest.kt
@@ -0,0 +1,24 @@
+package com.pinterest.ktlint.ruleset.standard
+
+import com.pinterest.ktlint.test.diffFileFormat
+import com.pinterest.ktlint.test.diffFileLint
+import org.assertj.core.api.Assertions.assertThat
+import org.testng.annotations.Test
+
+class SpacingAroundUnaryOperatorsRuleTest {
+
+    @Test
+    fun testLint() {
+        assertThat(SpacingAroundUnaryOperatorsRule().diffFileLint("spec/unary-op-spacing/lint.kt.spec")).isEmpty()
+    }
+
+    @Test
+    fun testFormat() {
+        assertThat(
+            SpacingAroundUnaryOperatorsRule().diffFileFormat(
+                "spec/unary-op-spacing/format.kt.spec",
+                "spec/unary-op-spacing/format-expected.kt.spec"
+            )
+        ).isEmpty()
+    }
+}
