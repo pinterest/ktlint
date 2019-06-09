3df1a76f39b5d14a7ffb94686d2489aa3e86f6eb
Jared Burrows <jaredsburrows@gmail.com>
2019-06-09 11:22:17 -0400

    fix main class name for this project, from 'com.github.shyiko.ktlint.Main' to 'com.pinterest.ktlint.Main' (#465)

--- ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/RuleSetProvider.kt
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/RuleSetProvider.kt
@@ -3,7 +3,7 @@ package com.pinterest.ktlint.core
 /**
  * `ktlint` uses [ServiceLoader](http://docs.oracle.com/javase/6/docs/api/java/util/ServiceLoader.html) to
  * discover all available `RuleSetProvider`s on the classpath and so each `RuleSetProvider` must be registered using
- * `META-INF/services/com.github.shyiko.ktlint.core.RuleSetProvider`
+ * `META-INF/services/com.pinterest.ktlint.core.RuleSetProvider`
  * (see `ktlint-ruleset-standard/src/main/resources` for an example).
  */
 interface RuleSetProvider {
