9c959bd33eb9334288e78deff1ec962165145166
Sha Sha Chu <shasha@pinterest.com>
2019-06-26 09:07:12 -0700

    fix lint issues with new test

--- ktlint-core/src/test/kotlin/com/pinterest/ktlint/core/DisabledRulesTest.kt
+++ ktlint-core/src/test/kotlin/com/pinterest/ktlint/core/DisabledRulesTest.kt
@@ -1,17 +1,16 @@
 package com.pinterest.ktlint.core
 
 import com.pinterest.ktlint.core.ast.ElementType
+import java.util.ArrayList
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.junit.Test
-import java.util.ArrayList
 
 class DisabledRulesTest {
 
     @Test
     fun testDisabledRule() {
         class NoVarRule : Rule("no-var") {
-
             override fun visit(
                 node: ASTNode,
                 autoCorrect: Boolean,
