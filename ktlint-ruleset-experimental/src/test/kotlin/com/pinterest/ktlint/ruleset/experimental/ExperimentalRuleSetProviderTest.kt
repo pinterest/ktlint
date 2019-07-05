657299b0f0e206cbc52f9b4c651799e143a3ee52
Roman Zavarnitsyn <rom4ek@users.noreply.github.com>
2019-07-05 18:32:34 +0200

    Add tests for missing rules in a ruleset (#477)

---
+++ ktlint-ruleset-experimental/src/test/kotlin/com/pinterest/ktlint/ruleset/experimental/ExperimentalRuleSetProviderTest.kt
@@ -0,0 +1,8 @@
+package com.pinterest.ktlint.ruleset.experimental
+
+import com.pinterest.ktlint.test.RuleSetProviderTest
+
+class ExperimentalRuleSetProviderTest : RuleSetProviderTest(
+    rulesetClass = ExperimentalRuleSetProvider::class.java,
+    packageName = "com.pinterest.ktlint.ruleset.experimental"
+)
---
+++ ktlint-test/src/main/kotlin/com/pinterest/ktlint/test/RuleSetProviderTest.kt
@@ -0,0 +1,29 @@
+package com.pinterest.ktlint.test
+
+import com.pinterest.ktlint.core.RuleSetProvider
+import java.io.File
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+open class RuleSetProviderTest(
+    private val rulesetClass: Class<out RuleSetProvider>,
+    private val packageName: String
+) {
+
+    @Test
+    fun checkAllRulesProvided() {
+        val srcLocation = rulesetClass.protectionDomain.codeSource.location.path
+        val rulesDir = File(srcLocation + packageName.replace(".", "/"))
+        val packageRules = rulesDir.listFiles()
+            ?.map { it.name.removeSuffix(".class") }
+            ?.filter { it.endsWith("Rule") }
+            ?: arrayListOf()
+
+        val provider = rulesetClass
+        val providerRules = provider.newInstance().get().rules.map { it::class.java.simpleName }
+        val diff = packageRules - providerRules
+        assertThat(diff)
+            .withFailMessage("%s is missing to provide the following rules: \n%s", provider.simpleName, diff.joinToString(separator = "\n"))
+            .hasSize(0)
+    }
+}
