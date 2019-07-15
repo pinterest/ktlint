c1fefba16d399f26a43b271caef9dc6d034b88f2
Sha Sha Chu <shasha@pinterest.com>
2019-07-14 23:23:27 -0700

    Disable autocorrect for MultiLineIfElseRule

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/MultiLineIfElseRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/MultiLineIfElseRule.kt
@@ -3,13 +3,12 @@ package com.pinterest.ktlint.ruleset.standard
 import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.ast.ElementType.ELSE
 import com.pinterest.ktlint.core.ast.ElementType.LBRACE
-import com.pinterest.ktlint.core.ast.ElementType.RBRACE
 import com.pinterest.ktlint.core.ast.ElementType.THEN
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.TreeElement
 
 /**
+ * https://kotlinlang.org/docs/reference/coding-conventions.html#formatting-control-flow-statements
+ *
  * TODO: if, for, when branch, do, while
  */
 class MultiLineIfElseRule : Rule("multiline-if-else") {
@@ -25,10 +24,11 @@ class MultiLineIfElseRule : Rule("multiline-if-else") {
             }
             if (node.firstChildNode?.firstChildNode?.elementType != LBRACE) {
                 emit(node.firstChildNode.startOffset, "Missing { ... }", true)
-                if (autoCorrect) {
+                // Disabled until we autocorrect with proper formatting
+                /*if (autoCorrect) {
                     (node.firstChildNode.firstChildNode as TreeElement).rawInsertBeforeMe(LeafPsiElement(RBRACE, "{"))
                     (node.lastChildNode.lastChildNode as TreeElement).rawInsertAfterMe(LeafPsiElement(LBRACE, "}"))
-                }
+                }*/
             }
         }
     }
