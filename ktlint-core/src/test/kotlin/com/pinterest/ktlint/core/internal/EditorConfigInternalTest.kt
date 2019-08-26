887fbf9b21b103fe8b2788038a572528d1717666
Yahor Berdnikau <egorr.berd@gmail.com>
2019-08-27 00:03:44 +0200

    Replace editorconfig parser to ec4j. (#561)
    * Change editorconfig parser.
    
    Replaced custom parser, that is hard to maintain and not working well,
    with https://github.com/ec4j/ec4j parser.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>
    
    * Trim whitespaces in parsed disabled rules values.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>
    
    * Add ec4j dependency to maven.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

--- ktlint-core/src/test/kotlin/com/pinterest/ktlint/core/internal/EditorConfigInternalTest.kt
+++ ktlint-core/src/test/kotlin/com/pinterest/ktlint/core/internal/EditorConfigInternalTest.kt
@@ -2,105 +2,143 @@ package com.pinterest.ktlint.core.internal
 
 import com.google.common.jimfs.Configuration
 import com.google.common.jimfs.Jimfs
+import java.nio.file.FileSystem
 import java.nio.file.Files
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.After
 import org.junit.Test
 
 class EditorConfigInternalTest {
+    private val tempFileSystem = Jimfs.newFileSystem(Configuration.forCurrentPlatform())
+
+    private fun FileSystem.writeEditorConfigFile(
+        filePath: String,
+        content: String
+    ) {
+        Files.createDirectories(getPath(filePath))
+        Files.write(getPath("$filePath/.editorconfig"), content.toByteArray())
+    }
+
+    @After
+    fun tearDown() {
+        tempFileSystem.close()
+    }
 
     @Test
     fun testParentDirectoryFallback() {
-        val fs = Jimfs.newFileSystem(Configuration.unix())
-        Files.createDirectories(fs.getPath("/projects/project-1/project-1-subdirectory"))
-        for (
-            cfg in arrayOf(
-                """
-                [*]
-                indent_size = 2
-                """,
-                """
-                root = true
-                [*]
-                indent_size = 2
-                """,
-                """
-                [*]
-                indent_size = 4
-                [*.{kt,kts}]
-                indent_size = 2
-                """,
-                """
-                [*.{kt,kts}]
-                indent_size = 4
-                [*]
-                indent_size = 2
-                """
+        val projectDir = "/projects/project-1"
+        val projectSubDirectory = "$projectDir/project-1-subdirectory"
+        Files.createDirectories(tempFileSystem.getPath(projectSubDirectory))
+        val editorConfigFiles = arrayOf(
+            """
+            [*]
+            indent_size = 2
+            """.trimIndent(),
+            """
+            root = true
+            [*]
+            indent_size = 2
+            """.trimIndent(),
+            """
+            [*]
+            indent_size = 4
+            [*.{kt,kts}]
+            indent_size = 2
+            """.trimIndent(),
+            """
+            [*.{kt,kts}]
+            indent_size = 4
+            [*]
+            indent_size = 2
+            """.trimIndent()
+        )
+
+        editorConfigFiles.forEach { editorConfigFileContent ->
+            tempFileSystem.writeEditorConfigFile(projectDir, editorConfigFileContent)
+
+            val editorConfig = EditorConfigInternal.of(
+                tempFileSystem.getPath(projectSubDirectory)
             )
-        ) {
-            Files.write(fs.getPath("/projects/project-1/.editorconfig"), cfg.trimIndent().toByteArray())
-            val editorConfig = EditorConfigInternal.of(fs.getPath("/projects/project-1/project-1-subdirectory"))
+
             assertThat(editorConfig?.parent).isNull()
             assertThat(editorConfig?.toMap())
-                .overridingErrorMessage("Expected \n%s\nto yield indent_size = 2", cfg.trimIndent())
-                .isEqualTo(mapOf("indent_size" to "2"))
+                .overridingErrorMessage(
+                    "Expected \n%s\nto yield indent_size = 2",
+                    editorConfigFileContent
+                )
+                .isEqualTo(
+                    mapOf(
+                        "indent_size" to "2",
+                        "tab_width" to "2"
+                    )
+                )
         }
     }
 
     @Test
     fun testRootTermination() {
-        val fs = Jimfs.newFileSystem(Configuration.unix())
-        Files.createDirectories(fs.getPath("/projects/project-1/project-1-subdirectory"))
-        Files.write(
-            fs.getPath("/projects/.editorconfig"),
+        val rootDir = "/projects"
+        val project1Dir = "$rootDir/project-1"
+        val project1Subdirectory = "$project1Dir/project-1-subdirectory"
+        tempFileSystem.writeEditorConfigFile(
+            rootDir,
             """
             root = true
             [*]
             end_of_line = lf
-            """.trimIndent().toByteArray()
+            """.trimIndent()
         )
-        Files.write(
-            fs.getPath("/projects/project-1/.editorconfig"),
+        tempFileSystem.writeEditorConfigFile(
+            project1Dir,
             """
             root = true
             [*.{kt,kts}]
             indent_size = 4
             indent_style = space
-            """.trimIndent().toByteArray()
+            """.trimIndent()
         )
-        Files.write(
-            fs.getPath("/projects/project-1/project-1-subdirectory/.editorconfig"),
+        tempFileSystem.writeEditorConfigFile(
+            project1Subdirectory,
             """
             [*]
             indent_size = 2
-            """.trimIndent().toByteArray()
+            """.trimIndent()
         )
-        EditorConfigInternal.of(fs.getPath("/projects/project-1/project-1-subdirectory")).let { editorConfig ->
-            assertThat(editorConfig?.parent).isNotNull()
-            assertThat(editorConfig?.parent?.parent).isNull()
-            assertThat(editorConfig?.toMap()).isEqualTo(
-                mapOf(
-                    "indent_size" to "2",
-                    "indent_style" to "space"
-                )
+
+        var parsedEditorConfig = EditorConfigInternal.of(
+            tempFileSystem.getPath(project1Subdirectory)
+        )
+        assertThat(parsedEditorConfig?.parent).isNotNull
+        assertThat(parsedEditorConfig?.parent?.parent).isNull()
+        assertThat(parsedEditorConfig?.toMap()).isEqualTo(
+            mapOf(
+                "indent_size" to "2",
+                "tab_width" to "2",
+                "indent_style" to "space"
             )
-        }
-        EditorConfigInternal.of(fs.getPath("/projects/project-1")).let { editorConfig ->
-            assertThat(editorConfig?.parent).isNull()
-            assertThat(editorConfig?.toMap()).isEqualTo(
-                mapOf(
-                    "indent_size" to "4",
-                    "indent_style" to "space"
-                )
+        )
+
+        parsedEditorConfig = EditorConfigInternal.of(
+            tempFileSystem.getPath(project1Dir)
+        )
+        assertThat(parsedEditorConfig?.parent).isNull()
+        assertThat(parsedEditorConfig?.toMap()).isEqualTo(
+            mapOf(
+                "indent_size" to "4",
+                "tab_width" to "4",
+                "indent_style" to "space"
             )
-        }
-        EditorConfigInternal.of(fs.getPath("/projects")).let { editorConfig ->
-            assertThat(editorConfig?.parent).isNull()
-            assertThat(editorConfig?.toMap()).isEqualTo(
-                mapOf(
-                    "end_of_line" to "lf"
-                )
+        )
+
+        parsedEditorConfig = EditorConfigInternal.of(
+            tempFileSystem.getPath(rootDir)
+        )
+        assertThat(parsedEditorConfig?.parent).isNull()
+        assertThat(parsedEditorConfig?.toMap()).isEqualTo(
+            mapOf(
+                "end_of_line" to "lf"
             )
-        }
+        )
     }
 
     @Test
@@ -122,5 +160,51 @@ class EditorConfigInternalTest {
         assertThat(EditorConfigInternal.parseSection("*.{js,py")).isEqualTo(listOf("*.js", "*.py"))
         assertThat(EditorConfigInternal.parseSection("*.{js,{py")).isEqualTo(listOf("*.js", "*.{py"))
         assertThat(EditorConfigInternal.parseSection("*.py}")).isEqualTo(listOf("*.py}"))
+    }
+
+    @Test
+    fun `Should parse assignment with spaces`() {
+        val projectDir = "/project"
+        val editorconfigFile =
+            """
+            [*.{kt, kts}]
+            insert_final_newline = true
+            disabled_rules = import-ordering
+            """.trimIndent()
+        tempFileSystem.writeEditorConfigFile(projectDir, editorconfigFile)
+
+        val parsedEditorConfig = EditorConfigInternal.of(
+            tempFileSystem.getPath(projectDir)
+        )
+
+        assertThat(parsedEditorConfig).isNotNull
+        assertThat(parsedEditorConfig?.toMap()).isEqualTo(
+            mapOf(
+                "insert_final_newline" to "true",
+                "disabled_rules" to "import-ordering"
+            )
+        )
+    }
+
+    @Test
+    fun `Should parse list with spaces after comma`() {
+        val projectDir = "/project"
+        val editorconfigFile =
+            """
+            [*.{kt, kts}]
+            disabled_rules = import-ordering, no-wildcard-imports
+            """.trimIndent()
+        tempFileSystem.writeEditorConfigFile(projectDir, editorconfigFile)
+
+        val parsedEditorConfig = EditorConfigInternal.of(
+            tempFileSystem.getPath(projectDir)
+        )
+
+        assertThat(parsedEditorConfig).isNotNull
+        assertThat(parsedEditorConfig?.toMap()).isEqualTo(
+            mapOf(
+                "disabled_rules" to "import-ordering, no-wildcard-imports"
+            )
+        )
     }
 }
