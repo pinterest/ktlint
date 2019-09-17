ad973cab8a6fc4af87c1f8097dc5867b3b5b2aed
AdamMc331 <amcneilly331@gmail.com>
2019-09-17 12:02:40 -0400

    Unit tested changes for #582.

---
+++ ktlint-reporter-plain/src/test/kotlin/com/pinterest/ktlint/reporter/plain/PlainReporterProviderTest.kt
@@ -0,0 +1,52 @@
+package com.pinterest.ktlint.reporter.plain
+
+import com.pinterest.ktlint.reporter.plain.internal.Color
+import org.junit.Assert.*
+import org.junit.Test
+import java.io.PrintStream
+import java.lang.System.out
+
+class PlainReporterProviderTest {
+    @Test
+    fun testColorDefaults() {
+        val plainReporter = PlainReporterProvider().get(
+            out = PrintStream(out, true),
+            opt = mapOf()
+        ) as PlainReporter
+
+        assertFalse(plainReporter.shouldColorOutput)
+        assertEquals(Color.DARK_GRAY, plainReporter.outputColor)
+    }
+
+    @Test
+    fun testValidColor() {
+        val plainReporter = PlainReporterProvider().get(
+            out = PrintStream(out, true),
+            opt = mapOf("color" to "true")
+        ) as PlainReporter
+
+        assertTrue(plainReporter.shouldColorOutput)
+    }
+
+    @Test
+    fun testInvalidOutputColor() {
+        val plainReporter = PlainReporterProvider().get(
+            out = PrintStream(out, true),
+            opt = mapOf("outputColor" to "GarbageInput")
+        ) as PlainReporter
+
+        assertEquals(Color.DARK_GRAY, plainReporter.outputColor)
+    }
+
+    @Test
+    fun testValidOutputColor() {
+        val outputColor = Color.RED
+
+        val plainReporter = PlainReporterProvider().get(
+            out = PrintStream(out, true),
+            opt = mapOf("outputColor" to outputColor.name)
+        ) as PlainReporter
+
+        assertEquals(outputColor, plainReporter.outputColor)
+    }
+}
