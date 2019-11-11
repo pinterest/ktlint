baac14ca4faceb4eac6f68029c5294c288d3e9bc
Aleksandr <aslepchenkov@parseq.pro>
2019-11-12 00:52:25 +0300

    Suppress remove curly braces from template #157 (#263)
    Adds a way to respect standard suppress code style annotations.

--- ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/KtLint.kt
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/KtLint.kt
@@ -35,7 +35,11 @@ import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.TreeCopyHandler
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtAnnotated
+import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import sun.reflect.ReflectionFactory
 
 object KtLint {
@@ -466,6 +470,14 @@ object KtLint {
                                 }
                         }
                     }
+                    // Extract all Suppress annotations and create SuppressionHints
+                    val psi = node.psi
+                    if (psi is KtAnnotated) {
+                        createSuppressionHintFromAnnotations(psi, suppressAnnotations, suppressAnnotationRuleMap)
+                            ?.let {
+                            result.add(it)
+                        }
+                    }
                 }
                 result.addAll(
                     open.map {
@@ -490,6 +502,42 @@ object KtLint {
                 comment.replace(Regex("\\s"), " ").replace(" {2,}", " ").split(" ")
 
             private fun <T> List<T>.tail() = this.subList(1, this.size)
+
+            private val suppressAnnotationRuleMap = mapOf(
+                "RemoveCurlyBracesFromTemplate" to "string-template"
+            )
+
+            private val suppressAnnotations = setOf("Suppress", "SuppressWarnings")
+
+            /**
+             * Creates [SuppressionHint] from annotations of given [psi]
+             * Returns null if no [targetAnnotations] present or no mapping exists
+             * between annotations' values and ktlint rules
+             */
+            private fun createSuppressionHintFromAnnotations(
+                psi: KtAnnotated,
+                targetAnnotations: Collection<String>,
+                annotationValueToRuleMapping: Map<String, String>
+            ): SuppressionHint? {
+
+                return psi.annotationEntries
+                    .filter {
+                        it.calleeExpression?.constructorReferenceExpression
+                            ?.getReferencedName() in targetAnnotations
+                    }.flatMap(KtAnnotationEntry::getValueArguments)
+                    .mapNotNull { it.getArgumentExpression()?.text?.removeSurrounding("\"") }
+                    .mapNotNull(annotationValueToRuleMapping::get)
+                    .let { suppressedRules ->
+                        if (suppressedRules.isNotEmpty()) {
+                            SuppressionHint(
+                                IntRange(psi.startOffset, psi.endOffset),
+                                suppressedRules.toSet()
+                            )
+                        } else {
+                            null
+                        }
+                    }
+            }
         }
     }
 
