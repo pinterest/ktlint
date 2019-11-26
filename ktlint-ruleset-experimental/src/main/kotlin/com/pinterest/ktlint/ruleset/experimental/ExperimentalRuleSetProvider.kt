375509ff16d86ef67785c6573b150b3002ce35ac
Yuku Kotani <yukukotani@gmail.com>
2019-11-26 19:34:24 +0900

    Implement a rule to lint enum entry name (#638)
    * Implement a rule to lint enum entry name
    
    * Disable auto format for enum entry name for now
    
    * Ignore formatting test

--- ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/ExperimentalRuleSetProvider.kt
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/ExperimentalRuleSetProvider.kt
@@ -11,6 +11,7 @@ class ExperimentalRuleSetProvider : RuleSetProvider {
         IndentationRule(),
         MultiLineIfElseRule(),
         NoEmptyFirstLineInMethodBlockRule(),
-        PackageNameRule()
+        PackageNameRule(),
+        EnumEntryNameCaseRule()
     )
 }
