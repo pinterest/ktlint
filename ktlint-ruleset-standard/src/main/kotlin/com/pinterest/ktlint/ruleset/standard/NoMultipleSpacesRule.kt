6d1606c4cd15800bdf48fe3100d3679024f765ea
Roman Zavarnitsyn <rom4ek93@gmail.com>
2020-05-17 22:31:34 +0200

    Allow multiple spaces in KDoc for standard:no-multi-spaces

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/NoMultipleSpacesRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/NoMultipleSpacesRule.kt
@@ -1,6 +1,8 @@
 package com.pinterest.ktlint.ruleset.standard
 
 import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.ElementType.KDOC_MARKDOWN_LINK
+import com.pinterest.ktlint.core.ast.ElementType.KDOC_TAG
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
@@ -12,7 +14,12 @@ class NoMultipleSpacesRule : Rule("no-multi-spaces") {
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
     ) {
-        if (node is PsiWhiteSpace && !node.textContains('\n') && node.getTextLength() > 1) {
+        if (node is PsiWhiteSpace && !node.textContains('\n') && node.getTextLength() > 1 &&
+            // allow multiple spaces in KDoc in case of KDOC_TAG for alignment, e.g.
+            // @param foo      stuff
+            // @param foobar   stuff2
+            !(node.treePrev.elementType == KDOC_MARKDOWN_LINK && node.treeParent.elementType == KDOC_TAG)
+        ) {
             emit(node.startOffset + 1, "Unnecessary space(s)", true)
             if (autoCorrect) {
                 (node as LeafPsiElement).rawReplaceWithText(" ")
