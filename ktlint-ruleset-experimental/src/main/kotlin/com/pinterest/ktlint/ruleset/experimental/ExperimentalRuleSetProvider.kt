15015afc1c327e29930bca4abcf1ccc485f40577
Sha Sha Chu <shasha@pinterest.com>
2020-06-01 11:33:33 -0700

    Adding two new experimental rules that follow new formatting rules introduced in Kotlin 1.3.70. (#757)
    See https://youtrack.jetbrains.com/issue/KT-35106 and https://youtrack.jetbrains.com/issue/KT-35088
    
    Fixes #721

--- ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/ExperimentalRuleSetProvider.kt
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/ExperimentalRuleSetProvider.kt
@@ -13,6 +13,8 @@ class ExperimentalRuleSetProvider : RuleSetProvider {
         NoEmptyFirstLineInMethodBlockRule(),
         PackageNameRule(),
         EnumEntryNameCaseRule(),
-        SpacingAroundDoubleColonRule()
+        SpacingAroundDoubleColonRule(),
+        SpacingBetweenDeclarationsWithCommentsRule(),
+        SpacingBetweenDeclarationsWithAnnotationsRule()
     )
 }
