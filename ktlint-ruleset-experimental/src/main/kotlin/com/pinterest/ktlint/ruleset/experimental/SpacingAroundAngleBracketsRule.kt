322183a2cddebdc6c7c5862fac7a40ff459a01e4
Yash Mittal <ymittal@users.noreply.github.com>
2020-06-09 11:35:40 -0700

    Add experimental SpacingAroundAngleBracketsRule (#769)
    * Move angle bracket test to experimental heirarchy
    
    The new angle bracket rule will live in the experimental rule set along with all the other new rules, so this commit refactors the associated tests to also live under the experimental package
    
    * Add Angle Brackets Rule
    
    Adds a rule to lint against white space before and after angle brackets in type arguments. This includes all of the following:
    Map< String, Int>
    Map<String, Int >
    Map <String, Int>
    
    In all of the above cases, the desired format is
    Map<String, Int>
    
    Specifically this rule does not affect generics formatting of the type
    fun myFun<T>()
    as this is already implemented in a separate rule.
    
    * Handle newlines and TYPE_PARAMETER_LIST
    
    * Move angle bracket test to experimental heirarchy
    
    The new angle bracket rule will live in the experimental rule set along with all the other new rules, so this commit refactors the associated tests to also live under the experimental package
    
    * Add Angle Brackets Rule
    
    Adds a rule to lint against white space before and after angle brackets in type arguments. This includes all of the following:
    Map< String, Int>
    Map<String, Int >
    Map <String, Int>
    
    In all of the above cases, the desired format is
    Map<String, Int>
    
    Specifically this rule does not affect generics formatting of the type
    fun myFun<T>()
    as this is already implemented in a separate rule.
    
    * Handle newlines and TYPE_PARAMETER_LIST
    
    * Rename to spacing-around-angle-brackets and fix "fun"
    
    * Add more tests
    
    Co-authored-by: Christina Lee <lee@lee-5CPJGH6.dyn.pinadmin.com>
    Co-authored-by: Yash Mittal <ymittal@pinterest.com>

---
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/SpacingAroundAngleBracketsRule.kt
@@ -0,0 +1,96 @@
+package com.pinterest.ktlint.ruleset.experimental
+
+import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.ElementType.FUN_KEYWORD
+import com.pinterest.ktlint.core.ast.ElementType.TYPE_ARGUMENT_LIST
+import com.pinterest.ktlint.core.ast.ElementType.TYPE_PARAMETER_LIST
+import com.pinterest.ktlint.core.ast.ElementType.VAL_KEYWORD
+import com.pinterest.ktlint.core.ast.ElementType.VAR_KEYWORD
+import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
+import com.pinterest.ktlint.core.ast.isWhiteSpaceWithoutNewline
+import com.pinterest.ktlint.core.ast.nextLeaf
+import com.pinterest.ktlint.core.ast.prevLeaf
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
+
+class SpacingAroundAngleBracketsRule : Rule("spacing-around-angle-brackets") {
+    private fun String.trimBeforeLastLine() = this.substring(this.lastIndexOf('\n'))
+
+    override fun visit(
+        node: ASTNode,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        if (node.elementType.let { it == TYPE_PARAMETER_LIST || it == TYPE_ARGUMENT_LIST }) {
+            val openingBracket = node.firstChildNode
+            if (openingBracket != null) {
+                // Check for rogue spacing before an opening bracket, e.g. Map <String, Int>
+                val beforeLeftAngle = openingBracket.prevLeaf()
+                if (beforeLeftAngle?.elementType == WHITE_SPACE) {
+                    // Ignore when the whitespace is preceded by certain keywords, e.g. fun <T> func(arg: T) {}
+                    if (!typesOkWithPrecedingWhitespace.contains(beforeLeftAngle.prevLeaf()?.elementType)) {
+                        emit(beforeLeftAngle.startOffset, "Unexpected spacing before \"<\"", true)
+                        if (autoCorrect) {
+                            beforeLeftAngle.treeParent.removeChild(beforeLeftAngle)
+                        }
+                    }
+                }
+
+                // Check for rogue spacing after an opening bracket
+                val afterLeftAngle = openingBracket.nextLeaf()
+                if (afterLeftAngle?.elementType == WHITE_SPACE) {
+                    if (afterLeftAngle.isWhiteSpaceWithoutNewline()) {
+                        // when spacing does not include any new lines, e.g. Map< String, Int>
+                        emit(afterLeftAngle.startOffset, "Unexpected spacing after \"<\"", true)
+                        if (autoCorrect) {
+                            afterLeftAngle.treeParent.removeChild(afterLeftAngle)
+                        }
+                    } else {
+                        // when spacing contains at least one new line, e.g.
+                        // SomeGenericType<[whitespace]
+                        //
+                        //      String, Int, String>
+                        // gets converted to
+                        // SomeGenericType<
+                        //      String, Int, String>
+                        val newLineWithIndent = afterLeftAngle.text.trimBeforeLastLine()
+                        if (autoCorrect) {
+                            (afterLeftAngle as LeafElement).rawReplaceWithText(newLineWithIndent)
+                        }
+                    }
+                }
+            }
+
+            val closingBracket = node.lastChildNode
+            if (closingBracket != null) {
+                val beforeRightAngle = closingBracket.prevLeaf()
+                // Check for rogue spacing before a closing bracket
+                if (beforeRightAngle?.elementType == WHITE_SPACE) {
+                    if (beforeRightAngle.isWhiteSpaceWithoutNewline()) {
+                        // when spacing does not include any new lines, e.g. Map<String, Int >
+                        emit(beforeRightAngle.startOffset, "Unexpected spacing before \">\"", true)
+                        if (autoCorrect) {
+                            beforeRightAngle.treeParent.removeChild(beforeRightAngle)
+                        }
+                    } else {
+                        // when spacing contains at least one new line, e.g.
+                        // SomeGenericType<String, Int, String[whitespace]
+                        //
+                        //      >
+                        // gets converted to
+                        // SomeGenericType<String, Int, String
+                        //      >
+                        val newLineWithIndent = beforeRightAngle.text.trimBeforeLastLine()
+                        if (autoCorrect) {
+                            (beforeRightAngle as LeafElement).rawReplaceWithText(newLineWithIndent)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val typesOkWithPrecedingWhitespace = setOf(VAL_KEYWORD, VAR_KEYWORD, FUN_KEYWORD)
+    }
+}
--- ktlint-ruleset-standard/src/test/resources/spec/angle-bracket-spacing/format.kt.spec
+++
@@ -1,5 +0,0 @@
-fun main() {
-  var a: Map< Int, String> = mapOf()
-  var b: Map<Int, String > = mapOf()
-  var c: Map <Int, String> = mapOf()
-}
