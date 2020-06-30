a40a8b339d51fc7aa0552f71bfa976c948569caa
伯言 <yangbinyhbn@gmail.com>
2020-07-01 06:32:54 +0800

    Add spacing rule for UnaryExpression (#798)
    * Add spacing rule for UnaryExpression
    
    * keep sort in RuleSetProvider
    
    * add more math cases
    
    * fix unary-op-spacing with comment insert
    
    * move emit to the before of correction
    
    * move unary-op-spacing to experimental rule set

---
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/SpacingAroundUnaryOperatorRule.kt
@@ -0,0 +1,38 @@
+package com.pinterest.ktlint.ruleset.experimental
+
+import com.pinterest.ktlint.core.Rule
+import com.pinterest.ktlint.core.ast.ElementType
+import com.pinterest.ktlint.core.ast.children
+import com.pinterest.ktlint.core.ast.isPartOfComment
+import com.pinterest.ktlint.core.ast.isWhiteSpace
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+
+/**
+ * Ensures there are no spaces around unary operators
+ *
+ * @see [Kotlin Style Guide](https://kotlinlang.org/docs/reference/coding-conventions.html#horizontal-whitespace)
+ */
+class SpacingAroundUnaryOperatorRule : Rule("unary-op-spacing") {
+
+    override fun visit(
+        node: ASTNode,
+        autoCorrect: Boolean,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
+    ) {
+        if (node.elementType == ElementType.PREFIX_EXPRESSION ||
+            node.elementType == ElementType.POSTFIX_EXPRESSION
+        ) {
+            val children = node.children().toList()
+
+            // ignore: var a = + /* comment */ 1
+            if (children.any { it.isPartOfComment() }) return
+
+            val whiteSpace = children.firstOrNull { it.isWhiteSpace() } ?: return
+            emit(whiteSpace.startOffset, "Unexpected spacing in ${node.text.replace("\n", "\\n")}", true)
+
+            if (autoCorrect) {
+                node.removeChild(whiteSpace)
+            }
+        }
+    }
+}
