459625230e522d1c51daee8d07274c356d304db5
Yahor Berdnikau <egorr.berd@gmail.com>
2020-08-29 14:19:15 +0200

    Introduce opt-in features annotations.
    Two annotations:
    - feature in alpha state - this is highly experimental feature that will change in the future.
    - feature in beta state - experimental feature that may have some bugs.
    
    Signed-off-by: Yahor Berdnikau <egorr.berd@gmail.com>

---
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/api/OptInFeatures.kt
@@ -0,0 +1,17 @@
+package com.pinterest.ktlint.core.api
+
+@RequiresOptIn(
+    message = "This Ktlint feature is highly experimental, and most probably will change in the future releases.",
+    level = RequiresOptIn.Level.ERROR
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+public annotation class FeatureInAlphaState
+
+@RequiresOptIn(
+    message = "This Ktlint feature is experimental, and may change in the future releases.",
+    level = RequiresOptIn.Level.WARNING
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+public annotation class FeatureInBetaState
