df58b411512d47c0f7af04b1d8e748600401d0c3
Toshiaki Kameyama <kmymtsak@gmail.com>
2020-11-19 22:44:15 +0900

    SpacingBetweenDeclarationsWithAnnotationsRule: don't report if not between declarations

--- ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/SpacingBetweenDeclarationsWithAnnotationsRule.kt
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/SpacingBetweenDeclarationsWithAnnotationsRule.kt
@@ -1,16 +1,14 @@
 package com.pinterest.ktlint.ruleset.experimental
 
 import com.pinterest.ktlint.core.Rule
-import com.pinterest.ktlint.core.ast.ElementType.FILE
 import com.pinterest.ktlint.core.ast.ElementType.MODIFIER_LIST
-import com.pinterest.ktlint.core.ast.ElementType.WHITE_SPACE
 import com.pinterest.ktlint.core.ast.children
-import com.pinterest.ktlint.core.ast.prevSibling
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.PsiComment
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
+import org.jetbrains.kotlin.psi.KtDeclaration
+import org.jetbrains.kotlin.psi.psiUtil.getPrevSiblingIgnoringWhitespaceAndComments
 
 /**
  * @see https://youtrack.jetbrains.com/issue/KT-35106
@@ -23,20 +21,19 @@ class SpacingBetweenDeclarationsWithAnnotationsRule : Rule("spacing-between-decl
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
     ) {
         if (node.elementType == MODIFIER_LIST && node.hasAnnotationsAsChildren()) {
-            val prevSibling = node.psi.parent.node.prevSibling { it.elementType != WHITE_SPACE }
-            if (prevSibling != null &&
-                prevSibling.elementType != FILE &&
-                prevSibling !is PsiComment
-            ) {
-                if (node.psi.parent.prevSibling is PsiWhiteSpace && node.psi.parent.prevSibling.text == "\n") {
-                    emit(
-                        node.startOffset,
-                        "Declarations and declarations with annotations should have an empty space between.",
-                        true
-                    )
-                    if (autoCorrect) {
-                        (node.psi.parent.prevSibling.node as LeafPsiElement).rawReplaceWithText("\n\n")
-                    }
+            val declaration = node.psi.parent as? KtDeclaration
+            val prevDeclaration =
+                declaration?.getPrevSiblingIgnoringWhitespaceAndComments(withItself = false) as? KtDeclaration
+            val prevWhiteSpace = declaration?.prevSibling as? PsiWhiteSpace
+            if (declaration != null && prevDeclaration != null && prevWhiteSpace?.text?.count { it == '\n' } == 1) {
+                emit(
+                    node.startOffset,
+                    "Declarations and declarations with annotations should have an empty space between.",
+                    true
+                )
+                if (autoCorrect) {
+                    val indent = prevWhiteSpace.text.substringAfter('\n')
+                    (prevWhiteSpace.node as LeafPsiElement).rawReplaceWithText("\n\n$indent")
                 }
             }
         }
