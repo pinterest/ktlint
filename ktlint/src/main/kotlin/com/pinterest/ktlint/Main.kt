d79314216a2860ef6d19536e1f860fbb36ab9a39
akuleshov7 <andrewkuleshov7@gmail.com>
2020-12-03 11:21:14 +0300

    Changing option --disabled_rules to have a 'standard' mode that disables standard ruleset
    ### What's done:
    Fixing CI/CD

--- ktlint/src/main/kotlin/com/pinterest/ktlint/Main.kt
+++ ktlint/src/main/kotlin/com/pinterest/ktlint/Main.kt
@@ -151,7 +151,7 @@ class KtlintCommandLine {
         names = ["--disabled_rules"],
         description = [
             "Comma-separated list of rules to globally disable." +
-            " To disable standard ktlint rule-set use --disabled_rules=standard"
+                " To disable standard ktlint rule-set use --disabled_rules=standard"
         ]
     )
     var disabledRules: String = ""
