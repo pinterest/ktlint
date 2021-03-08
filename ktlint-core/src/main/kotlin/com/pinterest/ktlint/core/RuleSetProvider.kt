8954d4cca156688a913957e4464931bc4c476158
Yahor Berdnikau <egorr.berd@gmail.com>
2021-03-08 21:28:57 +0100

    RuleSetProvider now implements Serializable interface.
    This is needed for better Gradle plugin support.

--- ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/RuleSetProvider.kt
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/RuleSetProvider.kt
@@ -1,16 +1,18 @@
 package com.pinterest.ktlint.core
 
+import java.io.Serializable
+
 /**
  * `ktlint` uses [ServiceLoader](http://docs.oracle.com/javase/6/docs/api/java/util/ServiceLoader.html) to
  * discover all available `RuleSetProvider`s on the classpath and so each `RuleSetProvider` must be registered using
  * `META-INF/services/com.pinterest.ktlint.core.RuleSetProvider`
  * (see `ktlint-ruleset-standard/src/main/resources` for an example).
  */
-interface RuleSetProvider {
+public interface RuleSetProvider : Serializable {
 
     /**
      * This method is going to be called once for each file (which means if any of the rules have state or
      * are not thread-safe - a new RuleSet must be created).
      */
-    fun get(): RuleSet
+    public fun get(): RuleSet
 }
--- ktlint-ruleset-template/src/main/kotlin/yourpkgname/CustomRuleSetProvider.kt
+++ ktlint-ruleset-template/src/main/kotlin/yourpkgname/CustomRuleSetProvider.kt
@@ -3,7 +3,7 @@ package yourpkgname
 import com.pinterest.ktlint.core.RuleSet
 import com.pinterest.ktlint.core.RuleSetProvider
 
-class CustomRuleSetProvider : RuleSetProvider {
+public class CustomRuleSetProvider : RuleSetProvider {
 
     override fun get(): RuleSet = RuleSet("custom", NoVarRule())
 }
