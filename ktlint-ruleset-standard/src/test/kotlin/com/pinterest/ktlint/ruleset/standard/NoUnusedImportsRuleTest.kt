pinterest/ktlint

commit 3a43dbb3f546866b2f9cf7ec11bf7d0516797833

Fix unused import when import is duplicated (#1129)
* Fix unused import when import is duplicated

ktlint detects unused imports, such as:

```
import lib
import lib2 // <- ktlint unused import rule is violated

fun main() {
	lib.function();
}
```

However, when an import is duplicated -- and the duplicate import line is thus an unused import -- ktlint emits no rule violation.

E.g:

```
import lib
import lib // unused, but no violation

fun main() {
	lib.function();
```

This commit fixes that, emitting a rule violation when two imports have the same import path.

* Revert verification metadata

* fix lint

Co-authored-by: Yahor Berdnikau <egorr.berd@gmail.com>
Co-authored-by: Sha Sha Chu <shasha@pinterest.com>

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/NoUnusedImportsRule.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtPackageDirective
+import org.jetbrains.kotlin.resolve.ImportPath
 
 class NoUnusedImportsRule : Rule("no-unused-imports") {
 
@@ -70,6 +71,7 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
     private val ref = mutableSetOf<Reference>()
     private val parentExpressions = mutableSetOf<String>()
     private val imports = mutableSetOf<String>()
+    private val usedImportPaths = mutableSetOf<ImportPath>()
     private var packageName = ""
     private var rootNode: ASTNode? = null
 
@@ -99,7 +101,11 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
                 ) {
                     ref.add(Reference(text.removeBackticks(), psi.parentDotQualifiedExpression() != null))
                 } else if (type == IMPORT_DIRECTIVE) {
-                    imports += (vnode.psi as KtImportDirective).importPath!!.pathStr.removeBackticks().trim()
+                    val importPath = (vnode.psi as KtImportDirective).importPath!!
+                    if (!usedImportPaths.add(importPath)) {
+                        emit(vnode.startOffset, "Unused import", false)
+                    }
+                    imports += importPath.pathStr.removeBackticks().trim()
                 }
             }
             val directCalls = ref.filter { !it.inDotQualifiedExpression }.map { it.text }

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/NoUnusedImportsRuleTest.kt
@@ -193,6 +193,88 @@ class NoUnusedImportsRuleTest {
     }
 
     @Test
+    fun testRepeatedImport() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+
+
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(6, 1, "no-unused-imports", "Unused import")
+            )
+        )
+    }
+
+    @Test
+    fun testRepeatedImportTwice() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+
+
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                                            )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(6, 1, "no-unused-imports", "Unused import"),
+                LintError(7, 1, "no-unused-imports", "Unused import")
+
+            )
+        )
+    }
+
+    @Test
+    fun testRepeatedImportsWithDistinctIncidences() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+
+
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.many
+                import org.repository.any
+                import org.repository.none
+                import org.repository.all
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial"), many("hello"), none("goodbye")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(7, 1, "no-unused-imports", "Unused import"),
+                LintError(9, 1, "no-unused-imports", "Unused import")
+            )
+        )
+    }
+
+    @Test
     fun testFormat() {
         assertThat(
             NoUnusedImportsRule().format(

commit 29e3ef5382e6511e8728b794ac8558b050f54a23

Re-apply #1129 with fixes for regressions (#1201)
* Fix regressions introduced in #1129

- Used imports state has to be reset per-file
- Allow repeated imports to be auto-corrected

* fix lint

* Fix bad merge conflict

* more merge fixes

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/NoUnusedImportsRule.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtPackageDirective
+import org.jetbrains.kotlin.resolve.ImportPath
 
 class NoUnusedImportsRule : Rule("no-unused-imports") {
 
@@ -70,6 +71,7 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
     private val ref = mutableSetOf<Reference>()
     private val parentExpressions = mutableSetOf<String>()
     private val imports = mutableSetOf<String>()
+    private val usedImportPaths = mutableSetOf<ImportPath>()
     private var packageName = ""
     private var rootNode: ASTNode? = null
 
@@ -84,6 +86,7 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
             ref.add(Reference("*", false))
             parentExpressions.clear()
             imports.clear()
+            usedImportPaths.clear()
             node.visit { vnode ->
                 val psi = vnode.psi
                 val type = vnode.elementType
@@ -99,7 +102,15 @@ class NoUnusedImportsRule : Rule("no-unused-imports") {
                 ) {
                     ref.add(Reference(text.removeBackticks(), psi.parentDotQualifiedExpression() != null))
                 } else if (type == IMPORT_DIRECTIVE) {
-                    imports += (vnode.psi as KtImportDirective).importPath!!.pathStr.removeBackticks().trim()
+                    val importPath = (vnode.psi as KtImportDirective).importPath!!
+                    if (!usedImportPaths.add(importPath)) {
+                        emit(vnode.startOffset, "Unused import", true)
+                        if (autoCorrect) {
+                            vnode.psi.delete()
+                        }
+                    } else {
+                        imports += importPath.pathStr.removeBackticks().trim()
+                    }
                 }
             }
             val directCalls = ref.filter { !it.inDotQualifiedExpression }.map { it.text }

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/NoUnusedImportsRuleTest.kt
@@ -193,6 +193,168 @@ class NoUnusedImportsRuleTest {
     }
 
     @Test
+    fun testRepeatedImport() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(4, 1, "no-unused-imports", "Unused import")
+            )
+        )
+
+        assertThat(
+            NoUnusedImportsRule().format(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            """
+            import org.repository.RepositoryPolicy
+            import org.repository.any
+            import org.repository.all
+            fun main() {
+                    RepositoryPolicy(
+                    any(false), all("trial")
+                )
+            }
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun testRepeatedImportTwice() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                                            )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(4, 1, "no-unused-imports", "Unused import"),
+                LintError(5, 1, "no-unused-imports", "Unused import")
+            )
+        )
+
+        assertThat(
+            NoUnusedImportsRule().format(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.any
+                import org.repository.any
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial")
+                                            )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            """
+            import org.repository.RepositoryPolicy
+            import org.repository.any
+            import org.repository.all
+            fun main() {
+                    RepositoryPolicy(
+                    any(false), all("trial")
+                                        )
+            }
+            """.trimIndent()
+        )
+    }
+
+    @Test
+    fun testRepeatedImportsWithDistinctIncidences() {
+        assertThat(
+            NoUnusedImportsRule().lint(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.many
+                import org.repository.any
+                import org.repository.none
+                import org.repository.all
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial"), many("hello"), none("goodbye")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            listOf(
+                LintError(5, 1, "no-unused-imports", "Unused import"),
+                LintError(7, 1, "no-unused-imports", "Unused import")
+            )
+        )
+
+        assertThat(
+            NoUnusedImportsRule().format(
+                """
+                import org.repository.RepositoryPolicy
+                import org.repository.any
+                import org.repository.all
+                import org.repository.many
+                import org.repository.any
+                import org.repository.none
+                import org.repository.all
+                fun main() {
+                        RepositoryPolicy(
+                        any(false), all("trial"), many("hello"), none("goodbye")
+                    )
+                }
+                """.trimIndent()
+            )
+        ).isEqualTo(
+            """
+            import org.repository.RepositoryPolicy
+            import org.repository.any
+            import org.repository.all
+            import org.repository.many
+            import org.repository.none
+            fun main() {
+                    RepositoryPolicy(
+                    any(false), all("trial"), many("hello"), none("goodbye")
+                )
+            }
+            """.trimIndent()
+        )
+    }
+
+    @Test
     fun testFormat() {
         assertThat(
             NoUnusedImportsRule().format(
@@ -642,6 +804,36 @@ class NoUnusedImportsRuleTest {
 
                 class Consumer(producer1: Producer1<String>) {
                     val x = 1
+                }
+                """.trimIndent()
+            )
+        ).isEmpty()
+    }
+
+    @Test
+    fun `repeated invocations on the same rule instance should work`() {
+        val rule = NoUnusedImportsRule()
+        assertThat(
+            rule.lint(
+                """
+                import foo.bar
+
+                fun main() {
+                    bar()
+                }
+                """.trimIndent()
+            )
+        ).isEmpty()
+
+        assertThat(
+            rule.lint(
+                """
+                import foo.bar
+                import foo.baz
+
+                fun main() {
+                    bar()
+                    baz()
                 }
                 """.trimIndent()
             )