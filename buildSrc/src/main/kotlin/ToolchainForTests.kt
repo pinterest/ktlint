9b70aa709620113bf4d36a098f76d742ec8c7b0a
Goooler <wangzongler@gmail.com>
2022-02-09 16:02:59 +0800

    Declare Project.javaToolchains

--- buildSrc/src/main/kotlin/ToolchainForTests.kt
+++ buildSrc/src/main/kotlin/ToolchainForTests.kt
@@ -1,10 +1,14 @@
-import gradle.kotlin.dsl.accessors._80e422bfb44acd38519c1b2ed4303c90.javaToolchains
 import org.gradle.api.Project
+import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.register
 
 const val testsOnJDK11TaskName: String = "testOnJdk11"
+
+val Project.javaToolchains: JavaToolchainService
+    get() = (this as ExtensionAware).extensions.getByName("javaToolchains") as JavaToolchainService
 
 fun Project.addJdk11Tests() {
     val testTask = tasks.register<Test>(testsOnJDK11TaskName) {
