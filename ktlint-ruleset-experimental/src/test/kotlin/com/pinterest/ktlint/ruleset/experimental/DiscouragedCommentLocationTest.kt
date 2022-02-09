a608b205bc71474aed8687f6ba7d3272b64220f5
Paul Dingemans <pdingemans@bol.com>
2022-02-09 19:57:31 +0100

    Add rule to report discouraged comment locations
    Autocorrect is not implemented. This is an error which only
    happens rarely. Its value is that it makes implementation of
    some rules way easier when comment do not have to be taken
    into account.
    
    Initial implementation just contains one single discouraged
    location. More will follow when implementing the rule to
    rewrite the function signature automatically as is described
    in #1341.

---
+++ ktlint-ruleset-experimental/src/test/kotlin/com/pinterest/ktlint/ruleset/experimental/DiscouragedCommentLocationTest.kt
@@ -0,0 +1,92 @@
+package com.pinterest.ktlint.ruleset.experimental
+
+import com.pinterest.ktlint.core.LintError
+import com.pinterest.ktlint.test.format
+import com.pinterest.ktlint.test.lint
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class DiscouragedCommentLocationTest {
+    @Test
+    fun `Given an EOL comment after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T> // some comment
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(1, 9, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+
+    @Test
+    fun `Given an EOL comment on a newline after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T>
+            // some comment
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(2, 1, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+
+    @Test
+    fun `Given a block comment after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T> /* some comment */
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(1, 9, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+
+    @Test
+    fun `Given a block comment on a newline after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T>
+            /* some comment */
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(2, 1, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+
+    @Test
+    fun `Given a KDOC comment after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T> /** some comment */
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(1, 9, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+
+    @Test
+    fun `Given a KDOC comment on a newline after a type parameter then report a discouraged comment location`() {
+        val code =
+            """
+            fun <T>
+            /**
+              * some comment
+              */
+            foo(t: T) = "some-result"
+            """.trimIndent()
+        assertThat(DiscouragedCommentLocationRule().lint(code)).containsExactly(
+            LintError(2, 1, "discouraged-comment-location", "No comment expected at this location")
+        )
+        assertThat(DiscouragedCommentLocationRule().format(code)).isEqualTo(code)
+    }
+}
