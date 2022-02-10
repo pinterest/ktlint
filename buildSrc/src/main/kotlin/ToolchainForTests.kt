5ff9425c1f22afc612b893682044ee559a961df3
Goooler <wangzongler@gmail.com>
2022-02-10 16:11:44 +0800

    Simplify javaToolchains

--- buildSrc/src/main/kotlin/ToolchainForTests.kt
+++ buildSrc/src/main/kotlin/ToolchainForTests.kt
@@ -1,12 +1,11 @@
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
 import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.register
 
 val Project.javaToolchains: JavaToolchainService
-    get() = (this as ExtensionAware).extensions.getByName("javaToolchains") as JavaToolchainService
+    get() = extensions.getByType(JavaToolchainService::class.java)
 
 fun Project.addJdk11Tests() {
     val testTask = tasks.register<Test>("testOnJdk11") {
