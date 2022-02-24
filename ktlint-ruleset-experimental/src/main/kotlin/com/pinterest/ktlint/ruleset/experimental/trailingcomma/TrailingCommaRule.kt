93637172b741a36507f9d145940fd4cd2d1de869
Paul Dingemans <pdingemans@bol.com>
2022-02-24 09:15:49 +0100

    Cleanup commented out code

--- ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/trailingcomma/TrailingCommaRule.kt
+++ ktlint-ruleset-experimental/src/main/kotlin/com/pinterest/ktlint/ruleset/experimental/trailingcomma/TrailingCommaRule.kt
@@ -237,7 +237,7 @@ public class TrailingCommaRule :
                     prevNode.psi.parent.addAfter(comma, prevNode.psi)
                 }
             }
-            TrailingCommaState.REDUNDANT -> { //if (!isTrailingCommaAllowed) {
+            TrailingCommaState.REDUNDANT -> {
                 emit(
                     trailingCommaNode!!.startOffset,
                     "Unnecessary trailing comma before \"${inspectNode.text}\"",
