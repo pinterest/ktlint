b7d5e3bca6f229b453e79acf16ebb183899b8d6c
= <=>
2022-05-24 20:01:45 +0200

    Merge ByteArrayExt into GitHookInstaller

--- ktlint/src/main/kotlin/com/pinterest/ktlint/internal/ByteArrayExt.kt
+++
@@ -1,9 +0,0 @@
-package com.pinterest.ktlint.internal
-
-import java.math.BigInteger
-import java.security.MessageDigest
-
-/**
- * Generate hex string for given [ByteArray] content.
- */
-internal val ByteArray.hex get() = BigInteger(MessageDigest.getInstance("SHA-256").digest(this)).toString(16)
