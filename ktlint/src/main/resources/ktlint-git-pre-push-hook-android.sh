cb1f8c881c4769d935115eb28e584c6f5739569f
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-05-31 21:48:16 +0200

    Fixed all uses of xargs to be consistent on Linux/Macs

--- ktlint/src/main/resources/ktlint-git-pre-push-hook-android.sh
+++ ktlint/src/main/resources/ktlint-git-pre-push-hook-android.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # https://github.com/pinterest/ktlint pre-push hook
-git diff --name-only HEAD origin/$(git rev-parse --abbrev-ref HEAD) | grep '\.kt[s"]\?$' | xargs ktlint --android --relative
+# On Linux xargs must be told to do nothing on no input but this is the default on Macs
+[ "$(uname -s)" != "Darwin" ] && no_run=--no-run-if-empty
+git diff --name-only HEAD origin/$(git rev-parse --abbrev-ref HEAD) | grep '\.kt[s"]\?$' | xargs $no_run ktlint --android --relative
 if [ $? -ne 0 ]; then exit 1; fi
