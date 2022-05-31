e5444ed5809ec0facf5d725b4bcb80a0a0dfa34c
paul-dingemans <paul-dingemans@users.noreply.github.com>
2022-05-31 19:40:53 +0200

    Remove deprecated 'DumpAST' typealias

--- ktlint-test/src/main/kotlin/com/pinterest/ktlint/test/DumpAST.kt
+++
@@ -1,6 +0,0 @@
-package com.pinterest.ktlint.test
-
-@Deprecated(
-    message = "Moved to 'test' rulesets. This typealias will be removed in the future versions."
-)
-public typealias DumpAST = com.pinterest.ruleset.test.DumpASTRule
