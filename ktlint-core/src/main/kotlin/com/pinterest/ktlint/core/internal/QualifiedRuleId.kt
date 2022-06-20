86784ce90b567f4ddcc04907c1a36b56ed2a163b
paul-dingemans <paul-dingemans@users.noreply.github.com>
2022-06-20 20:47:51 +0200

    Improve readability and maintainability (follow-up on #1508)

--- ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/internal/QualifiedRuleId.kt
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/internal/QualifiedRuleId.kt
@@ -4,8 +4,10 @@ internal fun String.toQualifiedRuleId() =
     if (contains(":")) {
         this
     } else {
+        // When IntelliJ IDEA is reformatting the ".editorconfig" file it sometimes add a space after the comma in a
+        // comma-separate-list.
         "standard:$this"
-    }
+    }.removeSpaces()
 
 internal fun RuleReference.toQualifiedRuleId() =
     toQualifiedRuleId(ruleSetId, ruleId)
@@ -13,16 +15,23 @@ internal fun RuleReference.toQualifiedRuleId() =
 internal fun toQualifiedRuleId(
     ruleSetId: String,
     ruleId: String
-) =
-    if (ruleId.startsWith("$ruleSetId:")) {
-        ruleId
+): String {
+    if (ruleSetId.contains(" ") || ruleId.contains(" ")) {
+        return toQualifiedRuleId(ruleSetId.removeSpaces(), ruleId.removeSpaces())
+    }
+    return if (ruleSetId == "" || ruleId.startsWith("$ruleSetId:")) {
+        ruleId.toQualifiedRuleId()
     } else {
         "$ruleSetId:$ruleId"
     }
+}
 
 internal fun RuleReference.toShortenedQualifiedRuleId() =
-    if (ruleSetId == "standard") {
-        ruleId
+    if (ruleSetId.removeSpaces() == "standard" || ruleSetId.removeSpaces() == "") {
+        ruleId.removeSpaces()
     } else {
         toQualifiedRuleId()
     }
+
+private fun String.removeSpaces() =
+    this.replace(" ", "")
