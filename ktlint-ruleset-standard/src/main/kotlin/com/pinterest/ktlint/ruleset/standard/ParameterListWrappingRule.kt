62cc122da84887aeb80fed4e578554c5c319067e
paul-dingemans <paul-dingemans@users.noreply.github.com>
2022-07-16 21:50:17 +0200

    Refactor ParameterListWrappingRule to no use the "package.visit" methods

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/ParameterListWrappingRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/ParameterListWrappingRule.kt
@@ -5,6 +5,7 @@ import com.pinterest.ktlint.core.Rule
 import com.pinterest.ktlint.core.api.DefaultEditorConfigProperties.indentSizeProperty
 import com.pinterest.ktlint.core.api.DefaultEditorConfigProperties.indentStyleProperty
 import com.pinterest.ktlint.core.api.DefaultEditorConfigProperties.maxLineLengthProperty
+import com.pinterest.ktlint.core.api.EditorConfigProperties
 import com.pinterest.ktlint.core.api.UsesEditorConfigProperties
 import com.pinterest.ktlint.core.ast.ElementType.FUNCTION_LITERAL
 import com.pinterest.ktlint.core.ast.ElementType.FUNCTION_TYPE
@@ -15,7 +16,6 @@ import com.pinterest.ktlint.core.ast.ElementType.RPAR
 import com.pinterest.ktlint.core.ast.ElementType.TYPE_PARAMETER_LIST
 import com.pinterest.ktlint.core.ast.ElementType.VALUE_PARAMETER
 import com.pinterest.ktlint.core.ast.ElementType.VALUE_PARAMETER_LIST
-import com.pinterest.ktlint.core.ast.children
 import com.pinterest.ktlint.core.ast.column
 import com.pinterest.ktlint.core.ast.isWhiteSpaceWithNewline
 import com.pinterest.ktlint.core.ast.lineIndent
@@ -29,9 +29,10 @@ import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import org.jetbrains.kotlin.psi.KtTypeArgumentList
+import org.jetbrains.kotlin.psi.psiUtil.children
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 
-class ParameterListWrappingRule :
+public class ParameterListWrappingRule :
     Rule("parameter-list-wrapping"),
     UsesEditorConfigProperties {
     override val editorConfigProperties: List<UsesEditorConfigProperties.EditorConfigProperty<*>> =
@@ -44,26 +45,41 @@ class ParameterListWrappingRule :
     private var indentConfig = IndentConfig.DEFAULT_INDENT_CONFIG
     private var maxLineLength = -1
 
-    override fun visit(
+    override fun beforeFirstNode(editorConfigProperties: EditorConfigProperties) {
+        indentConfig = IndentConfig(
+            indentStyle = editorConfigProperties.getEditorConfigValue(indentStyleProperty),
+            tabWidth = editorConfigProperties.getEditorConfigValue(indentSizeProperty)
+        )
+        maxLineLength = editorConfigProperties.getEditorConfigValue(maxLineLengthProperty)
+    }
+
+    override fun beforeVisitChildNodes(
         node: ASTNode,
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit
     ) {
-        if (node.isRoot()) {
-            indentConfig = IndentConfig(
-                indentStyle = node.getEditorConfigValue(indentStyleProperty),
-                tabWidth = node.getEditorConfigValue(indentSizeProperty)
-            )
-            maxLineLength = node.getEditorConfigValue(maxLineLengthProperty)
-            return
-        }
         if (indentConfig.disabled) {
             return
         }
 
+        when (node.elementType) {
+            NULLABLE_TYPE -> wrapNullableType(node, emit, autoCorrect)
+            VALUE_PARAMETER_LIST -> {
+                if (node.needToWrapParameterList()) {
+                    wrapParameterList(node, emit, autoCorrect)
+                }
+            }
+        }
+    }
+
+    private fun wrapNullableType(
+        node: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
+        autoCorrect: Boolean
+    ) {
+        require(node.elementType == NULLABLE_TYPE)
         node
-            .takeIf { it.elementType == NULLABLE_TYPE }
-            ?.takeUnless {
+            .takeUnless {
                 // skip when max line length not set or does not exceed max line length
                 maxLineLength <= 0 || (node.column - 1 + node.textLength) <= maxLineLength
             }?.findChildByType(FUNCTION_TYPE)
@@ -94,102 +110,120 @@ class ParameterListWrappingRule :
                         }
                     }
             }
+    }
 
-        if (node.elementType == VALUE_PARAMETER_LIST &&
-            // skip when there are no parameters
-            node.firstChildNode?.treeNext?.elementType != RPAR &&
+    private fun ASTNode.needToWrapParameterList() =
+        if ( // skip when there are no parameters
+            firstChildNode?.treeNext?.elementType != RPAR &&
             // skip lambda parameters
-            node.treeParent?.elementType != FUNCTION_LITERAL &&
+            treeParent?.elementType != FUNCTION_LITERAL &&
             // skip when function type is wrapped in a nullable type [which was already when processing the nullable
             // type node itself.
-            !(node.treeParent.elementType == FUNCTION_TYPE && node.treeParent?.treeParent?.elementType == NULLABLE_TYPE)
+            !(treeParent.elementType == FUNCTION_TYPE && treeParent?.treeParent?.elementType == NULLABLE_TYPE)
         ) {
             // each parameter should be on a separate line if
             // - at least one of the parameters is
             // - maxLineLength exceeded (and separating parameters with \n would actually help)
             // in addition, "(" and ")" must be on separates line if any of the parameters are (otherwise on the same)
-            val putParametersOnSeparateLines =
-                node.textContains('\n') ||
-                    // max_line_length exceeded
-                    maxLineLength > -1 && (node.column - 1 + node.textLength) > maxLineLength && !node.textContains('\n')
-            if (putParametersOnSeparateLines) {
-                val currentIndentLevel = indentConfig.indentLevelFrom(node.lineIndent())
-                val newIndentLevel =
-                    when {
-                        // IDEA quirk:
-                        // fun <
-                        //     T,
-                        //     R> test(
-                        //     param1: T
-                        //     param2: R
-                        // )
-                        // instead of
-                        // fun <
-                        //     T,
-                        //     R> test(
-                        //         param1: T
-                        //         param2: R
-                        //     )
-                        currentIndentLevel > 0 && node.hasTypeParameterListInFront() -> currentIndentLevel - 1
+            textContains('\n') || this.exceedsMaxLineLength()
+        } else {
+            false
+        }
 
-                        else -> currentIndentLevel
-                    }
-                val indent = "\n" + indentConfig.indent.repeat(newIndentLevel)
+    private fun wrapParameterList(
+        node: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
+        autoCorrect: Boolean
+    ) {
+        val newIndentLevel = getNewIndentLevel(node)
+        node
+            .children()
+            .forEach { child -> wrapParemeterInList(newIndentLevel, child, emit, autoCorrect) }
+    }
 
-                nextChild@ for (child in node.children()) {
-                    when (child.elementType) {
-                        LPAR -> {
-                            val prevLeaf = child.prevLeaf()
-                            if (prevLeaf is PsiWhiteSpace && prevLeaf.textContains('\n')) {
-                                emit(child.startOffset, errorMessage(child), true)
-                                if (autoCorrect) {
-                                    prevLeaf.delete()
-                                }
-                            }
-                        }
-                        VALUE_PARAMETER,
-                        RPAR -> {
-                            // aiming for
-                            // ... LPAR
-                            // <line indent + indentSize> VALUE_PARAMETER...
-                            // <line indent> RPAR
-                            val intendedIndent = if (child.elementType == VALUE_PARAMETER) {
-                                indent + indentConfig.indent
-                            } else {
-                                indent
-                            }
+    private fun getNewIndentLevel(node: ASTNode): Int {
+        val currentIndentLevel = indentConfig.indentLevelFrom(node.lineIndent())
+        return when {
+            // IDEA quirk:
+            // fun <
+            //     T,
+            //     R> test(
+            //     param1: T
+            //     param2: R
+            // )
+            // instead of
+            // fun <
+            //     T,
+            //     R> test(
+            //         param1: T
+            //         param2: R
+            //     )
+            currentIndentLevel > 0 && node.hasTypeParameterListInFront() -> currentIndentLevel - 1
 
-                            val prevLeaf = child.prevLeaf()
-                            if (prevLeaf is PsiWhiteSpace) {
-                                if (prevLeaf.getText().contains("\n")) {
-                                    // The current child is already wrapped to a new line. Checking and fixing the
-                                    // correct size of the indent is the responsibility of the IndentationRule.
-                                    continue@nextChild
-                                } else {
-                                    // The current child needs to be wrapped to a newline.
-                                    emit(child.startOffset, errorMessage(child), true)
-                                    if (autoCorrect) {
-                                        // The indentation is purely based on the previous leaf only. Note that in
-                                        // autoCorrect mode the indent rule, if enabled, runs after this rule and
-                                        // determines the final indentation. But if the indent rule is disabled then the
-                                        // indent of this rule is kept.
-                                        (prevLeaf as LeafPsiElement).rawReplaceWithText(intendedIndent)
-                                    }
-                                }
-                            } else {
-                                // Insert a new whitespace element in order to wrap the current child to a new line.
-                                emit(child.startOffset, errorMessage(child), true)
-                                if (autoCorrect) {
-                                    node.addChild(PsiWhiteSpaceImpl(intendedIndent), child)
-                                }
-                            }
-                            // Indentation of child nodes need to be fixed by the IndentationRule.
-                        }
+            else -> currentIndentLevel
+        }
+    }
+
+    private fun wrapParemeterInList(
+        newIndentLevel: Int,
+        child: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
+        autoCorrect: Boolean
+    ) {
+        val indent = "\n" + indentConfig.indent.repeat(newIndentLevel)
+        when (child.elementType) {
+            LPAR -> {
+                val prevLeaf = child.prevLeaf()
+                if (prevLeaf is PsiWhiteSpace && prevLeaf.textContains('\n')) {
+                    emit(child.startOffset, errorMessage(child), true)
+                    if (autoCorrect) {
+                        prevLeaf.delete()
                     }
                 }
             }
+            VALUE_PARAMETER,
+            RPAR -> {
+                // aiming for
+                // ... LPAR
+                // <line indent + indentSize> VALUE_PARAMETER...
+                // <line indent> RPAR
+                val intendedIndent = if (child.elementType == VALUE_PARAMETER) {
+                    indent + indentConfig.indent
+                } else {
+                    indent
+                }
+
+                val prevLeaf = child.prevLeaf()
+                if (prevLeaf is PsiWhiteSpace) {
+                    if (prevLeaf.getText().contains("\n")) {
+                        // The current child is already wrapped to a new line. Checking and fixing the
+                        // correct size of the indent is the responsibility of the IndentationRule.
+                        return
+                    } else {
+                        // The current child needs to be wrapped to a newline.
+                        emit(child.startOffset, errorMessage(child), true)
+                        if (autoCorrect) {
+                            // The indentation is purely based on the previous leaf only. Note that in
+                            // autoCorrect mode the indent rule, if enabled, runs after this rule and
+                            // determines the final indentation. But if the indent rule is disabled then the
+                            // indent of this rule is kept.
+                            (prevLeaf as LeafPsiElement).rawReplaceWithText(intendedIndent)
+                        }
+                    }
+                } else {
+                    // Insert a new whitespace element in order to wrap the current child to a new line.
+                    emit(child.startOffset, errorMessage(child), true)
+                    if (autoCorrect) {
+                        child.treeParent.addChild(PsiWhiteSpaceImpl(intendedIndent), child)
+                    }
+                }
+                // Indentation of child nodes need to be fixed by the IndentationRule.
+            }
         }
     }
+
+    private fun ASTNode.exceedsMaxLineLength() =
+        maxLineLength > -1 && (column - 1 + textLength) > maxLineLength && !textContains('\n')
 
     private fun errorMessage(node: ASTNode) =
         when (node.elementType) {
