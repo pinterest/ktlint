b75db46f5c027158ba39fda483182b89b8095a1f
paul-dingemans <paul-dingemans@users.noreply.github.com>
2022-08-16 21:24:08 +0200

    Revert dev-version back 1.7.10 to see whether this resolves Windows build problem

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 // Pass '-PkotlinDev' to command line to enable kotlin-in-development version
 val kotlinVersion = if (project.hasProperty("kotlinDev")) {
     logger.warn("Enabling kotlin dev version!")
-    "1.7.20-Beta"
+    "1.7.10"
 } else {
     "1.7.10" // Keep in sync with 'gradle/libs.versions.toml and check whether dokka-gradle-plugin has been updated as well
 }
