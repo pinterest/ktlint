7b028babbe08e9d6e4f5c6e932612299473c8d09
paul-dingemans <paul-dingemans@users.noreply.github.com>
2022-08-19 21:49:08 +0200

    Reload ".editorconfig" (#1594)
    Some API Consumers keep a long-running instance of the KtLint engine alive. In case an `.editorconfig` file is changed, which was already loaded into the internal cache of the KtLint engine this change would not be taken into account by KtLint. One way to deal with this, was to clear the entire KtLint cache after each change in an `.editorconfig` file.
    
    Now, the API consumer can reload an `.editorconfig`. If the `.editorconfig` with given path is actually found in the cached, it will be replaced with the new value directly. If the file is not yet loaded in the cache, loading will be deferred until the file is actually requested again.

--- ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/internal/ThreadSafeEditorConfigCache.kt
+++ ktlint-core/src/main/kotlin/com/pinterest/ktlint/core/internal/ThreadSafeEditorConfigCache.kt
@@ -1,40 +1,88 @@
 package com.pinterest.ktlint.core.internal
 
+import com.pinterest.ktlint.core.initKtLintKLogger
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
+import mu.KotlinLogging
 import org.ec4j.core.Cache
 import org.ec4j.core.EditorConfigLoader
 import org.ec4j.core.Resource
 import org.ec4j.core.model.EditorConfig
+
+private val logger = KotlinLogging.logger {}.initKtLintKLogger()
 
 /**
  * In-memory [Cache] implementation that could be safely accessed from any [Thread].
  */
 internal class ThreadSafeEditorConfigCache : Cache {
     private val readWriteLock = ReentrantReadWriteLock()
-    private val inMemoryMap = HashMap<Resource, EditorConfig>()
+    private val inMemoryMap = HashMap<Resource, CacheValue>()
 
     /**
-     * Gets the [editorConfigFile] from the cache. If not found, then the [editorConfigLoader] is used to retrieve the
+     * Gets the [resource] from the cache. If not found, then the [editorConfigLoader] is used to retrieve the
      * '.editorconfig' properties. The result is stored in the cache.
      */
     override fun get(
-        editorConfigFile: Resource,
+        resource: Resource,
         editorConfigLoader: EditorConfigLoader,
     ): EditorConfig {
         readWriteLock.read {
-            return inMemoryMap[editorConfigFile]
+            val cachedEditConfig = inMemoryMap[resource]
+                ?.also {
+                    logger.trace { "Retrieving EditorConfig cache entry for path ${resource.path}" }
+                }?.editConfig
+            return cachedEditConfig
                 ?: readWriteLock.write {
-                    val result = editorConfigLoader.load(editorConfigFile)
-                    inMemoryMap[editorConfigFile] = result
-                    result
+                    CacheValue(resource, editorConfigLoader)
+                        .also { cacheValue ->
+                            inMemoryMap[resource] = cacheValue
+                            logger.trace { "Creating new EditorConfig cache entry for path ${resource.path}" }
+                        }.editConfig
                 }
         }
     }
 
+    /**
+     * Reloads an '.editorconfig' file given that it is currently loaded into the cache.
+     */
+    fun reloadIfExists(resource: Resource) {
+        readWriteLock.read {
+            inMemoryMap[resource]
+                ?.let { cacheValue ->
+                    readWriteLock.write {
+                        cacheValue
+                            .copy(editConfig = cacheValue.editorConfigLoader.load(resource))
+                            .let { cacheValue -> inMemoryMap[resource] = cacheValue }
+                            .also {
+                                logger.trace { "Reload EditorConfig cache entry for path ${resource.path}" }
+                            }
+                    }
+                }
+        }
+    }
+
+    /**
+     * Clears entire cache to free-up memory.
+     */
     fun clear() = readWriteLock.write {
-        inMemoryMap.clear()
+        inMemoryMap
+            .also {
+                logger.trace { "Removing ${it.size} entries from the EditorConfig cache" }
+            }.clear()
+    }
+
+    private data class CacheValue(
+        val editorConfigLoader: EditorConfigLoader,
+        val editConfig: EditorConfig,
+    ) {
+        constructor(
+            resource: Resource,
+            editorConfigLoader: EditorConfigLoader,
+        ) : this(
+            editorConfigLoader = editorConfigLoader,
+            editConfig = editorConfigLoader.load(resource),
+        )
     }
 
     internal companion object {
