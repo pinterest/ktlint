5a34e199b093f99183601a3f35149989a83a4b8b
Michael Federczuk <federczuk.michael@protonmail.com>
2022-08-24 22:11:06 +0200

    Update Git hook scripts
    * Instead of using `grep` to filter Kotlin (script) files, doing it
      directly with `git diff`
    
    * Using the `-z` option for the `git` command and
      the `--patterns-from-stdin=''` option for the `ktlint` command so that
      files with special characters are handled properly.
      This also eliminates having to use `xargs` with that
      `--no-run-if-empty` hack
    
    * Quoted the `origin/$(git rev-parse --abbref-ref HEAD)` argument, just
      in case
    
    * Removed the `if` statement at the end that checks the exit code, since
      it's unnecessary - it's basically the same as
      `if (condition) return true else return false`

--- ktlint/src/main/resources/ktlint-git-pre-push-hook.sh
+++ ktlint/src/main/resources/ktlint-git-pre-push-hook.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-# https://github.com/pinterest/ktlint pre-push hook
-# On Linux xargs must be told to do nothing on no input. On MacOS (linux distribution "Darwin") this is default behavior and the xargs flag "--no-run-if-empty" flag does not exists
-[ "$(uname -s)" != "Darwin" ] && no_run_if_empty=--no-run-if-empty
-git diff --name-only HEAD origin/$(git rev-parse --abbrev-ref HEAD) | grep '\.kt[s"]\?$' | xargs $no_run_if_empty ktlint --relative
-if [ $? -ne 0 ]; then exit 1; fi
+
+# <https://github.com/pinterest/ktlint> pre-push hook
+
+git diff --name-only -z HEAD "origin/$(git rev-parse --abbrev-ref HEAD)" -- '*.kt' '*.kts' | ktlint --relative --patterns-from-stdin=''
