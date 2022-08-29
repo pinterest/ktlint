0fe529940463c2eb51746de3f7195e1584984687
Goooler <wangzongler@gmail.com>
2022-08-29 15:11:57 +0800

    Build on JDK 18

--- buildSrc/src/main/kotlin/ToolchainForTests.kt
+++ buildSrc/src/main/kotlin/ToolchainForTests.kt
@@ -9,7 +9,7 @@ val Project.javaToolchains: JavaToolchainService
 
 fun Project.addAdditionalJdkVersionTests() {
     // Tests should be run on all supported LTS versions of the JDK. For example, see https://endoflife.date/java
-    arrayOf(8, 11, 17).forEach(::addJdkVersionTests)
+    listOf(8, 11, 17, 18).forEach(::addJdkVersionTests)
 }
 
 private fun Project.addJdkVersionTests(jdkVersion: Int) {
@@ -19,14 +19,19 @@ private fun Project.addJdkVersionTests(jdkVersion: Int) {
                 languageVersion.set(JavaLanguageVersion.of(jdkVersion))
             },
         )
-        if (jdkVersion > 16) {
+        val jvmArgs = mutableListOf<String>()
+        if (jdkVersion >= 16) {
             // https://docs.gradle.org/7.5/userguide/upgrading_version_7.html#removes_implicit_add_opens_for_test_workers
-            val jvmArgs = listOf(
+            jvmArgs += listOf(
                 "--add-opens=java.base/java.lang=ALL-UNNAMED",
                 "--add-opens=java.base/java.util=ALL-UNNAMED",
             )
-            setJvmArgs(jvmArgs)
         }
+        if (jdkVersion >= 18) {
+            // https://openjdk.org/jeps/411
+            jvmArgs += "-Djava.security.manager=allow"
+        }
+        setJvmArgs(jvmArgs)
     }
     tasks.named("check") {
         dependsOn(jdkVersionTests)
