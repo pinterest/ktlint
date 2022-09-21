1d988c6dc0637a1f1e4065cb4bcb33692feb2704
Goooler <wangzongler@gmail.com>
2022-09-21 09:52:44 +0800

    Revert changes on tests, will add it back in a followup

--- build-logic/src/main/kotlin/ToolchainForTests.kt
+++ build-logic/src/main/kotlin/ToolchainForTests.kt
@@ -3,14 +3,13 @@ import org.gradle.api.tasks.testing.Test
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.register
-import org.gradle.language.base.plugins.LifecycleBasePlugin
 
 val Project.javaToolchains: JavaToolchainService
     get() = extensions.getByType(JavaToolchainService::class.java)
 
 fun Project.addAdditionalJdkVersionTests() {
     // Tests should be run on all supported LTS versions of the JDK. For example, see https://endoflife.date/java
-    listOf(8, 11, 17).forEach(::addJdkVersionTests)
+    listOf(8, 11, 17, 18).forEach(::addJdkVersionTests)
 }
 
 private fun Project.addJdkVersionTests(jdkVersion: Int) {
@@ -20,8 +19,6 @@ private fun Project.addJdkVersionTests(jdkVersion: Int) {
                 languageVersion.set(JavaLanguageVersion.of(jdkVersion))
             },
         )
-        description = "Runs the test suite on JDK $jdkVersion"
-        group = LifecycleBasePlugin.VERIFICATION_GROUP
         val jvmArgs = mutableListOf<String>()
         if (jdkVersion >= 16) {
             // https://docs.gradle.org/7.5/userguide/upgrading_version_7.html#removes_implicit_add_opens_for_test_workers
@@ -35,10 +32,6 @@ private fun Project.addJdkVersionTests(jdkVersion: Int) {
             jvmArgs += "-Djava.security.manager=allow"
         }
         setJvmArgs(jvmArgs)
-        (tasks.getByName("test") as Test).let { testTask ->
-            classpath = testTask.classpath
-            testClassesDirs = testTask.testClassesDirs
-        }
     }
     tasks.named("check") {
         dependsOn(jdkVersionTests)
