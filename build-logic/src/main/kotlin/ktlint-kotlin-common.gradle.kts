ec953e68c788d9fd68a79176c864e66e7bd5303a
Goooler <wangzongler@gmail.com>
2023-02-14 16:17:38 +0800

    Gradle 8.0 (#1819)
    * Gradle 8.0
    
    https://docs.gradle.org/8.0/release-notes.html
    https://docs.gradle.org/8.0/userguide/upgrading_version_7.html#changes_8.0
    
    * Comment compileJava task configration
    
    * Cleanup keys
    
    * Re-generate verification-metadata

--- build-logic/src/main/kotlin/ktlint-kotlin-common.gradle.kts
+++ build-logic/src/main/kotlin/ktlint-kotlin-common.gradle.kts
@@ -15,3 +15,10 @@ tasks.withType<KotlinCompile>().configureEach {
         jvmTarget.set(JvmTarget.JVM_1_8)
     }
 }
+
+// compileJava task and compileKotlin task jvm target compatibility should be set to the same Java version.
+// For some reason, we fallback from toolchain using, see https://github.com/pinterest/ktlint/pull/1787
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+}
