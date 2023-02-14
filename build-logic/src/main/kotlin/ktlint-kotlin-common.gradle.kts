e27ceda994f4509e0a71a4aa0cc7701ad3b17da4
Goooler <wangzongler@gmail.com>
2023-02-14 10:55:25 +0800

    Comment compileJava task configration

--- build-logic/src/main/kotlin/ktlint-kotlin-common.gradle.kts
+++ build-logic/src/main/kotlin/ktlint-kotlin-common.gradle.kts
@@ -16,6 +16,8 @@ tasks.withType<KotlinCompile>().configureEach {
     }
 }
 
+// compileJava task and compileKotlin task jvm target compatibility should be set to the same Java version.
+// For some reason, we fallback from toolchain using, see https://github.com/pinterest/ktlint/pull/1787
 tasks.withType<JavaCompile>().configureEach {
     sourceCompatibility = JavaVersion.VERSION_1_8.toString()
     targetCompatibility = JavaVersion.VERSION_1_8.toString()
