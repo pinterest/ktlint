c68b2a711d6925ed11067d769770dbfe987a1b46
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-03-07 12:24:01 +0100

    Replacing a whitespace containing a newline after the return type may not result in a max-line-length violation
    In case the whitespace contains a newline than replacing it with a single space results in merging the lines to a single line. This rule allows this only when the merged lines entirely fit on a single line.
    
    Suppose that code below does not fit on a single line than this rule no longer emits a violation:
        fun foo():
            String = "some-looooooooooooooooong-string"
    
    See FunctionSignatureRule for reformatting the function like below:
        fun foo(): String =
            "some-looooooooooooooooong-string"
    
    Closes #1764

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/FunctionReturnTypeSpacingRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/FunctionReturnTypeSpacingRule.kt
@@ -5,15 +5,28 @@ import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUN
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.WHITE_SPACE
 import com.pinterest.ktlint.rule.engine.core.api.Rule
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
+import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfig
+import com.pinterest.ktlint.rule.engine.core.api.editorconfig.MAX_LINE_LENGTH_PROPERTY
+import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
 import com.pinterest.ktlint.rule.engine.core.api.nextLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
 import com.pinterest.ktlint.rule.engine.core.api.upsertWhitespaceAfterMe
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.psi.psiUtil.leaves
 
 public class FunctionReturnTypeSpacingRule :
-    StandardRule("function-return-type-spacing"),
+    StandardRule(
+        id = "function-return-type-spacing",
+        usesEditorConfigProperties = setOf(MAX_LINE_LENGTH_PROPERTY),
+    ),
     Rule.Experimental {
+    private var maxLineLength = MAX_LINE_LENGTH_PROPERTY.defaultValue
+
+    override fun beforeFirstNode(editorConfig: EditorConfig) {
+        maxLineLength = editorConfig[MAX_LINE_LENGTH_PROPERTY]
+    }
+
     override fun beforeVisitChildNodes(
         node: ASTNode,
         autoCorrect: Boolean,
@@ -57,13 +70,38 @@ public class FunctionReturnTypeSpacingRule :
             ?.takeIf { it.elementType == WHITE_SPACE }
             .let { whiteSpaceAfterColon ->
                 if (whiteSpaceAfterColon?.text != " ") {
-                    emit(node.startOffset, "Single space expected between colon and return type", true)
-                    if (autoCorrect) {
-                        node.upsertWhitespaceAfterMe(" ")
+                    // In case the whitespace contains a newline than replacing it with a single space results in merging the lines to a
+                    // single line. This rule allows this only when the merged lines entirely fit on a single line. Suppose that code below
+                    // does not fit on a single line:
+                    //    fun foo():
+                    //        String = "some-looooooooooooooooong-string"
+                    // This rule does *not* attempt to reformat the code as follows:
+                    //    fun foo(): String =
+                    //        "some-looooooooooooooooong-string"
+                    // See FunctionSignatureRule for such reformatting.
+                    val newLineLength =
+                        node.lengthUntilNewline(false) + // Length of line before but excluding the colon
+                            node.textLength + // Length of the colon itself
+                            1 + // Length of the fixed whitespace
+                            whiteSpaceAfterColon.lengthUntilNewline(true) // Length of the line after but excluding the whitespace
+                    if (newLineLength <= maxLineLength) {
+                        emit(node.startOffset, "Single space expected between colon and return type", true)
+                        if (autoCorrect) {
+                            node.upsertWhitespaceAfterMe(" ")
+                        }
                     }
                 }
             }
     }
+
+    private fun ASTNode?.lengthUntilNewline(forward: Boolean) =
+        if (this == null) {
+            0
+        } else {
+            leaves(forward = forward)
+                .takeWhile { !it.isWhiteSpaceWithNewline() }
+                .sumOf { it.textLength }
+        }
 }
 
 public val FUNCTION_RETURN_TYPE_SPACING_RULE_ID: RuleId = FunctionReturnTypeSpacingRule().ruleId
