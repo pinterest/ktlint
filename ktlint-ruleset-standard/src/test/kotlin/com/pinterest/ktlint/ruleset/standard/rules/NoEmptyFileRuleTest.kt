42a26f19ec632477b7bb60c8d681845d6f7a3990
ao0000 <ao0000dev@gmail.com>
2023-04-19 08:08:54 +0900

    Fix default setting and remove rule experimental

--- ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/NoEmptyFileRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/NoEmptyFileRuleTest.kt
@@ -1,5 +1,6 @@
 package com.pinterest.ktlint.ruleset.standard.rules
 
+import com.pinterest.ktlint.ruleset.standard.rules.NoEmptyFileRule.Companion.NO_EMPTY_FILE_PROPERTY
 import com.pinterest.ktlint.test.KtLintAssertThat.Companion.assertThatRule
 import org.junit.jupiter.api.Test
 
@@ -8,36 +9,39 @@ class NoEmptyFileRuleTest {
 
     @Test
     fun `Given not empty kotlin file then ignore the rule for this file`() {
-        val code = """
+        val code =
+            """
             package tmp
-        """.trimIndent()
-
+            """.trimIndent()
         noEmptyFileRuleAssertThat(code)
             .asFileWithPath("/some/path/Tmp.kt")
+            .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to true)
             .hasNoLintViolations()
     }
 
     @Test
     fun `Given an empty kotlin file then do a return lint error`() {
         val fileName = "Tmp.kt"
-        val code = """
+        val code =
+            """
 
-        """.trimIndent()
-
+            """.trimIndent()
         noEmptyFileRuleAssertThat(code)
             .asFileWithPath("/some/path/$fileName")
+            .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to true)
             .hasLintViolationWithoutAutoCorrect(1, 1, "File `$fileName` should not be empty")
     }
 
     @Test
     fun `Given an empty kotlin script file then do a return lint error`() {
         val fileName = "Tmp.kts"
-        val code = """
+        val code =
+            """
 
-        """.trimIndent()
-
+            """.trimIndent()
         noEmptyFileRuleAssertThat(code)
             .asFileWithPath("/some/path/$fileName")
+            .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to true)
             .hasLintViolationWithoutAutoCorrect(1, 1, "File `$fileName` should not be empty")
     }
 
