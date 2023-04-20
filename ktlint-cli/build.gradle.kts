87b29feebc0f2de17025cee9b6182a9e03a88aca
Sha Sha Chu <shasha@pinterest.com>
2023-04-20 13:57:13 -0700

    Revert "Exclude asc from list of signed files (#1952)"
    This reverts commit 4740f4ba2884d81320d29cd0a023bef8528000bb.

--- ktlint-cli/build.gradle.kts
+++ ktlint-cli/build.gradle.kts
@@ -39,6 +39,17 @@ dependencies {
     testImplementation(projects.ktlintTest)
 }
 
+tasks.register<Checksum>("shadowJarExecutableChecksum") {
+    dependsOn(tasks.signMavenPublication)
+    description = "Generates MD5 checksum for ktlint executable"
+    group = "Distribution"
+
+    inputFiles.setFrom(shadowJarExecutable.map { it.outputs.files })
+    // put the checksums in the same folder with the executable itself
+    outputDirectory.fileProvider(shadowJarExecutable.map { it.outputs.files.files.first().parentFile })
+    checksumAlgorithm.set(Checksum.Algorithm.MD5)
+}
+
 // Implements https://github.com/brianm/really-executable-jars-maven-plugin maven plugin behaviour.
 // To check details how it works, see https://skife.org/java/unix/2011/06/20/really_executable_jars.html.
 val shadowJarExecutable by tasks.registering(DefaultTask::class) {
@@ -74,25 +85,13 @@ val shadowJarExecutable by tasks.registering(DefaultTask::class) {
         execFile.appendBytes(inputs.files.singleFile.readBytes())
         execFile.setExecutable(true, false)
     }
-}
-
-tasks.register<Checksum>("shadowJarExecutableChecksum") {
-    description = "Generates MD5 checksum for ktlint executable"
-    group = "Distribution"
-
-    dependsOn(shadowJarExecutable)
-
-    inputFiles.setFrom(shadowJarExecutable.map { it.outputs.files })
-    // put the checksums in the same folder with the executable itself
-    outputDirectory.fileProvider(shadowJarExecutable.map { it.outputs.files.files.first().parentFile })
-    checksumAlgorithm.set(Checksum.Algorithm.MD5)
+    finalizedBy(tasks.named("shadowJarExecutableChecksum"))
 }
 
 tasks.signMavenPublication {
     dependsOn(shadowJarExecutable)
     if (!version.toString().endsWith("SNAPSHOT")) {
-        // Just need to sign execFile.
-        sign(shadowJarExecutable.map { it.outputs.files.first() }.get())
+        sign(*shadowJarExecutable.map { it.outputs.files.files }.get().toTypedArray())
     }
 }
 
@@ -111,3 +110,5 @@ tasks.withType<Test>().configureEach {
         systemProperty("ktlint-version", ktlintVersion)
     }
 }
+
+tasks.getByName("publishMavenPublicationToMavenCentralRepository").dependsOn(tasks.named("shadowJarExecutableChecksum"))
