2ce9573d4694b94fc3bdf96b1b1e0f9942819661
ao0000 <ao0000dev@gmail.com>
2023-04-23 10:27:31 +0900

    Fix NoEmptyFileRuleTest

--- ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/NoEmptyFileRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/NoEmptyFileRuleTest.kt
@@ -8,10 +8,13 @@ class NoEmptyFileRuleTest {
     private val noEmptyFileRuleAssertThat = assertThatRule { NoEmptyFileRule() }
 
     @Test
-    fun `Given not empty kotlin file then ignore the rule for this file`() {
+    fun `Given non-empty kotlin file then ignore the rule for this file`() {
         val code =
             """
             package tmp
+            fun main(){
+                println("Hello world")
+            }
             """.trimIndent()
         noEmptyFileRuleAssertThat(code)
             .asFileWithPath("/some/path/Tmp.kt")
@@ -21,39 +24,32 @@ class NoEmptyFileRuleTest {
 
     @Test
     fun `Given an empty kotlin file then do a return lint error`() {
-        val fileName = "Tmp.kt"
-        val code =
-            """
-
-            """.trimIndent()
+        val code = EMPTY_FILE
         noEmptyFileRuleAssertThat(code)
-            .asFileWithPath("/some/path/$fileName")
+            .asFileWithPath("/some/path/Tmp.kt")
             .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to true)
-            .hasLintViolationWithoutAutoCorrect(1, 1, "File `$fileName` should not be empty")
+            .hasLintViolationWithoutAutoCorrect(1, 1, "File `/project/some/path/Tmp.kt` should not be empty")
     }
 
     @Test
     fun `Given an empty kotlin script file then do a return lint error`() {
-        val fileName = "Tmp.kts"
-        val code =
-            """
-
-            """.trimIndent()
+        val code = EMPTY_FILE
         noEmptyFileRuleAssertThat(code)
-            .asFileWithPath("/some/path/$fileName")
+            .asFileWithPath("/some/path/Tmp.kts")
             .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to true)
-            .hasLintViolationWithoutAutoCorrect(1, 1, "File `$fileName` should not be empty")
+            .hasLintViolationWithoutAutoCorrect(1, 1, "File `/project/some/path/Tmp.kts` should not be empty")
     }
 
     @Test
-    fun testLintOff() {
-        val code =
-            """
-
-            """.trimIndent()
+    fun `Given empty kotlin file when lint disable then ignore the rule for this file`() {
+        val code = EMPTY_FILE
         noEmptyFileRuleAssertThat(code)
             .asFileWithPath("/some/path/Tmp.kt")
             .withEditorConfigOverride(NO_EMPTY_FILE_PROPERTY to false)
             .hasNoLintViolations()
+    }
+
+    private companion object {
+        private const val EMPTY_FILE = ""
     }
 }
