8e929ece0c0b63dc9c543efcefecec38aa079e04
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-05-18 19:01:04 +0200

    Refactor - Keep main element as type ASTNode and convert to psi when needed

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundColonRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundColonRule.kt
@@ -2,24 +2,22 @@ package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.ANNOTATION
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.ANNOTATION_ENTRY
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.COLON
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.EQ
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
-import com.pinterest.ktlint.rule.engine.core.api.children
 import com.pinterest.ktlint.rule.engine.core.api.isPartOf
 import com.pinterest.ktlint.rule.engine.core.api.isPartOfComment
-import com.pinterest.ktlint.rule.engine.core.api.isPartOfString
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpace
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
 import com.pinterest.ktlint.rule.engine.core.api.nextLeaf
 import com.pinterest.ktlint.rule.engine.core.api.nextSibling
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
+import com.pinterest.ktlint.rule.engine.core.api.prevSibling
 import com.pinterest.ktlint.rule.engine.core.api.upsertWhitespaceAfterMe
 import com.pinterest.ktlint.rule.engine.core.api.upsertWhitespaceBeforeMe
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import org.jetbrains.kotlin.psi.KtBlockExpression
 import org.jetbrains.kotlin.psi.KtClassOrObject
 import org.jetbrains.kotlin.psi.KtConstructor
@@ -36,32 +34,26 @@ public class SpacingAroundColonRule : StandardRule("colon-spacing") {
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
-        if (node is LeafPsiElement && node.textMatches(":") && !node.isPartOfString() && !node.isPartOfComment()) {
+        if (node.elementType == COLON) {
+            val psiParent = node.psi.parent
             if (node.isPartOf(ANNOTATION) || node.isPartOf(ANNOTATION_ENTRY)) {
                 // todo: enforce "no spacing"
                 return
             }
-            val removeSpacingBefore =
-                node.parent !is KtClassOrObject &&
-                    node.parent !is KtConstructor<*> && // constructor : this/super
-                    node.parent !is KtTypeConstraint && // where T : S
-                    node.parent?.parent !is KtTypeParameterList
             val prevLeaf = node.prevLeaf()
             if (prevLeaf != null && prevLeaf.isWhiteSpaceWithNewline()) {
                 emit(prevLeaf.startOffset, "Unexpected newline before \":\"", true)
                 if (autoCorrect) {
-                    val parent = node.parent
                     val prevNonCodeElements =
                         node
-                            .siblings(forward = false, withItself = false)
-                            .takeWhile { it.node.isWhiteSpace() || it.node.isPartOfComment() }
+                            .siblings(forward = false)
+                            .takeWhile { it.isWhiteSpace() || it.isPartOfComment() }
                             .toList()
                             .reversed()
                     when {
-                        parent is KtProperty || parent is KtNamedFunction -> {
+                        psiParent is KtProperty || psiParent is KtNamedFunction -> {
                             val equalsSignElement =
                                 node
-                                    .node
                                     .siblings(forward = true)
                                     .firstOrNull { it.elementType == EQ }
                             if (equalsSignElement != null) {
@@ -69,7 +61,7 @@ public class SpacingAroundColonRule : StandardRule("colon-spacing") {
                                     .treeNext
                                     ?.let { treeNext ->
                                         prevNonCodeElements.forEach {
-                                            parent.node.addChild(it.node, treeNext)
+                                            node.treeParent.addChild(it, treeNext)
                                         }
                                         if (treeNext.isWhiteSpace()) {
                                             equalsSignElement.treeParent.removeChild(treeNext)
@@ -79,9 +71,8 @@ public class SpacingAroundColonRule : StandardRule("colon-spacing") {
                             }
                             val blockElement =
                                 node
-                                    .siblings(forward = true, withItself = false)
+                                    .siblings(forward = true)
                                     .firstIsInstanceOrNull<KtBlockExpression>()
-                                    ?.node
                             if (blockElement != null) {
                                 val before =
                                     blockElement
@@ -89,36 +80,27 @@ public class SpacingAroundColonRule : StandardRule("colon-spacing") {
                                         .nextSibling()
                                 prevNonCodeElements
                                     .let {
-                                        if (it.first().node.isWhiteSpace()) {
-                                            blockElement.treeParent.removeChild(it.first().node)
+                                        if (it.first().isWhiteSpace()) {
+                                            blockElement.treeParent.removeChild(it.first())
                                             it.drop(1)
                                         } else {
-                                            blockElement.addChild(PsiWhiteSpaceImpl("#"), before)
                                             it
                                         }
-                                        if (it.last().node.isWhiteSpaceWithNewline()) {
-                                            blockElement.treeParent.removeChild(it.last().node)
+                                        if (it.last().isWhiteSpaceWithNewline()) {
+                                            blockElement.treeParent.removeChild(it.last())
                                             it.dropLast(1)
                                         } else {
                                             it
                                         }
                                     }.forEach {
-                                        blockElement.addChild(it.node, before)
+                                        blockElement.addChild(it, before)
                                     }
-                            }
-                            if (prevNonCodeElements.first() != prevNonCodeElements.last()) {
-//                                parent.deleteChildRange(prevNonCodeElements.first(), prevNonCodeElements.last())
-//                                parent.node.removeRange(prevNonCodeElements.first().node, prevNonCodeElements.last().node)
-                                Unit
-                            } else {
-//                                parent.node.removeChild(prevNonCodeElements.first().node)
-                                Unit
                             }
                         }
                         prevLeaf.prevLeaf()?.isPartOfComment() == true -> {
                             val nextLeaf = node.nextLeaf()
                             prevNonCodeElements.forEach {
-                                node.treeParent.addChild(it.node, nextLeaf)
+                                node.treeParent.addChild(it, nextLeaf)
                             }
                             if (nextLeaf != null && nextLeaf.isWhiteSpace()) {
                                 node.treeParent.removeChild(nextLeaf)
@@ -126,52 +108,67 @@ public class SpacingAroundColonRule : StandardRule("colon-spacing") {
                         }
                         else -> {
                             val text = prevLeaf.text
-                            if (removeSpacingBefore) {
+                            if (node.removeSpacingBefore) {
                                 prevLeaf.treeParent.removeChild(prevLeaf)
                             } else {
                                 (prevLeaf as LeafPsiElement).rawReplaceWithText(" ")
                             }
-                            (node as ASTNode).upsertWhitespaceAfterMe(text)
+                            node.upsertWhitespaceAfterMe(text)
                         }
                     }
                 }
             }
-            if (node.prevSibling is PsiWhiteSpace && removeSpacingBefore && !prevLeaf.isWhiteSpaceWithNewline()) {
+            if (node.prevSibling().isWhiteSpace() && node.removeSpacingBefore && !prevLeaf.isWhiteSpaceWithNewline()) {
                 emit(node.startOffset, "Unexpected spacing before \":\"", true)
                 if (autoCorrect) {
-                    node.prevSibling.node.treeParent.removeChild(node.prevSibling.node)
+                    node
+                        .prevSibling()
+                        ?.let { prevSibling ->
+                            prevSibling.treeParent.removeChild(prevSibling)
+                        }
                 }
             }
             val missingSpacingBefore =
-                node.prevSibling !is PsiWhiteSpace &&
+                !node.prevSibling().isWhiteSpace() &&
                     (
-                        node.parent is KtClassOrObject || node.parent is KtConstructor<*> ||
-                            node.parent is KtTypeConstraint || node.parent.parent is KtTypeParameterList
+                        psiParent is KtClassOrObject || psiParent is KtConstructor<*> ||
+                            psiParent is KtTypeConstraint || psiParent.parent is KtTypeParameterList
                         )
-            val missingSpacingAfter = node.nextSibling !is PsiWhiteSpace
+            val missingSpacingAfter = !node.nextSibling().isWhiteSpace()
             when {
                 missingSpacingBefore && missingSpacingAfter -> {
                     emit(node.startOffset, "Missing spacing around \":\"", true)
                     if (autoCorrect) {
-                        (node as ASTNode).upsertWhitespaceBeforeMe(" ")
-                        (node as ASTNode).upsertWhitespaceAfterMe(" ")
+                        node.upsertWhitespaceBeforeMe(" ")
+                        node.upsertWhitespaceAfterMe(" ")
                     }
                 }
                 missingSpacingBefore -> {
                     emit(node.startOffset, "Missing spacing before \":\"", true)
                     if (autoCorrect) {
-                        (node as ASTNode).upsertWhitespaceBeforeMe(" ")
+                        node.upsertWhitespaceBeforeMe(" ")
                     }
                 }
                 missingSpacingAfter -> {
                     emit(node.startOffset + 1, "Missing spacing after \":\"", true)
                     if (autoCorrect) {
-                        (node as ASTNode).upsertWhitespaceAfterMe(" ")
+                        node.upsertWhitespaceAfterMe(" ")
                     }
                 }
             }
         }
     }
+
+    private inline val ASTNode.removeSpacingBefore: Boolean
+        get() =
+            psi
+                .parent
+                .let { psiParent ->
+                    psiParent !is KtClassOrObject &&
+                        psiParent !is KtConstructor<*> && // constructor : this/super
+                        psiParent !is KtTypeConstraint && // where T : S
+                        psiParent?.parent !is KtTypeParameterList
+                }
 }
 
 public val SPACING_AROUND_COLON_RULE_ID: RuleId = SpacingAroundColonRule().ruleId
