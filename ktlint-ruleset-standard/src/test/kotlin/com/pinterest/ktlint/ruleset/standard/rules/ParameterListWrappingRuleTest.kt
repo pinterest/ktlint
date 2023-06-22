pinterest/ktlint

commit ec9f6972ad1a273e892a0e00bbf5c76f2c35ba15

AstNodeExtensions && EditorConfig (#2050)
* Extract method noNewLineInOpenRange as counter partner for noNewLineInClosedRange

* Add methods `childIndentOf`, `siblingIndentOf` and `parentIndentOf` to `IndentConfig` to improve readability

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ParameterListWrappingRule.kt
@@ -22,7 +22,6 @@ import com.pinterest.ktlint.rule.engine.core.api.editorconfig.MAX_LINE_LENGTH_PR
 import com.pinterest.ktlint.rule.engine.core.api.firstChildLeafOrSelf
 import com.pinterest.ktlint.rule.engine.core.api.indent
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
-import com.pinterest.ktlint.rule.engine.core.api.parent
 import com.pinterest.ktlint.rule.engine.core.api.prevCodeLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevSibling
@@ -107,13 +106,13 @@ public class ParameterListWrappingRule :
                                     true,
                                 )
                                 if (autoCorrect) {
-                                    it.upsertWhitespaceAfterMe("\n${indentConfig.indent}")
+                                    it.upsertWhitespaceAfterMe(indentConfig.childIndentOf(node))
                                 }
                             }
                             RPAR -> {
                                 emit(it.startOffset, errorMessage(it), true)
                                 if (autoCorrect) {
-                                    it.upsertWhitespaceBeforeMe("\n")
+                                    it.upsertWhitespaceBeforeMe(indentConfig.parentIndentOf(node))
                                 }
                             }
                         }
@@ -183,15 +182,13 @@ public class ParameterListWrappingRule :
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
         autoCorrect: Boolean,
     ) {
-        val newIndentLevel = getNewIndentLevel(node)
         node
             .children()
-            .forEach { child -> wrapParameterInList(newIndentLevel, child, emit, autoCorrect) }
+            .forEach { child -> wrapParameterInList(child, emit, autoCorrect) }
     }
 
-    private fun getNewIndentLevel(node: ASTNode): Int {
-        val currentIndentLevel = indentConfig.indentLevelFrom(node.indent(false))
-        return when {
+    private fun intendedIndent(child: ASTNode): String =
+        when {
             // IDEA quirk:
             // fun <
             //     T,
@@ -206,19 +203,28 @@ public class ParameterListWrappingRule :
             //         param1: T
             //         param2: R
             //     )
-            currentIndentLevel > 0 && node.hasTypeParameterListInFront() -> currentIndentLevel - 1
+            child.treeParent.hasTypeParameterListInFront() -> -1
 
-            else -> currentIndentLevel
+            else -> 0
+        }.let {
+            if (child.elementType == VALUE_PARAMETER) {
+                it + 1
+            } else {
+                it
+            }
+        }.let { indentLevelFix ->
+            val indentLevel =
+                indentConfig
+                    .indentLevelFrom(child.treeParent.indent(false))
+                    .plus(indentLevelFix)
+            "\n" + indentConfig.indent.repeat(indentLevel)
         }
-    }
 
     private fun wrapParameterInList(
-        newIndentLevel: Int,
         child: ASTNode,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
         autoCorrect: Boolean,
     ) {
-        val indent = "\n" + indentConfig.indent.repeat(newIndentLevel)
         when (child.elementType) {
             LPAR -> {
                 val prevLeaf = child.prevLeaf()
@@ -236,12 +242,7 @@ public class ParameterListWrappingRule :
                 // ... LPAR
                 // <line indent + indentSize> VALUE_PARAMETER...
                 // <line indent> RPAR
-                val intendedIndent =
-                    if (child.elementType == VALUE_PARAMETER) {
-                        indent + indentConfig.indent
-                    } else {
-                        indent
-                    }
+                val intendedIndent = intendedIndent(child)
                 val prevLeaf = child.prevLeaf()
                 if (prevLeaf is PsiWhiteSpace) {
                     if (prevLeaf.getText().contains("\n")) {

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ParameterListWrappingRuleTest.kt
@@ -517,18 +517,28 @@ class ParameterListWrappingRuleTest {
         val code =
             """
             var changesListener: ((width: Double?, depth: Double?, length: Double?, area: Double?) -> Unit)? = null
+            fun foo() {
+                var changesListener: ((width: Double?, depth: Double?, length: Double?, area: Double?) -> Unit)? = null
+            }
             """.trimIndent()
         val formattedCode =
             """
             var changesListener: (
                 (width: Double?, depth: Double?, length: Double?, area: Double?) -> Unit
             )? = null
+            fun foo() {
+                var changesListener: (
+                    (width: Double?, depth: Double?, length: Double?, area: Double?) -> Unit
+                )? = null
+            }
             """.trimIndent()
         parameterListWrappingRuleAssertThat(code)
             .withEditorConfigOverride(MAX_LINE_LENGTH_PROPERTY to 80)
             .hasLintViolations(
                 LintViolation(1, 22, "Parameter of nullable type should be on a separate line (unless the type fits on a single line)"),
                 LintViolation(1, 95, """Missing newline before ")""""),
+                LintViolation(3, 26, "Parameter of nullable type should be on a separate line (unless the type fits on a single line)"),
+                LintViolation(3, 99, """Missing newline before ")""""),
             ).isFormattedAs(formattedCode)
     }
 

ktlint-rule-engine-core/src/main/kotlin/com/pinterest/ktlint/rule/engine/core/api/ASTNodeExtension.kt
@@ -385,8 +385,19 @@ public fun noNewLineInClosedRange(
     to: ASTNode,
 ): Boolean =
     !from.isWhiteSpaceWithNewline() &&
-        leavesInOpenRange(from, to).none { it.textContains('\n') } &&
+        noNewLineInOpenRange(from, to) &&
         !to.isWhiteSpaceWithNewline()
+
+/**
+ * Verifies that no leaf contains a newline in the open range [from] - [to]. This means that the boundary nodes are excluded from the range
+ * in case they would happen to be a leaf node. In case [from] is a [CompositeElement] than the first leaf node in the sequence is the first
+ * leaf node in this [CompositeElement]. In case [to] is a [CompositeElement] than the last node in the sequence is the last leaf node prior
+ * to this [CompositeElement].
+ */
+public fun noNewLineInOpenRange(
+    from: ASTNode,
+    to: ASTNode,
+): Boolean = leavesInOpenRange(from, to).none { it.textContains('\n') }
 
 /**
  * Creates a sequence of leaf nodes in the open range [from] - [to]. This means that the boundary nodes are excluded

ktlint-rule-engine-core/src/main/kotlin/com/pinterest/ktlint/rule/engine/core/api/IndentConfig.kt
@@ -5,6 +5,7 @@ import com.pinterest.ktlint.rule.engine.core.api.IndentConfig.IndentStyle.TAB
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_SIZE_PROPERTY
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_STYLE_PROPERTY
 import org.ec4j.core.model.PropertyType
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 
 public data class IndentConfig(
     val indentStyle: IndentStyle,
@@ -78,6 +79,21 @@ public data class IndentConfig(
                 SPACE -> indentChar.toString().repeat(tabWidth)
             }
         }
+
+    /**
+     * Get the indentation including the newline character for a node at the next indent level compared to the given node.
+     */
+    public fun childIndentOf(node: ASTNode): String = node.indent().plus(indent)
+
+    /**
+     * Get the indentation including the newline character for a node at the same indent level as the given node.
+     */
+    public fun siblingIndentOf(node: ASTNode): String = parentIndentOf(node).plus(indent)
+
+    /**
+     * Get the indentation including the newline character for a node at the same indent level as the parent of the given node.
+     */
+    public fun parentIndentOf(node: ASTNode): String = node.treeParent.indent()
 
     /**
      * Converts [text] to a normalized indent. If [text] contains a new line, then only text after the last new line

ktlint-rule-engine-core/src/test/kotlin/com/pinterest/ktlint/rule/engine/core/api/ASTNodeExtensionTest.kt
@@ -53,6 +53,59 @@ class ASTNodeExtensionTest {
     }
 
     @Nested
+    inner class NoNewLineInOpenRange {
+        @Test
+        fun `Given an enum class with no whitespace leaf containing a newline between the first and last enum entry`() {
+            val code =
+                """
+                enum class Shape {
+                    FOO, FOOBAR, BAR
+                }
+                """.trimIndent()
+            val enumEntries =
+                code
+                    .transformAst(::toEnumClassBodySequence)
+                    .filter { it.elementType == ENUM_ENTRY }
+
+            val actual = noNewLineInOpenRange(enumEntries.first(), enumEntries.last())
+
+            assertThat(actual).isTrue
+        }
+
+        @Test
+        fun `Given a range of nodes not starting or ending with a whitespace leaf containing a newline but containing another whitespace leaf containing a newline`() {
+            val code =
+                """
+                enum class Shape {
+                    FOO,
+                    FOOBAR,
+                    BAR
+                }
+                """.trimIndent()
+            val enumClassBody = code.transformAst(::toEnumClassBodySequence)
+
+            val actual = noNewLineInClosedRange(enumClassBody.first(), enumClassBody.last())
+
+            assertThat(actual).isFalse
+        }
+
+        @Test
+        fun `Given a range of nodes not containing a newline inside a block comment in between`() {
+            val code =
+                """
+                enum class Shape { FOO, /*
+                newline in a block comment is ignored as it is not part of a whitespace leaf
+                */ FOOBAR, BAR }
+                """.trimIndent()
+            val enumClassBody = code.transformAst(::toEnumClassBodySequence)
+
+            val actual = noNewLineInClosedRange(enumClassBody.first(), enumClassBody.last())
+
+            assertThat(actual).isFalse
+        }
+    }
+
+    @Nested
     inner class HasNewLineInClosedRange {
         @Test
         fun `Given an enum class with no whitespace leaf containing a newline between the first and last enum entry`() {
@@ -118,7 +171,7 @@ class ASTNodeExtensionTest {
         }
 
         @Test
-        fun `Given a range of nodes not containing a newline inside a block comment in between`() {
+        fun `Given a range of nodes containing a newline inside a block comment in between`() {
             val code =
                 """
                 enum class Shape { FOO, /*

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/AnnotationRule.kt
@@ -137,11 +137,11 @@ public class AnnotationRule :
             val expectedIndent =
                 when {
                     node.elementType == ANNOTATED_EXPRESSION ->
-                        node.treeParent.indent().plus(indentConfig.indent)
+                        indentConfig.siblingIndentOf(node)
                     node.hasAnnotationBeforeConstructor() ->
-                        node.treeParent.treeParent.indent().plus(indentConfig.indent)
+                        indentConfig.siblingIndentOf(node.treeParent)
                     else ->
-                        node.treeParent.indent()
+                        indentConfig.parentIndentOf(node)
                 }
 
             node
@@ -264,7 +264,7 @@ public class AnnotationRule :
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
         autoCorrect: Boolean,
     ) {
-        val expectedIndent = node.indent().plus(indentConfig.indent)
+        val expectedIndent = indentConfig.childIndentOf(node)
 
         node
             .children()

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ArgumentListWrappingRule.kt
@@ -80,10 +80,9 @@ public class ArgumentListWrappingRule :
 
         if (node.elementType == VALUE_ARGUMENT_LIST) {
             if (needToWrapArgumentList(node)) {
-                val newIndentLevel = node.getNewIndentLevel()
                 node
                     .children()
-                    .forEach { child -> wrapArgumentInList(newIndentLevel, child, emit, autoCorrect) }
+                    .forEach { child -> wrapArgumentInList(child, emit, autoCorrect) }
             }
         }
     }
@@ -107,9 +106,8 @@ public class ArgumentListWrappingRule :
 
     private fun ASTNode.exceedsMaxLineLength() = (column - 1 + textLength) > maxLineLength && !textContains('\n')
 
-    private fun ASTNode.getNewIndentLevel(): Int {
-        val currentIndentLevel = editorConfigIndent.indentLevelFrom(indent(false))
-        return when {
+    private fun intendedIndent(child: ASTNode): String =
+        when {
             // IDEA quirk:
             // generic<
             //     T,
@@ -124,7 +122,7 @@ public class ArgumentListWrappingRule :
             //         1,
             //         2
             //     )
-            currentIndentLevel > 0 && hasTypeArgumentListInFront() -> currentIndentLevel - 1
+            child.treeParent.hasTypeArgumentListInFront() -> -1
 
             // IDEA quirk:
             // foo
@@ -138,25 +136,34 @@ public class ArgumentListWrappingRule :
             //         1,
             //         2
             //     )
-            currentIndentLevel > 0 && isPartOfDotQualifiedAssignmentExpression() -> currentIndentLevel - 1
+            child.treeParent.isPartOfDotQualifiedAssignmentExpression() -> -1
 
-            else -> currentIndentLevel
+            else -> 0
         }.let {
-            if (isOnSameLineAsControlFlowKeyword()) {
+            if (child.treeParent.isOnSameLineAsControlFlowKeyword()) {
                 it + 1
             } else {
                 it
             }
+        }.let {
+            if (child.elementType == ElementType.VALUE_ARGUMENT) {
+                it + 1
+            } else {
+                it
+            }
+        }.let { indentLevelFix ->
+            val indentLevel =
+                editorConfigIndent
+                    .indentLevelFrom(child.treeParent.indent(false))
+                    .plus(indentLevelFix)
+            "\n" + editorConfigIndent.indent.repeat(indentLevel)
         }
-    }
 
     private fun wrapArgumentInList(
-        newIndentLevel: Int,
         child: ASTNode,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
         autoCorrect: Boolean,
     ) {
-        val indent = "\n" + editorConfigIndent.indent.repeat(newIndentLevel)
         when (child.elementType) {
             ElementType.LPAR -> {
                 val prevLeaf = child.prevLeaf()
@@ -174,12 +181,7 @@ public class ArgumentListWrappingRule :
                 // ... LPAR
                 // <line indent + indentSize> VALUE_PARAMETER...
                 // <line indent> RPAR
-                val intendedIndent =
-                    if (child.elementType == ElementType.VALUE_ARGUMENT) {
-                        indent + editorConfigIndent.indent
-                    } else {
-                        indent
-                    }
+                val intendedIndent = intendedIndent(child)
                 val prevLeaf = child.prevWhiteSpaceWithNewLine() ?: child.prevLeaf()
                 if (prevLeaf is PsiWhiteSpace) {
                     if (prevLeaf.getText().contains("\n")) {

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ChainWrappingRule.kt
@@ -22,7 +22,6 @@ import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfig
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_SIZE_PROPERTY
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.INDENT_STYLE_PROPERTY
-import com.pinterest.ktlint.rule.engine.core.api.indent
 import com.pinterest.ktlint.rule.engine.core.api.isPartOfComment
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithoutNewline
@@ -90,7 +89,7 @@ public class ChainWrappingRule :
                     // <prevLeaf><node="."><spaceBeforeComment><comment><nextLeaf="\n"> to
                     // <prevLeaf><delete space if any><spaceBeforeComment><comment><nextLeaf="\n"><node="."><space if needed>
                     if (node.elementType == ELVIS) {
-                        node.upsertWhitespaceBeforeMe(node.indent().plus(indentConfig.indent))
+                        node.upsertWhitespaceBeforeMe(indentConfig.childIndentOf(node))
                         node.upsertWhitespaceAfterMe(" ")
                     } else {
                         node.treeParent.removeChild(node)

commit 2da2f9e30e7cc675da2e9839a07f5736952f5330

Do not wrap a multiline parameter list in a class property
Fix a bug, introduced in #2050, causing an internal error. In #2050 a when entry condition was simplified as it was not triggered by unit tests. The method hasTypeParameterListInFront incorrectly checked whether a multiline parameter type list was associated with a function

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ParameterListWrappingRule.kt
@@ -1,11 +1,11 @@
 package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.ElementType
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUN
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUNCTION_LITERAL
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUNCTION_TYPE
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.LPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.NULLABLE_TYPE
-import com.pinterest.ktlint.rule.engine.core.api.ElementType.PRIMARY_CONSTRUCTOR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.RPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.TYPE_PARAMETER_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_PARAMETER
@@ -26,7 +26,6 @@ import com.pinterest.ktlint.rule.engine.core.api.leavesIncludingSelf
 import com.pinterest.ktlint.rule.engine.core.api.nextLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevCodeLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
-import com.pinterest.ktlint.rule.engine.core.api.prevSibling
 import com.pinterest.ktlint.rule.engine.core.api.upsertWhitespaceAfterMe
 import com.pinterest.ktlint.rule.engine.core.api.upsertWhitespaceBeforeMe
 import com.pinterest.ktlint.ruleset.standard.StandardRule
@@ -34,9 +33,7 @@ import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
-import org.jetbrains.kotlin.psi.KtTypeArgumentList
 import org.jetbrains.kotlin.psi.psiUtil.children
-import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 import org.jetbrains.kotlin.psi.psiUtil.leaves
 
 public class ParameterListWrappingRule :
@@ -207,7 +204,7 @@ public class ParameterListWrappingRule :
             //         param1: T
             //         param2: R
             //     )
-            child.treeParent.hasTypeParameterListInFront() -> -1
+            child.treeParent.isFunWithTypeParameterListInFront() -> -1
 
             else -> 0
         }.let {
@@ -306,20 +303,13 @@ public class ParameterListWrappingRule :
             else -> throw UnsupportedOperationException()
         }
 
-    private fun ASTNode.hasTypeParameterListInFront(): Boolean {
-        val parent = this.treeParent
-        val typeParameterList =
-            if (parent.elementType == PRIMARY_CONSTRUCTOR) {
-                parent.prevSibling { it.elementType == TYPE_PARAMETER_LIST }
-            } else {
-                parent.children().firstOrNull { it.elementType == TYPE_PARAMETER_LIST }
-            }
-        val typeListNode =
-            typeParameterList
-                ?: parent.psi.collectDescendantsOfType<KtTypeArgumentList>().firstOrNull()?.node
-                ?: return false
-        return typeListNode.children().any { it.isWhiteSpaceWithNewline() }
-    }
+    private fun ASTNode.isFunWithTypeParameterListInFront() =
+        treeParent
+            .takeIf { elementType == FUN }
+            ?.findChildByType(TYPE_PARAMETER_LIST)
+            ?.children()
+            ?.any { it.isWhiteSpaceWithNewline() }
+            ?: false
 }
 
 public val PARAMETER_LIST_WRAPPING_RULE_ID: RuleId = ParameterListWrappingRule().ruleId

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ParameterListWrappingRuleTest.kt
@@ -522,6 +522,20 @@ class ParameterListWrappingRuleTest {
         parameterListWrappingRuleAssertThat(code).hasNoLintViolations()
     }
 
+    @Test
+    fun `Given a multiline type argument list in a class property`() {
+        val code =
+            """
+            data class FooBar(
+                public val fooBar: List<
+                    Foo,
+                    Bar
+                    >,
+            )
+            """.trimIndent()
+        parameterListWrappingRuleAssertThat(code).hasNoLintViolations()
+    }
+
     @Nested
     inner class `Issue 1255 - Given a variable declaration for nullable function type` {
         @Test