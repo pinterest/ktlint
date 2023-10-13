2acdeeb4be9a545d2d5a5348581183bb07a73b98
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2023-10-13 08:28:27 +0200

    Add maven relocation (#2282)
    In the `1.0.0` release, the maven coordinates of some modules were changed. To support migration to the new coordinates a special relocatation `pom.xml`s (with version `0.51.0-FINAL`) are created. Builds running on a release up until `0.50.x` that will upgrade to the `0.51.0-FINAL` release will then get a message with the new maven coordinates.
    
    Closes #2280

--- ktlint-cli/build.gradle.kts
+++ ktlint-cli/build.gradle.kts
@@ -172,3 +172,46 @@ sdkman {
     url = "https://github.com/pinterest/ktlint/releases/download/$sdkmanVersion/ktlint-$sdkmanVersion.zip"
     hashtag = "ktlint"
 }
+
+// TODO: Remove in release after ktlint 1.0.1
+publishing {
+    publications {
+        create<MavenPublication>("relocation-ktlint-cli") {
+            pom {
+                // Old artifact coordinates ktlint-cli
+                groupId = "com.pinterest"
+                artifactId = "ktlint"
+                version = "0.51.0-FINAL"
+
+                distributionManagement {
+                    relocation {
+                        // New artifact coordinates
+                        groupId.set("com.pinterest.ktlint")
+                        artifactId.set("ktlint-cli")
+                        version.set("1.0.0")
+                        message.set("groupId and artifactId have been changed")
+                    }
+                }
+            }
+        }
+    }
+}
+
+// TODO: Remove in release after ktlint 1.0.1
+signing {
+    // Uncomment following line to use gpg-agent for signing
+    // See https://docs.gradle.org/current/userguide/signing_plugin.html#sec:using_gpg_agent how to configure it
+    // useGpgCmd()
+
+    val signingKeyId = System.getenv("ORG_GRADLE_PROJECT_signingKeyId")
+    val signingKey = System.getenv("ORG_GRADLE_PROJECT_signingKey")
+    val signingPassword = System.getenv("ORG_GRADLE_PROJECT_signingKeyPassword")
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+
+    // This property allows OS package maintainers to disable signing
+    val enableSigning = providers.gradleProperty("ktlint.publication.signing.enable").orNull != "false"
+
+    sign(publishing.publications["relocation-ktlint-cli"])
+
+    isRequired = enableSigning && !version.toString().endsWith("SNAPSHOT")
+}
