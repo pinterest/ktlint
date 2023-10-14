24d2f8c168f242d9a1ad3f49aab2169c19371c67
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2023-10-14 15:00:32 +0200

    Delete blank line just to retrigger new snapshot build (#2315)

--- ktlint-cli/build.gradle.kts
+++ ktlint-cli/build.gradle.kts
@@ -241,7 +241,6 @@ signing {
     // Uncomment following line to use gpg-agent for signing
     // See https://docs.gradle.org/current/userguide/signing_plugin.html#sec:using_gpg_agent how to configure it
     // useGpgCmd()
-
     val signingKeyId = System.getenv("ORG_GRADLE_PROJECT_signingKeyId")
     val signingKey = System.getenv("ORG_GRADLE_PROJECT_signingKey")
     val signingPassword = System.getenv("ORG_GRADLE_PROJECT_signingKeyPassword")
