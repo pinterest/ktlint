2d4251f12d1fe39e33cb77da27fec8252165f514
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-10-14 14:20:49 +0200

    Add metadata and repository information to relocations poms

--- ktlint-cli-reporter-json/build.gradle.kts
+++ ktlint-cli-reporter-json/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     id("ktlint-publication-library")
 }
@@ -18,12 +20,50 @@ publishing {
                 artifactId = "ktlint-reporter-json"
                 version = "0.51.0-FINAL"
 
+                name = artifactId
+                description = providers.gradleProperty("POM_DESCRIPTION")
+                url = providers.gradleProperty("POM_URL")
+                licenses {
+                    license {
+                        name = providers.gradleProperty("POM_LICENSE_NAME")
+                        url = providers.gradleProperty("POM_LICENSE_URL")
+                        distribution = "repo"
+                    }
+                }
+                developers {
+                    developer {
+                        id = providers.gradleProperty("POM_DEVELOPER_ID")
+                        name = providers.gradleProperty("POM_DEVELOPER_NAME")
+                    }
+                }
+                scm {
+                    url = providers.gradleProperty("POM_SCM_URL")
+                    connection = providers.gradleProperty("POM_SCM_CONNECTION")
+                    developerConnection = providers.gradleProperty("POM_SCM_DEV_CONNECTION")
+                }
+
                 distributionManagement {
                     relocation {
                         // New artifact coordinates
                         artifactId.set("ktlint-cli-reporter-json")
                         version.set("1.0.0")
                         message.set("artifactId has been changed")
+                    }
+                }
+
+                repositories {
+                    maven {
+                        val releasesRepoUrl = URI.create("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                        val snapshotsRepoUrl = URI.create("https://oss.sonatype.org/content/repositories/snapshots/")
+                        name = "mavenCentral"
+                        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+                        credentials {
+                            username = providers.gradleProperty("SONATYPE_NEXUS_USERNAME").orNull
+                                ?: System.getenv("SONATYPE_NEXUS_USERNAME")
+                            password = providers.gradleProperty("SONATYPE_NEXUS_PASSWORD").orNull
+                                ?: System.getenv("SONATYPE_NEXUS_PASSWORD")
+                        }
                     }
                 }
             }
@@ -42,10 +82,7 @@ signing {
     val signingPassword = System.getenv("ORG_GRADLE_PROJECT_signingKeyPassword")
     useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
 
-    // This property allows OS package maintainers to disable signing
-    val enableSigning = providers.gradleProperty("ktlint.publication.signing.enable").orNull != "false"
-
     sign(publishing.publications["relocation-ktlint-cli-reporter-json"])
 
-    isRequired = enableSigning && !version.toString().endsWith("SNAPSHOT")
+    isRequired = true
 }
