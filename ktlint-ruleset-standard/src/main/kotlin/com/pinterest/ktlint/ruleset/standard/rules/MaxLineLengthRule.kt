0d2fd6b67a8af065fd7fba7be9f8de36fc8a3dfe
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-12-03 14:54:12 +0100

    Simplify `max-line-length` implementation

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/MaxLineLengthRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/MaxLineLengthRule.kt
@@ -1,6 +1,7 @@
 package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.ElementType
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.IDENTIFIER
 import com.pinterest.ktlint.rule.engine.core.api.Rule.VisitorModifier.RunAfterRule.Mode.REGARDLESS_WHETHER_RUN_AFTER_RULE_IS_LOADED_OR_DISABLED
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint
@@ -10,15 +11,16 @@ import com.pinterest.ktlint.rule.engine.core.api.editorconfig.EditorConfigProper
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.MAX_LINE_LENGTH_PROPERTY
 import com.pinterest.ktlint.rule.engine.core.api.editorconfig.MAX_LINE_LENGTH_PROPERTY_OFF
 import com.pinterest.ktlint.rule.engine.core.api.isPartOf
-import com.pinterest.ktlint.rule.engine.core.api.isRoot
+import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpace
+import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
+import com.pinterest.ktlint.rule.engine.core.api.leavesOnLine
 import com.pinterest.ktlint.rule.engine.core.api.nextLeaf
 import com.pinterest.ktlint.rule.engine.core.api.parent
-import com.pinterest.ktlint.rule.engine.core.api.prevCodeSibling
+import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.ec4j.core.model.PropertyType
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiComment
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.kdoc.psi.api.KDoc
 import org.jetbrains.kotlin.psi.KtImportDirective
@@ -53,7 +55,6 @@ public class MaxLineLengthRule :
             ),
     ) {
     private var maxLineLength: Int = MAX_LINE_LENGTH_PROPERTY.defaultValue
-    private var rangeTree = RangeTree()
     private var ignoreBackTickedIdentifier = IGNORE_BACKTICKED_IDENTIFIER_PROPERTY.defaultValue
 
     override fun beforeFirstNode(editorConfig: EditorConfig) {
@@ -69,48 +70,53 @@ public class MaxLineLengthRule :
         autoCorrect: Boolean,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> Unit,
     ) {
-        if (node.isRoot()) {
-            val errorOffset = arrayListOf<Int>()
-            node
-                .getElementsPerLine()
-                .filter { it.lineLength(ignoreBackTickedIdentifier) > maxLineLength }
-                .forEach { parsedLine ->
-                    val el = parsedLine.elements.last()
-                    if (!el.isPartOf(KDoc::class) && !el.isPartOfRawMultiLineString()) {
-                        if (!el.isPartOf(PsiComment::class)) {
-                            if (!el.isPartOf(KtPackageDirective::class) && !el.isPartOf(KtImportDirective::class)) {
-                                // fixme:
-                                // normally we would emit here but due to API limitations we need to hold off until
-                                // node spanning the same offset is 'visit'ed
-                                // this will be rectified in the upcoming release(s)
-                                errorOffset.add(parsedLine.offset)
-                            }
-                        } else {
-                            // if comment is the only thing on the line - fine, otherwise emit an error
-                            val prevLeaf = el.prevCodeSibling()
-                            if (prevLeaf != null && prevLeaf.startOffset >= parsedLine.offset) {
-                                // fixme:
-                                // normally we would emit here but due to API limitations we need to hold off until
-                                // node spanning the same offset is 'visit'ed
-                                // this will be rectified in the upcoming release(s)
-                                errorOffset.add(parsedLine.offset)
-                            }
-                        }
+        if (node.isWhiteSpace()) {
+            return
+        }
+        node
+            .takeIf { it is LeafPsiElement }
+            ?.takeIf { it.nextLeaf() == null || it.nextLeaf().isWhiteSpaceWithNewline() }
+            ?.takeIf { it.lineLength() > maxLineLength }
+            ?.takeUnless { it.isPartOf(KtPackageDirective::class) }
+            ?.takeUnless { it.isPartOf(KtImportDirective::class) }
+            ?.takeUnless { it.isPartOf(KDoc::class) }
+            ?.takeUnless { it.isPartOfRawMultiLineString() }
+            ?.takeUnless { it.isLineOnlyContainingComment() }
+            ?.let { lastNodeOnLine ->
+                emit(
+                    lastNodeOnLine.startOffset + lastNodeOnLine.textLength - 1,
+                    "Exceeded max line length ($maxLineLength)",
+                    false,
+                )
+            }
+    }
+
+    private fun ASTNode.lineLength() =
+        leavesOnLine()
+            .sumOf {
+                when {
+                    it.isWhiteSpaceWithNewline() -> {
+                        it.text.substringAfterLast('\n').length
+                    }
+
+                    it.elementType == IDENTIFIER &&
+                        it.text.matches(BACKTICKED_IDENTIFIER_REGEX) &&
+                        ignoreBackTickedIdentifier
+                    -> {
+                        0
+                    }
+
+                    else -> {
+                        it.textLength
                     }
                 }
-            rangeTree = RangeTree(errorOffset)
-        } else if (!rangeTree.isEmpty() && node.psi is LeafPsiElement) {
-            rangeTree
-                .query(node.startOffset, node.startOffset + node.textLength)
-                .forEach { offset ->
-                    emit(offset, "Exceeded max line length ($maxLineLength)", false)
-                }
-        }
-    }
+            }
 
     private fun ASTNode.isPartOfRawMultiLineString() =
         parent(ElementType.STRING_TEMPLATE, strict = false)
             ?.let { it.firstChildNode.text == "\"\"\"" && it.textContains('\n') } == true
+
+    private fun ASTNode.isLineOnlyContainingComment() = isPartOf(PsiComment::class) && prevLeaf().isWhiteSpaceWithNewline()
 
     public companion object {
         public val IGNORE_BACKTICKED_IDENTIFIER_PROPERTY: EditorConfigProperty<Boolean> =
@@ -124,138 +130,7 @@ public class MaxLineLengthRule :
                     ),
                 defaultValue = false,
             )
-    }
-}
-
-private fun ASTNode.getElementsPerLine(): List<ParsedLine> {
-    val parsedLines = mutableListOf<ParsedLine>()
-    val lines = text.split("\n")
-    var offset = 0
-    for (line in lines) {
-        val elements = mutableListOf<ASTNode>()
-        var el = psi.findElementAt(offset)?.node
-        while (el != null && el.startOffset < offset + line.length) {
-            elements.add(el)
-            el = el.nextLeaf()
-        }
-        parsedLines.add(ParsedLine(line, offset, elements))
-        offset += line.length + 1 // +1 for the newline which is stripped due to the splitting of the lines
-    }
-    return parsedLines
-}
-
-private data class ParsedLine(
-    val line: String,
-    val offset: Int,
-    val elements: List<ASTNode>,
-) {
-    fun lineLength(ignoreBackTickedIdentifier: Boolean): Int =
-        if (ignoreBackTickedIdentifier) {
-            line.length - totalLengthBacktickedElements()
-        } else {
-            line.length
-        }
-
-    private fun totalLengthBacktickedElements(): Int =
-        elements
-            .filterIsInstance(PsiElement::class.java)
-            .filter { it.text.matches(BACKTICKED_IDENTIFIER_REGEX) }
-            .sumOf(PsiElement::getTextLength)
-
-    private companion object {
-        val BACKTICKED_IDENTIFIER_REGEX = Regex("`.*`")
-    }
-}
-
-internal class RangeTree(
-    seq: List<Int> = emptyList(),
-) {
-    private var emptyArrayView = ArrayView(0, 0)
-    private var arr: IntArray = seq.toIntArray()
-
-    init {
-        if (arr.isNotEmpty()) {
-            arr.reduce { p, n ->
-                require(p <= n) { "Input must be sorted" }
-                n
-            }
-        }
-    }
-
-    // runtime: O(log(n)+k), where k is number of matching points
-    // space: O(1)
-    fun query(
-        vmin: Int,
-        vmax: Int,
-    ): ArrayView {
-        var r = arr.size - 1
-        if (r == -1 || vmax < arr[0] || arr[r] < vmin) {
-            return emptyArrayView
-        }
-        // binary search for min(arr[l] >= vmin)
-        var l = 0
-        while (l < r) {
-            val m = (r + l) / 2
-            if (vmax < arr[m]) {
-                r = m - 1
-            } else if (arr[m] < vmin) {
-                l = m + 1
-            } else {
-                // arr[l] ?<=? vmin <= arr[m] <= vmax ?<=? arr[r]
-                if (vmin <= arr[l]) break else l++ // optimization
-                r = m
-            }
-        }
-        if (l > r || arr[l] < vmin) {
-            return emptyArrayView
-        }
-        // find max(k) such as arr[k] < vmax
-        var k = l
-        while (k < arr.size) {
-            if (arr[k] >= vmax) {
-                break
-            }
-            k++
-        }
-        return ArrayView(l, k)
-    }
-
-    fun isEmpty(): Boolean = arr.isEmpty()
-
-    inner class ArrayView(
-        private var l: Int,
-        private val r: Int,
-    ) {
-        val size: Int = r - l
-
-        fun get(i: Int): Int {
-            if (i < 0 || i >= size) {
-                throw IndexOutOfBoundsException()
-            }
-            return arr[l + i]
-        }
-
-        inline fun forEach(cb: (v: Int) -> Unit) {
-            var i = 0
-            while (i < size) {
-                cb(get(i++))
-            }
-        }
-
-        override fun toString(): String {
-            if (l == r) {
-                return "[]"
-            }
-            val sb = StringBuilder("[")
-            var i = l
-            while (i < r) {
-                sb.append(arr[i]).append(", ")
-                i++
-            }
-            sb.replace(sb.length - 2, sb.length, "")
-            sb.append("]")
-            return sb.toString()
-        }
+        private val BACKTICKED_IDENTIFIER_REGEX = Regex("`.*`")
     }
 }
 
