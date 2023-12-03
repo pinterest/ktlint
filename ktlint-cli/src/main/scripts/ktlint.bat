c3c05480c15a4c91b102fb0e206d4419a5b9d130
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-12-03 11:56:04 +0100

    Fix path to ktlint JAR file in `ktlint.bat`
    Closes #2402

--- ktlint-cli/src/main/scripts/ktlint.bat
+++ ktlint-cli/src/main/scripts/ktlint.bat
@@ -1,7 +1,8 @@
 @echo off
-REM By default this batch file and the "ktlint.jar" are located in same directory. Please adjust path below when this batch
-REM file and the jar file are located in different directories.
-if not defined JAR_PATH set JAR_PATH=.\ktlint.jar
+REM By default this batch file and the "ktlint" are located in same directory. Note that "ktlint" is a JAR
+REM despite it is not being suffixed with extension ".jar". Please adjust path below when this batch file
+REM and the jar file are located in different directories.
+if not defined JAR_PATH set JAR_PATH=.\ktlint
 
 REM The --add-opens argument is needed for java 16+ (see https://github.com/pinterest/ktlint/issues/1986)
 java --add-opens=java.base/java.lang=ALL-UNNAMED -jar "%JAR_PATH%" %*
