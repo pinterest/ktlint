95183eb7c09656c3f3101ccac47bd7259e88cbe6
paul-dingemans <paul-dingemans@users.noreply.github.com>
2023-12-11 14:19:11 +0100

    Align name of test class with production code

--- ktlint-rule-engine/src/test/kotlin/com/pinterest/ktlint/rule/engine/api/KtlintRuleEngineSuppressionKtTest.kt
+++ ktlint-rule-engine/src/test/kotlin/com/pinterest/ktlint/rule/engine/api/KtlintRuleEngineSuppressionKtTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 
-class KtlintSuppressionInserterKtTest {
+class KtlintRuleEngineSuppressionKtTest {
     private val ktLintRuleEngine =
         KtLintRuleEngine(
             ruleProviders = setOf(RuleProvider { SomeRule() }),
