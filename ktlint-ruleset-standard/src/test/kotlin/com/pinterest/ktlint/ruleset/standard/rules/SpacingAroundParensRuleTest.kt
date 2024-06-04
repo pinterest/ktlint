pinterest/ktlint

commit 0502a2bcabb78d9bbba6b9f73e9d41678e8eccf2

Fix unwanted whitespace between super class constructor and its argument list (#2630)
Co-authored-by: Paul Dingemans <paul-dingemans@users.noreply.github.com>

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundParensRule.kt
@@ -9,6 +9,7 @@ import com.pinterest.ktlint.rule.engine.core.api.ElementType.LPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.PRIMARY_CONSTRUCTOR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.RPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.SUPER_KEYWORD
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.TYPE_ARGUMENT_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_ARGUMENT_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_PARAMETER_LIST
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
@@ -46,7 +47,8 @@ public class SpacingAroundParensRule : StandardRule("paren-spacing") {
                                 node.treeParent?.treeParent?.elementType != FUNCTION_TYPE ||
                                 // Super keyword needs special-casing
                                 prevLeaf.prevLeaf()?.elementType == SUPER_KEYWORD ||
-                                prevLeaf.prevLeaf()?.treeParent?.elementType == PRIMARY_CONSTRUCTOR
+                                prevLeaf.prevLeaf()?.treeParent?.elementType == PRIMARY_CONSTRUCTOR ||
+                                prevLeaf.prevLeaf()?.treeParent?.elementType == TYPE_ARGUMENT_LIST
                         ) &&
                         (
                             node.treeParent?.elementType == VALUE_PARAMETER_LIST ||

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundParensRuleTest.kt
@@ -29,6 +29,40 @@ class SpacingAroundParensRuleTest {
     }
 
     @Test
+    fun `Given class with superclass constructor call`() {
+        val code =
+            """
+            open class Bar(param: String)
+            class Foo : Bar ("test")
+            """.trimIndent()
+        val formattedCode =
+            """
+            open class Bar(param: String)
+            class Foo : Bar("test")
+            """.trimIndent()
+        spacingAroundParensRuleAssertThat(code)
+            .hasLintViolation(2, 16, "Unexpected spacing before \"(\"")
+            .isFormattedAs(formattedCode)
+    }
+
+    @Test
+    fun `Given class with superclass constructor call with type parameter`() {
+        val code =
+            """
+            open class Bar<T>(param: T)
+            class Foo : Bar<String> ("test")
+            """.trimIndent()
+        val formattedCode =
+            """
+            open class Bar<T>(param: T)
+            class Foo : Bar<String>("test")
+            """.trimIndent()
+        spacingAroundParensRuleAssertThat(code)
+            .hasLintViolation(2, 24, "Unexpected spacing before \"(\"")
+            .isFormattedAs(formattedCode)
+    }
+
+    @Test
     fun `Given a variable declaration with unexpected spacing around the opening parenthesis of the expression`() {
         val code =
             """

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ClassSignatureRule.kt
@@ -584,6 +584,21 @@ public class ClassSignatureRule :
                 }
         }
 
+        // Disallow:
+        //    class Foo : Bar<String> ("foobar")
+        superTypes
+            .filter { it.elementType == SUPER_TYPE_CALL_ENTRY }
+            .forEach { superTypeCallEntry ->
+                superTypeCallEntry
+                    .findChildByType(WHITE_SPACE)
+                    ?.let { whitespace ->
+                        emit(whitespace.startOffset, "No whitespace expected", true)
+                        if (autoCorrect) {
+                            whitespace.treeParent.removeChild(whitespace)
+                        }
+                    }
+            }
+
         return whiteSpaceCorrection
     }
 

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/ClassSignatureRuleTest.kt
@@ -1734,6 +1734,55 @@ class ClassSignatureRuleTest {
             .hasNoLintViolations()
     }
 
+    @Nested
+    inner class `Issue 2630 - White space in super type call entry` {
+        @Test
+        fun `Issue 2630 - Given a super type call entry without type argument list`() {
+            val code =
+                """
+                class Foo1 : Bar("foobar")
+                class Foo2 : Bar ("foobar")
+                class Foo3 : Bar
+                    ("foobar")
+                """.trimIndent()
+            val formattedCode =
+                """
+                class Foo1 : Bar("foobar")
+                class Foo2 : Bar("foobar")
+                class Foo3 : Bar("foobar")
+                """.trimIndent()
+            classSignatureWrappingRuleAssertThat(code)
+                .hasLintViolations(
+                    LintViolation(2, 17, "No whitespace expected"),
+                    LintViolation(3, 14, "Super type should start on a newline"),
+                    LintViolation(3, 17, "No whitespace expected"),
+                ).isFormattedAs(formattedCode)
+        }
+
+        @Test
+        fun `Issue 2630 - Given a super type call entry with type argument list`() {
+            val code =
+                """
+                class Foo1 : Bar<String>("foobar")
+                class Foo2 : Bar<String> ("foobar")
+                class Foo3 : Bar<String>
+                    ("foobar")
+                """.trimIndent()
+            val formattedCode =
+                """
+                class Foo1 : Bar<String>("foobar")
+                class Foo2 : Bar<String>("foobar")
+                class Foo3 : Bar<String>("foobar")
+                """.trimIndent()
+            classSignatureWrappingRuleAssertThat(code)
+                .hasLintViolations(
+                    LintViolation(2, 25, "No whitespace expected"),
+                    LintViolation(3, 14, "Super type should start on a newline"),
+                    LintViolation(3, 25, "No whitespace expected"),
+                ).isFormattedAs(formattedCode)
+        }
+    }
+
     private companion object {
         const val UNEXPECTED_SPACES = "  "
         const val NO_SPACE = ""

commit 055892d0d8089bbc9cf842e8e193ad240ed89329

Do not remove the space before a function type inside a type projection
Fixes regression bug introduced by #2630

ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundParensRule.kt
@@ -2,6 +2,7 @@ package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.BLOCK_COMMENT
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.CONSTRUCTOR_CALLEE
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.EOL_COMMENT
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUNCTION_TYPE
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.IDENTIFIER
@@ -10,6 +11,7 @@ import com.pinterest.ktlint.rule.engine.core.api.ElementType.LPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.PRIMARY_CONSTRUCTOR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.RPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.SUPER_KEYWORD
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.SUPER_TYPE_CALL_ENTRY
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.TYPE_ARGUMENT_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_ARGUMENT_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_PARAMETER_LIST
@@ -20,6 +22,7 @@ import com.pinterest.ktlint.rule.engine.core.api.ifAutocorrectAllowed
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithoutNewline
 import com.pinterest.ktlint.rule.engine.core.api.nextLeaf
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
+import com.pinterest.ktlint.rule.engine.core.api.prevSibling
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiWhiteSpace
@@ -49,7 +52,14 @@ public class SpacingAroundParensRule : StandardRule("paren-spacing") {
                                 // Super keyword needs special-casing
                                 prevLeaf.prevLeaf()?.elementType == SUPER_KEYWORD ||
                                 prevLeaf.prevLeaf()?.treeParent?.elementType == PRIMARY_CONSTRUCTOR ||
-                                prevLeaf.prevLeaf()?.treeParent?.elementType == TYPE_ARGUMENT_LIST
+                                (
+                                    // Disallow:
+                                    //     class Foo : Bar ("test")
+                                    //     class Foo : Bar<String> ("test")
+                                    node.treeParent.treeParent.elementType == SUPER_TYPE_CALL_ENTRY &&
+                                        prevLeaf.prevSibling()?.elementType == CONSTRUCTOR_CALLEE &&
+                                        prevLeaf.prevLeaf()?.treeParent?.elementType == TYPE_ARGUMENT_LIST
+                                )
                         ) &&
                         (
                             node.treeParent?.elementType == VALUE_PARAMETER_LIST ||

ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingAroundParensRuleTest.kt
@@ -63,6 +63,15 @@ class SpacingAroundParensRuleTest {
     }
 
     @Test
+    fun `Given a function type inside a type projection then do not remove space before the opening parenthesis`() {
+        val code =
+            """
+            val foo: Map<Foo, (Foo) -> Foo> = emptyMap()
+            """.trimIndent()
+        spacingAroundParensRuleAssertThat(code).hasNoLintViolations()
+    }
+
+    @Test
     fun `Given a variable declaration with unexpected spacing around the opening parenthesis of the expression`() {
         val code =
             """