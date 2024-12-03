9cf206980cde087af8cee43e190a7d9e5da01d32
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2024-12-03 12:14:12 +0100

    Fix false positive when empty parameter list is in between trailing lambda's of a nested call expression
    Closes #2884

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/UnnecessaryParenthesesBeforeTrailingLambdaRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/UnnecessaryParenthesesBeforeTrailingLambdaRule.kt
@@ -2,7 +2,9 @@ package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.CALL_EXPRESSION
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.FUNCTION_LITERAL
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.LAMBDA_ARGUMENT
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.LAMBDA_EXPRESSION
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.LPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.RPAR
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.VALUE_ARGUMENT_LIST
@@ -14,6 +16,7 @@ import com.pinterest.ktlint.rule.engine.core.api.children
 import com.pinterest.ktlint.rule.engine.core.api.ifAutocorrectAllowed
 import com.pinterest.ktlint.rule.engine.core.api.isPartOf
 import com.pinterest.ktlint.rule.engine.core.api.nextCodeSibling
+import com.pinterest.ktlint.rule.engine.core.api.prevCodeSibling
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 
@@ -27,8 +30,9 @@ public class UnnecessaryParenthesesBeforeTrailingLambdaRule : StandardRule("unne
         node: ASTNode,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision,
     ) {
-        if (node.isPartOf(CALL_EXPRESSION) &&
-            node.isEmptyArgumentList() &&
+        if (node.isEmptyArgumentList() &&
+            node.isPartOf(CALL_EXPRESSION) &&
+            node.isNotPrecededByCallExpressionEndingWithLambdaArgument() &&
             node.nextCodeSibling()?.elementType == LAMBDA_ARGUMENT
         ) {
             emit(
@@ -46,6 +50,17 @@ public class UnnecessaryParenthesesBeforeTrailingLambdaRule : StandardRule("unne
             children()
                 .filterNot { it.elementType == LPAR || it.elementType == RPAR }
                 .none()
+
+    private fun ASTNode.isNotPrecededByCallExpressionEndingWithLambdaArgument() =
+        prevCodeSibling()
+            ?.takeIf { it.elementType == CALL_EXPRESSION }
+            ?.lastChildNode
+            ?.takeIf { it.elementType == LAMBDA_ARGUMENT }
+            ?.lastChildNode
+            ?.takeIf { it.elementType == LAMBDA_EXPRESSION }
+            ?.lastChildNode
+            ?.let { it.elementType != FUNCTION_LITERAL }
+            ?: true
 }
 
 public val UNNECESSARY_PARENTHESES_BEFORE_TRAILING_LAMBDA_RULE_ID: RuleId = UnnecessaryParenthesesBeforeTrailingLambdaRule().ruleId
