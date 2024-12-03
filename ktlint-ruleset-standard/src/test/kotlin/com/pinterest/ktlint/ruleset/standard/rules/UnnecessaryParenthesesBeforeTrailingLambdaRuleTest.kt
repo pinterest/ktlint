1bc924cb8830e75fcc267300fe1ff51cf892b863
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2024-12-03 14:23:59 +0100

    Fix false positive when empty parameter list is in between trailing lambda's of a nested call expression (#2891)
    Closes #2884

--- ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/UnnecessaryParenthesesBeforeTrailingLambdaRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/com/pinterest/ktlint/ruleset/standard/rules/UnnecessaryParenthesesBeforeTrailingLambdaRuleTest.kt
@@ -23,4 +23,15 @@ class UnnecessaryParenthesesBeforeTrailingLambdaRuleTest {
             .hasLintViolation(2, 24, "Empty parentheses in function call followed by lambda are unnecessary")
             .isFormattedAs(formattedCode)
     }
+
+    @Test
+    fun `Issue 2884 - Given some a call expression ending with a lambda argument, followed by an empty argument list followed by another lambda argument then do not remove empty parameter list`() {
+        val code =
+            """
+            fun fooBar(foo: () -> String): (() -> String) -> String = { bar -> foo().plus("  ").plus(bar()) }
+
+            val foobar = fooBar { "Hello" }() { "world" }
+            """.trimIndent()
+        unnecessaryParenthesesBeforeTrailingLambdaRuleAssertThat(code).hasNoLintViolations()
+    }
 }
