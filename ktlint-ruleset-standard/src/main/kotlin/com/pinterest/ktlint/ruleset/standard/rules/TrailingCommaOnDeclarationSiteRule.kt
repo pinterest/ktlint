b082814fbb292753d7f0b5fbc88f425c84302ede
mgroth0 <mgroth49@gmail.com>
2024-12-29 13:23:11 -0500

    remove a getPsi() from TrailingCommaOnDeclarationSiteRule

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/TrailingCommaOnDeclarationSiteRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/TrailingCommaOnDeclarationSiteRule.kt
@@ -41,6 +41,7 @@ import org.ec4j.core.model.PropertyType.PropertyValueParser
 import org.jetbrains.kotlin.KtNodeTypes.WHEN_ENTRY_GUARD
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.PsiWhiteSpaceImpl
 import org.jetbrains.kotlin.com.intellij.psi.tree.TokenSet
@@ -324,7 +325,7 @@ public class TrailingCommaOnDeclarationSiteRule :
                                     .treeParent
                                     .indent()
                             if (leafBeforeArrowOrNull.isWhiteSpace()) {
-                                (leafBeforeArrowOrNull.psi as LeafPsiElement).rawReplaceWithText(indent)
+                                (leafBeforeArrowOrNull as LeafElement).rawReplaceWithText(indent)
                             } else {
                                 inspectNode
                                     .prevCodeLeaf()
