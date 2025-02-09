00a4664a07a9b407b728b05c738254137225a2d4
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-02-09 17:33:30 +0100

    Refactor for readability

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingBetweenDeclarationsWithCommentsRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/SpacingBetweenDeclarationsWithCommentsRule.kt
@@ -1,7 +1,6 @@
 package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
-import com.pinterest.ktlint.rule.engine.core.api.ElementType.FILE
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
 import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint
 import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint.Status.EXPERIMENTAL
@@ -9,7 +8,6 @@ import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint.Status.STABLE
 import com.pinterest.ktlint.rule.engine.core.api.TokenSets
 import com.pinterest.ktlint.rule.engine.core.api.ifAutocorrectAllowed
 import com.pinterest.ktlint.rule.engine.core.api.isDeclaration
-import com.pinterest.ktlint.rule.engine.core.api.isPartOfComment
 import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpace
 import com.pinterest.ktlint.rule.engine.core.api.prevCodeSibling
 import com.pinterest.ktlint.rule.engine.core.api.prevLeaf
@@ -30,31 +28,37 @@ public class SpacingBetweenDeclarationsWithCommentsRule : StandardRule("spacing-
     ) {
         node
             .takeIf { it.elementType in TokenSets.COMMENTS }
-            ?.takeIf { it.treeParent.isDeclaration() }
-            ?.let { node ->
-                val declarationNode = node.treeParent
-                val isTailComment = node.startOffset > declarationNode.startOffset
-                if (isTailComment || !declarationNode.prevCodeSibling().isDeclaration()) return
+            ?.takeUnless { it.isTailComment() }
+            ?.treeParent
+            ?.takeIf { it.isDeclaration() }
+            ?.takeIf { it.prevCodeSibling().isDeclaration() }
+            ?.let { visitCommentedDeclaration(it, emit) }
+    }
 
-                val prevSibling = declarationNode.prevSibling { !it.isWhiteSpace() }
-                if (prevSibling != null &&
-                    prevSibling.elementType != FILE &&
-                    !prevSibling.isPartOfComment()
-                ) {
-                    val prevNode = declarationNode.prevSibling()
-                    if (prevNode.isWhiteSpace() && prevNode.text.count { it == '\n' } < 2) {
-                        emit(
-                            node.startOffset,
-                            "Declarations and declarations with comments should have an empty space between.",
-                            true,
-                        ).ifAutocorrectAllowed {
-                            val indent = node.prevLeaf()?.text?.trim('\n') ?: ""
-                            (prevNode as LeafElement).rawReplaceWithText("\n\n$indent")
-                        }
+    private fun ASTNode.isTailComment() = startOffset > treeParent.startOffset
+
+    private fun visitCommentedDeclaration(
+        node: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision,
+    ) {
+        node
+            .prevSibling()
+            ?.takeUnless { it.isBlankLine() }
+            ?.let { prevSibling ->
+                emit(node.startOffset, "Declarations and declarations with comments should have an empty space between.", true)
+                    .ifAutocorrectAllowed {
+                        val indent =
+                            node
+                                .prevLeaf()
+                                ?.text
+                                ?.trim('\n')
+                                ?: ""
+                        (prevSibling as LeafElement).rawReplaceWithText("\n\n$indent")
                     }
-                }
             }
     }
+
+    private fun ASTNode.isBlankLine() = isWhiteSpace() && text.startsWith("\n\n")
 }
 
 public val SPACING_BETWEEN_DECLARATIONS_WITH_COMMENTS_RULE_ID: RuleId = SpacingBetweenDeclarationsWithCommentsRule().ruleId
