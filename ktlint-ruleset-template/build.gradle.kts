c41142b1d2150079e402503c7a0d8cc903d3afc6
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-04-16 16:56:33 +0200

    Add explicit runtime dependency on junit-platform-launcher
    Since Gradle 8 the platform launcher needs explicitly be defined as runtime dependency to avoid classpath problems. See https://docs.gradle.org/8.12/userguide/upgrading_version_8.html#test_framework_implementation_dependencies

--- ktlint-ruleset-template/build.gradle.kts
+++ ktlint-ruleset-template/build.gradle.kts
@@ -33,6 +33,11 @@ dependencies {
 
     testImplementation(projects.ktlintTest)
     testRuntimeOnly(libs.slf4j)
+
+    testImplementation(libs.junit5.jupiter)
+    // Since Gradle 8 the platform launcher needs explicitly be defined as runtime dependency to avoid classpath problems
+    // https://docs.gradle.org/8.12/userguide/upgrading_version_8.html#test_framework_implementation_dependencies
+    testRuntimeOnly(libs.junit5.platform.launcher)
 }
 
 val ktlintCheck by tasks.registering(JavaExec::class) {
--- ktlint-test/build.gradle.kts
+++ ktlint-test/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation(projects.ktlintRuleEngine)
     implementation(projects.ktlintCliRulesetCore)
     api(libs.assertj)
-    api(libs.junit5)
+    api(libs.junit5.jupiter)
     api(libs.janino)
     api(libs.jimfs)
 }
