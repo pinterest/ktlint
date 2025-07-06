6e277f5fb7fba6a1896002fd80cf24fb798e6e19
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-07-06 13:14:18 +0200

    The suppression "--sun-misc-unsafe-memory-access=allow" should not override the suppression for "--add-opens=java.base/java.lang=ALL-UNNAMED" (#3042)
    Closes #2793

--- ktlint-cli/src/main/scripts/ktlint.bat
+++ ktlint-cli/src/main/scripts/ktlint.bat
@@ -11,4 +11,4 @@ java --add-opens=java.base/java.lang=ALL-UNNAMED -jar "%JAR_PATH%" %*
 
 REM With Java24+
 REM Suppress warning "sun.misc.Unsafe::objectFieldOffset" on Java24+ (https://github.com/pinterest/ktlint/issues/2973)
-REM java --sun-misc-unsafe-memory-access=allow -jar "%JAR_PATH%" %*
+REM java --add-opens=java.base/java.lang=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow -jar "%JAR_PATH%" %*
