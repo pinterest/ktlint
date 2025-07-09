920394c0e10c98209f0d25f7258acb45bdb6c206
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-07-09 21:34:57 +0200

    Compile with Java 24 (#3049)
    * Compile with Java 24
    
    * Prevent test failures on Windows runners in GitHub build
    
    Especially on Windows runners it sometimes happens that the test process is not terminated after completion of the command that is being tested. If the command prints its exit code to the log, that is being used once the test process times out. Commands which are fully handled by Clikt, do not print the exit code to the log, which results in a test failure if the test process expires. So, for Clikt commands the assertions on the exit code are ignored, if the test process has timed out.
    
    ---------
    
    Co-authored-by: Mateusz Kwieciński <mateusz.kwiatek@gmail.com>

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
     // TODO: Remove if the build is migrated to Gradle 9
     kotlin("jvm") version libs.versions.kotlin.get() apply false // Enforce higher Kotlin version to make `poko-gradle-plugin` compatible.
@@ -17,6 +20,15 @@ kotlin {
             .get()
             .toInt(),
     )
+}
+
+// Override java target for build-logic, to the latest version. Kotlin version forced by kotlin-dsl doesn't yet support targeting Java 24
+val buildLogicJavaTarget = JvmTarget.JVM_22
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions.jvmTarget.set(buildLogicJavaTarget)
+}
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(buildLogicJavaTarget.target.toInt())
 }
 
 dependencies {
