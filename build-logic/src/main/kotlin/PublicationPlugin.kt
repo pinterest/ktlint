eb5b59eecb2e27eb7b372527c7063ac9cbc68b23
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2025-07-15 22:53:42 +0200

    Switch publication config to vanniktech's maven-publish plugin

--- build-logic/src/main/kotlin/PublicationPlugin.kt
+++ build-logic/src/main/kotlin/PublicationPlugin.kt
@@ -1,3 +1,4 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.provider.Provider
@@ -5,17 +6,14 @@ import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.get
-import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.the
 import org.gradle.plugins.signing.SigningExtension
-import java.net.URI
+import org.gradle.plugins.signing.SigningPlugin
 
 abstract class PublicationPlugin : Plugin<Project> {
     override fun apply(target: Project): Unit =
         with(target) {
-            pluginManager.apply("maven-publish")
-            pluginManager.apply("signing")
+            pluginManager.apply("com.vanniktech.maven.publish")
 
             val definedVersion: String = providers.gradleProperty("VERSION_NAME").get()
 
@@ -26,137 +24,45 @@ abstract class PublicationPlugin : Plugin<Project> {
                     definedVersion
                 }
 
-            // Create placeholder jars to satisfy Central Portal validation.
-            // See https://central.sonatype.org/publish/requirements/#supply-javadoc-and-sources
-            val readmePath = "$rootDir/gradle/"
-            val sourcePlaceholderJar =
-                tasks.register("sourcePlaceholderJar", Jar::class.java) {
-                    archiveBaseName.set("${localGradleProperty("POM_NAME").get()}")
-                    archiveVersion.set(this@with.version.toString())
-                    archiveClassifier.set("sources")
-                    from(layout.projectDirectory.file("$readmePath/README-sources.md")) {
-                        into("")
-                        rename { "README.md" }
-                    }
-                }
-
-            val docsPlaceholderJar =
-                tasks.register("docsPlaceholderJar", Jar::class.java) {
-                    archiveBaseName.set("${localGradleProperty("POM_NAME").get()}")
-                    archiveVersion.set(this@with.version.toString())
-                    archiveClassifier.set("javadoc")
-                    from(layout.projectDirectory.file("$readmePath/README-javadoc.md")) {
-                        into("")
-                        rename { "README.md" }
-                    }
-                }
-
-            extensions.configure<PublishingExtension> {
-                publications {
-                    register<MavenPublication>("maven") {
-                        groupId = localGradleProperty("POM_GROUP_ID").get()
-                        version = version.toString()
-                        artifactId = localGradleProperty("POM_ARTIFACT_ID").get()
-
-                        artifact(sourcePlaceholderJar)
-                        artifact(docsPlaceholderJar)
-
-                        pom {
-                            name.set(localGradleProperty("POM_NAME"))
-                            description.set(providers.gradleProperty("POM_DESCRIPTION"))
-                            url.set(providers.gradleProperty("POM_URL"))
-                            licenses {
-                                license {
-                                    name.set(providers.gradleProperty("POM_LICENSE_NAME"))
-                                    url.set(providers.gradleProperty("POM_LICENSE_URL"))
-                                    distribution.set("repo")
-                                }
-                            }
-                            developers {
-                                developer {
-                                    id.set(providers.gradleProperty("POM_DEVELOPER_ID"))
-                                    name.set(providers.gradleProperty("POM_DEVELOPER_NAME"))
-                                }
-                            }
-                            scm {
-                                url.set(providers.gradleProperty("POM_SCM_URL"))
-                                connection.set(providers.gradleProperty("POM_SCM_CONNECTION"))
-                                developerConnection.set(providers.gradleProperty("POM_SCM_DEV_CONNECTION"))
-                            }
-                        }
-                    }
-                }
-
-                repositories {
-                    maven {
-                        val releasesRepoUrl =
-                            URI.create(
-                                "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/",
-                            )
-                        val snapshotsRepoUrl = URI.create("https://central.sonatype.com/repository/maven-snapshots/")
-                        name = "mavenCentral"
-                        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-
-                        logger.info("Set publication repository for version $version to $url")
-
-                        credentials {
-                            username = providers.gradleProperty("CENTRAL_PORTAL_USERNAME").orNull
-                                ?: System.getenv("CENTRAL_PORTAL_USERNAME")
-                            password = providers.gradleProperty("CENTRAL_PORTAL_TOKEN").orNull
-                                ?: System.getenv("CENTRAL_PORTAL_TOKEN")
-                        }
-                    }
-
-                    maven {
-                        name = "mavenCentralRelocation"
-                        url = URI.create("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-
-                        credentials {
-                            username = providers.gradleProperty("CENTRAL_PORTAL_USERNAME").orNull
-                                ?: System.getenv("CENTRAL_PORTAL_USERNAME")
-                            password = providers.gradleProperty("CENTRAL_PORTAL_TOKEN").orNull
-                                ?: System.getenv("CENTRAL_PORTAL_TOKEN")
-                        }
-                    }
-                }
-            }
-
-            /*
-             * Following signing parameters must be configured in `$HOME/.gradle/gradle.properties`:
-             * ```properties
-             * signing.keyId=12345678
-             * signing.password=some_password
-             * signing.secretKeyRingFile=/Users/yourusername/.gnupg/secring.gpg
-             * ```
-             */
-            extensions.configure<SigningExtension> {
-                // Uncomment following line to use gpg-agent for signing
-                // See https://docs.gradle.org/current/userguide/signing_plugin.html#sec:using_gpg_agent how to configure it
-                // useGpgCmd()
-
-                val signingKeyId = System.getenv("ORG_GRADLE_PROJECT_signingKeyId")?.takeIf { it.isNotEmpty() }
-                val signingKey = System.getenv("ORG_GRADLE_PROJECT_signingKey")?.takeIf { it.isNotEmpty() }
-                val signingPassword = System.getenv("ORG_GRADLE_PROJECT_signingKeyPassword")?.takeIf { it.isNotEmpty() }
-                if (signingKeyId != null && signingKey != null && signingPassword != null) {
-                    // Avoid setting empty strings as signing keys. This avoids breaking the build when PR is opened from a fork.
-                    // Also, due to https://github.com/gradle/gradle/issues/18477 signing tasks try to prematurely access the signatory.
-                    // This also improves error messages if something's misconfigured
-                    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-                } else {
-                    logger.info(
-                        listOfNotNull(
-                            "signingKeyId".takeIf { signingKeyId.isNullOrBlank() },
-                            "signingKey".takeIf { signingKey.isNullOrBlank() },
-                            "signingPassword".takeIf { signingPassword.isNullOrBlank() },
-                        ).joinToString(prefix = "Signing info not complete. Field(s) should not be empty: "),
-                    )
-                }
+            extensions.configure<MavenPublishBaseExtension> {
+                publishToMavenCentral()
+                coordinates(
+                    groupId = localGradleProperty("POM_GROUP_ID").get(),
+                    artifactId = localGradleProperty("POM_ARTIFACT_ID").get(),
+                    version = version.toString(),
+                )
 
                 // This property allows OS package maintainers to disable signing
                 val enableSigning = providers.gradleProperty("ktlint.publication.signing.enable").orNull != "false"
 
-                sign(the<PublishingExtension>().publications["maven"])
-                isRequired = enableSigning && !version.toString().endsWith("SNAPSHOT")
+                signAllPublications()
+                the<SigningExtension>().isRequired = enableSigning && !version.toString().endsWith("SNAPSHOT")
+
+                configureBasedOnAppliedPlugins()
+
+                pom {
+                    name.set(localGradleProperty("POM_NAME"))
+                    description.set(providers.gradleProperty("POM_DESCRIPTION"))
+                    url.set(providers.gradleProperty("POM_URL"))
+                    licenses {
+                        license {
+                            name.set(providers.gradleProperty("POM_LICENSE_NAME"))
+                            url.set(providers.gradleProperty("POM_LICENSE_URL"))
+                            distribution.set("repo")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set(providers.gradleProperty("POM_DEVELOPER_ID"))
+                            name.set(providers.gradleProperty("POM_DEVELOPER_NAME"))
+                        }
+                    }
+                    scm {
+                        url.set(providers.gradleProperty("POM_SCM_URL"))
+                        connection.set(providers.gradleProperty("POM_SCM_CONNECTION"))
+                        developerConnection.set(providers.gradleProperty("POM_SCM_DEV_CONNECTION"))
+                    }
+                }
             }
         }
 
