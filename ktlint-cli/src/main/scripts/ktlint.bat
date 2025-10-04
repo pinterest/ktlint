00b8de03d6fb27505a4cbcfa2d73f38a8544492e
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2025-10-04 12:04:09 +0200

    Revert "Add stdout endcoding to `ktlint.bat`"
    This reverts commit 4ddcd3ca118069e8c8c5263d7946da23a3d808e4.

--- ktlint-cli/src/main/scripts/ktlint.bat
+++ ktlint-cli/src/main/scripts/ktlint.bat
@@ -7,8 +7,8 @@ REM and the jar file are located in different directories.
 if not defined JAR_PATH set JAR_PATH=%~dp0ktlint
 
 REM Up until Java version 23
-java --add-opens=java.base/java.lang=ALL-UNNAMED -Dstdout.encoding=UTF-8 -jar "%JAR_PATH%" %*
+java --add-opens=java.base/java.lang=ALL-UNNAMED -jar "%JAR_PATH%" %*
 
 REM With Java24+
 REM Suppress warning "sun.misc.Unsafe::objectFieldOffset" on Java24+ (https://github.com/pinterest/ktlint/issues/2973)
-REM java --sun-misc-unsafe-memory-access=allow -Dstdout.encoding=UTF-8 -jar "%JAR_PATH%" %*
+REM java --sun-misc-unsafe-memory-access=allow -jar "%JAR_PATH%" %*
