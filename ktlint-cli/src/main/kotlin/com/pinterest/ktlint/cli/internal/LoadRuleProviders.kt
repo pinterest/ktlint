1bf4cc1454406d2fefe14269603edfbe27dc48cf
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2026-02-21 17:45:12 +0100

    Load both RuleSetV2Provider, and RuleSetProviderV3, from every url

--- ktlint-cli/src/main/kotlin/com/pinterest/ktlint/cli/internal/LoadRuleProviders.kt
+++ ktlint-cli/src/main/kotlin/com/pinterest/ktlint/cli/internal/LoadRuleProviders.kt
@@ -14,29 +14,30 @@ private val LOGGER = KotlinLogging.logger {}.initKtLintKLogger()
  * Loads given list of paths to jar files.
  */
 internal fun loadRuleProviders(urls: List<URL>): Set<RuleInstanceProvider> {
-    // TODO: Process per jar for find RuleSetV2 versus RuleSetProviderV3
+    val ruleProviders = mutableListOf<RuleInstanceProvider>()
     try {
-        LOGGER.debug { "Try loading ruleset provider of type 'RuleSetV2Provider'" }
-        val ruleProviders =
+        LOGGER.debug { "Try loading ruleset provider of type 'RuleSetV2Provider' found for $urls" }
+        ruleProviders.addAll(
             RuleSetV2Provider::class.java
                 .loadFromJarFiles(urls, providerId = { it.id.value }, ERROR_WHEN_REQUIRED_PROVIDER_IS_MISSING)
                 .flatMap { it.getRuleProviders() }
                 .toSet()
-        if (ruleProviders.isNotEmpty()) {
-            return ruleProviders
-        }
-        LOGGER.debug { "No rule providers of type 'RuleSetV2Provider' found for $urls" }
+                .also { LOGGER.debug { "Found ${it.size} rule providers of type 'RuleSetV2Provider' found for $urls" } },
+        )
     } catch (t: Throwable) {
-        LOGGER.warn { "Cannot find ruleset providers of type 'RuleSetV2Provider'" }
+        LOGGER.warn(t) { "Cannot find ruleset providers of type 'RuleSetV2Provider'" }
     }
-
-    return try {
-        LOGGER.debug { "Try loading fallback ruleset provider of type 'RuleSetProviderV3'" }
-        RuleSetProviderV3::class.java
-            .loadFromJarFiles(urls, providerId = { it.id.value }, CustomJarProviderCheck.ERROR_WHEN_DEPRECATED_PROVIDER_IS_FOUND)
-            .flatMap { it.getRuleProviders() }
-            .toSet()
+    try {
+        LOGGER.debug { "Try loading ruleset provider of type 'RuleSetProviderV3' found for $urls" }
+        ruleProviders.addAll(
+            RuleSetProviderV3::class.java
+                .loadFromJarFiles(urls, providerId = { it.id.value }, CustomJarProviderCheck.ERROR_WHEN_DEPRECATED_PROVIDER_IS_FOUND)
+                .flatMap { it.getRuleProviders() }
+                .toSet()
+                .also { LOGGER.debug { "Found ${it.size} rule providers of type 'RuleSetProviderV3' found for $urls" } },
+        )
     } catch (t: Throwable) {
-        emptySet()
+        LOGGER.warn(t) { "Cannot find ruleset providers of type 'RuleSetProviderV3'" }
     }
+    return ruleProviders.toSet()
 }
