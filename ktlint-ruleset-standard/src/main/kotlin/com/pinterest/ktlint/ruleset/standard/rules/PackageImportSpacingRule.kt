7da2f633b2012606eb52004a9ee6f7ac02771949
maheen8q <mah.een8q@gmai.com>
2026-03-17 02:54:07 +0500

    reviews-addressed

--- ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/PackageImportSpacingRule.kt
+++ ktlint-ruleset-standard/src/main/kotlin/com/pinterest/ktlint/ruleset/standard/rules/PackageImportSpacingRule.kt
@@ -1,44 +1,41 @@
 package com.pinterest.ktlint.ruleset.standard.rules
 
 import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
-import com.pinterest.ktlint.rule.engine.core.api.ElementType.IMPORT_DIRECTIVE
+import com.pinterest.ktlint.rule.engine.core.api.ElementType.IMPORT_LIST
 import com.pinterest.ktlint.rule.engine.core.api.ElementType.PACKAGE_DIRECTIVE
 import com.pinterest.ktlint.rule.engine.core.api.RuleId
+import com.pinterest.ktlint.rule.engine.core.api.RuleV2
 import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint
 import com.pinterest.ktlint.rule.engine.core.api.SinceKtlint.Status.EXPERIMENTAL
 import com.pinterest.ktlint.rule.engine.core.api.ifAutocorrectAllowed
-import com.pinterest.ktlint.rule.engine.core.api.nextCodeSibling20
+import com.pinterest.ktlint.rule.engine.core.api.isWhiteSpaceWithNewline
+import com.pinterest.ktlint.rule.engine.core.api.siblings
 import com.pinterest.ktlint.ruleset.standard.StandardRule
 import org.jetbrains.kotlin.com.intellij.lang.ASTNode
-import org.jetbrains.kotlin.com.intellij.psi.impl.source.tree.LeafElement
 
 /**
  * https://developer.android.com/kotlin/style-guide#structure
  */
-@SinceKtlint("1.x", EXPERIMENTAL)
-public class PackageImportSpacingRule : StandardRule("package-import-spacing") {
+@SinceKtlint("2.0", EXPERIMENTAL)
+public class PackageImportSpacingRule : StandardRule(
+    "package-import-spacing",
+), RuleV2.Experimental {
     override fun beforeVisitChildNodes(
         node: ASTNode,
         emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision,
     ) {
         node
             .takeIf { it.elementType == PACKAGE_DIRECTIVE }
-            ?.let { packageNode ->
-                val nextSibling = packageNode.treeNext ?: return
-                val nextCodeSibling = packageNode.nextCodeSibling20
-                if (nextCodeSibling?.elementType == IMPORT_DIRECTIVE) {
-                    val newlineCount = nextSibling.text.count { it == '\n' }
-                    if (newlineCount != 2) {
-                        emit(
-                            nextSibling.startOffset,
-                            "Missing blank line between package statement and import statements",
-                            true,
-                        ).ifAutocorrectAllowed {
-                            val corrected = "\n\n" + nextSibling.text.trimStart('\n')
-                            (nextSibling as? LeafElement)?.rawReplaceWithText(corrected)
-                        }
-                    }
-                }
+            ?.siblings()
+            ?.takeWhile { it.elementType != IMPORT_LIST }
+            ?.firstOrNull { it.isWhiteSpaceWithNewline }
+            ?.takeIf { whitespace -> whitespace.text.count { it == '\n' } != 2 }
+            ?.let { whitespace ->
+                emit(
+                    whitespace.startOffset,
+                    "Expected exactly one blank line between package statement and import statements",
+                    true,
+                ).ifAutocorrectAllowed { whitespace.replaceTextWith("\n\n") }
             }
     }
 }
