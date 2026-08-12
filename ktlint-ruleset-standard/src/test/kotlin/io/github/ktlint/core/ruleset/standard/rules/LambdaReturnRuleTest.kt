2b26894d7a1ae97a962337b9beb3460f6e31b3dc
Paul Dingemans <paul-dingemans@users.noreply.github.com>
2026-08-12 17:06:22 +0200

    Only remove labeled return in lambda if it refers to the lamba containing the return statement (`lambda-return`)
    When the return statement refers to an out lambda, it may not be removed as this can result in changing behavior or compilation errors.

--- ktlint-ruleset-standard/src/test/kotlin/io/github/ktlint/core/ruleset/standard/rules/LambdaReturnRuleTest.kt
+++ ktlint-ruleset-standard/src/test/kotlin/io/github/ktlint/core/ruleset/standard/rules/LambdaReturnRuleTest.kt
@@ -1,6 +1,8 @@
 package io.github.ktlint.core.ruleset.standard.rules
 
 import io.github.ktlint.core.test.KtLintAssertThat.Companion.assertThatRule
+import io.github.ktlint.core.test.KtlintDocumentationTest
+import io.github.ktlint.core.test.LintViolation
 import org.junit.jupiter.api.Test
 
 class LambdaReturnRuleTest {
@@ -19,7 +21,7 @@ class LambdaReturnRuleTest {
     fun `Given a lambda with single statement return statement then do not emit`() {
         val code =
             """
-            val foo = bar { return@foo "value" }
+            val foo = bar { return@bar "value" }
             """.trimIndent()
         val formattedCode =
             """
@@ -72,7 +74,7 @@ class LambdaReturnRuleTest {
             """
             val foo = bar {
                 // comment-1
-                return@foo /* comment-2 */ "value" // comment-3
+                return@bar /* comment-2 */ "value" // comment-3
                 /* comment-4 */
             }
             """.trimIndent()
@@ -87,5 +89,56 @@ class LambdaReturnRuleTest {
         lambdaReturnRuleAssertThat(code)
             .hasLintViolation(3, 5, "Unnecessary return")
             .isFormattedAs(formattedCode)
+    }
+
+    @KtlintDocumentationTest
+    fun `Given some nested lambda arguments then only remove return when it refers to the inner most lambda`() {
+        val code =
+            $$"""
+            val foo =
+                "Foo"
+                    .let outer@{ foo ->
+                        if (foo != "Foo") {
+                            return@outer "$foo is not expected input"
+                        }
+                        foo
+                            .let { return@let "$it was a" }
+                            .let secondLet@{
+                                if (foo != "Foo") {
+                                    return@secondLet "$foo is not expected input"
+                                }
+                                return@secondLet "$it success"
+                            }.let { return@outer "$it (map after let)" }
+                        // This is unreachable code due to "return@outer" in let above.
+                        return@outer "$foo was a failure"
+                    }.let { return@let "$it (let after also)" }
+            """.trimIndent()
+        val formattedCode =
+            $$"""
+            val foo =
+                "Foo"
+                    .let outer@{ foo ->
+                        if (foo != "Foo") {
+                            return@outer "$foo is not expected input"
+                        }
+                        foo
+                            .let { "$it was a" }
+                            .let secondLet@{
+                                if (foo != "Foo") {
+                                    return@secondLet "$foo is not expected input"
+                                }
+                                "$it success"
+                            }.let { return@outer "$it (map after let)" }
+                        // This is unreachable code due to "return@outer" in let above.
+                        "$foo was a failure"
+                    }.let { "$it (let after also)" }
+            """.trimIndent()
+        lambdaReturnRuleAssertThat(code)
+            .hasLintViolations(
+                LintViolation(8, 24, "Unnecessary return"),
+                LintViolation(13, 21, "Unnecessary return"),
+                LintViolation(16, 13, "Unnecessary return"),
+                LintViolation(17, 17, "Unnecessary return"),
+            ).isFormattedAs(formattedCode)
     }
 }
